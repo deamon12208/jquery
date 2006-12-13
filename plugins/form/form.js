18cbdf352cca1e678a05f232128091a5ab17cfa6
joern <joern@c715fcbe-d12f-0410-84c4-316a508785bb>
2006-12-13 19:03:47 +0000

    Added revision to files
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@707 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/form/form.js
+++ plugins/form/form.js
@@ -5,6 +5,9 @@
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
+ *
+ * Revision: $Id$
+ *
  */
 
 /**
