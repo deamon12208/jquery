522db57a058794b6506b81913c1cbbdcc11fdec0
stefan <stefan@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-01-02 12:23:16 +0000

    Moved to Interface, rewrite it
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@847 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/interface/animateClass.js
+++ plugins/interface/animateClass.js
@@ -1,286 +1,79 @@
 /**
- * @author Paul Bakaus
+ * Interface Elements for jQuery
+ * Animate Class
+ * 
+ * http://interface.eyecon.ro
+ * 
+ * Copyright (c) 2006 Stefan Petre, Paul Bakaus (http://www.paulbakaus.com)
+ * Dual licensed under the MIT (MIT-LICENSE.txt) 
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *   
+ *
  */
-jQuery.fn.animateClass = function(c1,c2,c3) {
-	var colorIntervals = [];
-	var colorTimers = [];
+ 
+/**
+ * @author Paul Bakaus
+ * @author Stefan Petre
+ */
+ 
+jQuery.fx.animateClass = function(el, classToAnimate, duration, callback) {
+	var endClass = typeof classToAnimate == 'string' ? classToAnimate : classToAnimate[1];
+	var startClass = typeof classToAnimate == 'string' ? null : classToAnimate[0];
+	var oldStyleAttr = jQuery(el).attr("style") || '';
+	jQuery(el).removeClass(endClass);
+	if (startClass) {
+		jQuery(el).addClass(startClass);
+	}
+	var oldStyles = [];
+	var oldColors = [];
 	
-	return jQuery(this).each(function(){
-
-		/* If the new class is already set, return */
-		for(var i=0;i<this.className.split(" ").length;i++)
-		{ if(this.className.split(" ")[i] == c1) return; }
-			
-		var aniObj = {};
-		var aniDuration = (c2 && typeof c2 != "string") ? c2 : c3;
-		var oldStyleAttr = ($(this).attr("style") || '');
-		/* Stupidly in IE, style is a object.. */
-		if(typeof oldStyleAttr == 'object') oldStyleAttr = oldStyleAttr["cssText"];
-
-		/* Create and append a dummy element for computing the styles
-		 * on the new class. It will not be visible, of course.
-		 */
-		var dummyEl = this.cloneNode(true);
-		$(dummyEl).html($(this).html());
-		$(dummyEl).css("visibility", "hidden").css("position", "absolute");
-		$(this.parentNode).append(dummyEl);
-
-		/* Set the new class on dummy and compute the style
-		 * Then remove it again.
-		 */
-		if(typeof c2 != "string") $(dummyEl).get(0).className = this.className; else $(dummyEl).get(0).className = "";
-		$(dummyEl).addClass(c1);
-
-		if(document.defaultView) {
-			var oldStyle = document.defaultView.getComputedStyle(this,null);
-			var newStyle = document.defaultView.getComputedStyle(dummyEl,null);
-		} else {
-			var oldStyle = this.currentStyle;
-			var newStyle = dummyEl.currentStyle;
-		}
-
-		/* The main function to form the object for animation */
-		if(window.console != undefined) console.log("Animating element to class "+c1+" with the following properties:");
-		
-		for(var n in newStyle) {
-			if( typeof newStyle[n] != "function" && newStyle[n] /* No functions and null properties */
-				&& n.indexOf("Moz") == -1 && n.indexOf("length") == -1 /* No mozilla spezific render properties. */
-				&& newStyle[n] != oldStyle[n] /* Only values that have changed are used for the animation */
-			)
-			{ 
-				/* At this part, we suggest, that all changes the user made are:
-				 * 1) Numeric changes (Integer) or
-				 * 2) Color changes
-				 * We cannot insert strings into animate
-				 * (there is no way anyway to animate a solid border to an outset one).
-				 */
-
-				if(n.indexOf("Color") == -1 && n.indexOf("color") == -1) {
-					if(!isNaN(parseInt(newStyle[n].replace(/px/, "")))) {
+	var currentStyle = document.defaultView ? document.defaultView.getComputedStyle(el,null) :  el.currentStyle;
 	
-	                	/* If it's not positioned, remove position values because of performance */
-	                    if(oldStyle.position != "static"  || (oldStyle.position == "static" && n != "left" && n != "top" && n != "bottom" && n != "right"))
-	                    {
-							if(window.console != undefined) console.log(n+": "+parseInt(newStyle[n].replace(/px/, "")));  /* Debug line */
-							aniObj[n] = parseInt(newStyle[n].replace(/px/, ""));	/* Remove px from every value and convert to integer */
-	                	}
-	                }
-				} else {
-					/* Okay this is the tricky part...
-					 * let's start the color fading.
-					 */	
-					animateColor(this,n,oldStyle[n],newStyle[n], aniDuration, oldStyleAttr);
-					if(window.console != undefined) console.log(n+": "+newStyle[n]);  /* Debug line */
-				}
+	for (var i=0; i<jQuery.fx.animatedCssRules.length; i++) {
+		if (currentStyle[jQuery.fx.animatedCssRules[i]])
+			oldStyles[i] = parseInt(currentStyle[jQuery.fx.animatedCssRules[i]]) || 0;
+	}
+	for (var i=0; i<jQuery.fx.animatedColorsCssRules.length; i++) {
+		if (currentStyle[jQuery.fx.animatedColorsCssRules[i]])
+			oldColors[i] = currentStyle[jQuery.fx.animatedColorsCssRules[i]];
+	}
+	
+	if (startClass) {
+		jQuery(el).removeClass(startClass);
+	}
+	jQuery(el).addClass(endClass);
+	var toAnimate = {};
+	var toColors = {};
+	var currentStyle = document.defaultView ? document.defaultView.getComputedStyle(el,null) :  el.currentStyle;
+	for (var i=0; i<jQuery.fx.animatedCssRules.length; i++) {
+		if (currentStyle[jQuery.fx.animatedCssRules[i]]) {
+			newStyle = parseInt(currentStyle[jQuery.fx.animatedCssRules[i]]) || 0;
+			if (newStyle != oldStyles[i]) {
+				toAnimate[jQuery.fx.animatedCssRules[i]] = newStyle;
 			}
 		}
+	}
+	for (var i=0; i<jQuery.fx.animatedColorsCssRules.length; i++) {
+		if (currentStyle[jQuery.fx.animatedColorsCssRules[i]] && currentStyle[jQuery.fx.animatedColorsCssRules[i]] != oldColors[i]) {
+			toColors[jQuery.fx.animatedColorsCssRules[i]] = [oldColors[i],currentStyle[jQuery.fx.animatedColorsCssRules[i]]];
+		}
+	}
+	jQuery(el)
+		.removeClass(endClass)
+		.animateColor(duration, toColors, callback)
+		.animate(
+		toAnimate,
+		duration,
+		function()
+		{
+			jQuery(this).addClass(endClass).attr('style',oldStyleAttr);
+			jQuery.dequeue(this, 'interfaceClassFX');
+		}
+	);
+}
 
-		/* Finally, we'll do the animation with our newly constructed animation
-		 * object with our changed values.
-		 */
-		$(this).animate(aniObj, aniDuration, function() {			
-			/* Toggle classes */
-			if(typeof c2 == "string") $(this).removeClass(c2);
-			$(this).addClass(c1);
-			
-			/* Change style attribute back to original.
-			 * For stupid IE, we need to clear the damn object.
-			 */
-			if(typeof $(this).attr("style") == 'object') {
-				$(this).attr("style")["cssText"] = "";
-				$(this).attr("style")["cssText"] = oldStyleAttr;
-			} else {
-				$(this).attr("style", oldStyleAttr);	
-			}
-			
-			/* Remove the dummy */
-			$(dummyEl).remove();
-		});
+jQuery.fn.animateClass = function(classToAnimate, duration, callback, transition) {
+	return this.queue('interfaceClassFX',function(){
+		jQuery.fx.animateClass(this, classToAnimate, duration, callback, transition);
 	});
-	
-	/* Our color animation function */
-	function animateColor(that,prop,oldColor,newColor, aniDuration, oldStyleAttr) {
-				
-		var nSC,oSC;
-		/* Workaround for special case 'transparent':
-		 * We need to set it to white, fading from transparent to red
-		 * is just very difficult.
-		 */
-		if(oldColor == "transparent") { oSC = [255,255,255]; } else {
-			if(oldColor.substr(0, 3) == "rgb") oSC = oldColor.substr(4).replace(/\)/, "").split(","); /* It's a gecko rgb value */
-			if(oldColor.substr(0, 1) == "#" && oldColor.length == 7) oSC = [parseInt(oldColor.substr(1,2),16),parseInt(oldColor.substr(3,2),16),parseInt(oldColor.substr(5,2),16)]; /* it's a hex value.. */
-			if(oldColor.substr(0, 1) == "#" && oldColor.length == 4) oSC = [parseInt(oldColor.substr(1,1)+oldColor.substr(1,1),16),parseInt(oldColor.substr(2,1)+oldColor.substr(2,1),16),parseInt(oldColor.substr(3,1)+oldColor.substr(3,1),16)]; /* it's a short hex value.. */
-			/* No luck with them? Then it's something written */
-			if(oldColor.substr(0, 3) != "rgb" && oldColor.substr(0, 1) != "#") oSC = colorToArray(oldColor);
-		}
-		if(newColor == "transparent") {
-			nSC = [255,255,255]	
-		} else {
-			if(newColor.substr(0, 3) == "rgb") nSC = newColor.substr(4).replace(/\)/, "").split(","); /* It's a gecko rgb value */
-			if(newColor.substr(0, 1) == "#" && newColor.length == 7) nSC = [parseInt(newColor.substr(1,2),16),parseInt(newColor.substr(3,2),16),parseInt(newColor.substr(5,2),16)]; /* it's a hex value.. */
-			if(newColor.substr(0, 1) == "#" && newColor.length == 4) nSC = [parseInt(newColor.substr(1,1)+newColor.substr(1,1),16),parseInt(newColor.substr(2,1)+newColor.substr(2,1),16),parseInt(newColor.substr(3,1)+newColor.substr(3,1),16)]; /* it's a short hex value.. */
-			/* No luck with them? Then it's something written */
-			if(newColor.substr(0, 3) != "rgb" && newColor.substr(0, 1) != "#") nSC = colorToArray(newColor);		
-		}
-		
-		var diffR = parseInt(nSC[0]) - parseInt(oSC[0]);
-		var diffG = parseInt(nSC[1]) - parseInt(oSC[1]);
-		var diffB = parseInt(nSC[2]) - parseInt(oSC[2]);
-		
-		colorTimers[prop] = 0;
-		colorIntervals[prop] = window.setInterval(intervalColor,20);
-		
-		function intervalColor() {
-			colorTimers[prop] = colorTimers[prop] + 20;
-			
-			var newR = Math.round(parseInt(oSC[0]) + (diffR/aniDuration)*colorTimers[prop]);
-			var newG = Math.round(parseInt(oSC[1]) + (diffG/aniDuration)*colorTimers[prop]);
-			var newB = Math.round(parseInt(oSC[2]) + (diffB/aniDuration)*colorTimers[prop]);
-			$(that).css(prop, "rgb("+newR+","+newG+","+newB+")");
-
-			if(colorTimers[prop] == aniDuration) {
-				window.clearInterval(colorIntervals[prop]);
-				/* CLEAR THE DAMN HARDCODED CSS STYLE TAG!!! FINALLY!! */
-				if(typeof $(that).attr("style") == 'object') {
-					$(that).attr("style")["cssText"] = "";
-					$(that).attr("style")["cssText"] = oldStyleAttr;
-				}					
-			}
-		};
-		function colorToArray(cColor) {
-			switch(cColor) {
-				case 'aqua':
-					return [0,255,255];
-					break;
-				case 'azure':
-					return [240,255,255];
-					break;
-				case 'beige':
-					return [245,245,220];
-					break;
-				case 'black':
-					return [0,0,0];
-					break;
-				case 'blue':
-					return [0,0,255];
-					break;
-				case 'brown':
-					return [165,42,42];
-					break;
-				case 'cyan':
-					return [0,255,255];
-					break;
-				case 'darkblue':
-					return [0,0,139];
-					break;
-				case 'darkcyan':
-					return [0,139,139];
-					break;
-				case 'darkgrey':
-					return [169,169,169];
-					break;
-				case 'darkgreen':
-					return [0,100,0];
-					break;
-				case 'darkkhaki':
-					return [189,183,107];
-					break;
-				case 'darkmagenta':
-					return [139,0,139];
-					break;
-				case 'darkolivegreen':
-					return [85,107,47];
-					break;
-				case 'darkorange':
-					return [255,140,0];
-					break;
-				case 'darkorchid':
-					return [153,50,204];
-					break;
-				case 'darkred':
-					return [139,0,0];
-					break;
-				case 'darksalmon':
-					return [233,150,122];
-					break;
-				case 'darkviolet':
-					return [148,0,211];
-					break;
-				case 'fuchsia':
-					return [255,0,255];
-					break;
-				case 'gold':
-					return [255,215,0];
-					break;
-				case 'green':
-					return [0,128,0];
-					break;
-				case 'indigo':
-					return [75,0,130];
-					break;
-				case 'khaki':
-					return [240,230,140];
-					break;
-				case 'lightblue':
-					return [173,216,230];
-					break;
-				case 'lightcyan':
-					return [224,255,255];
-					break;
-				case 'lightgreen':
-					return [144,238,144];
-					break;
-				case 'lightgrey':
-					return [211,211,211];
-					break;
-				case 'lightpink':
-					return [255,182,193];
-					break;
-				case 'lightyellow':
-					return [255,255,224];
-					break;
-				case 'lime':
-					return [0,255,0];
-					break;
-				case 'magenta':
-					return [255,0,255];
-					break;
-				case 'maroon':
-					return [128,0,0];
-					break;
-				case 'navy':
-					return [0,0,128];
-					break;
-				case 'olive':
-					return [128,128,0];
-					break;
-				case 'orange':
-					return [255,165,0];
-					break;
-				case 'pink':
-					return [255,192,203];
-					break;
-				case 'purple':
-					return [128,0,128];
-					break;
-				case 'red':
-					return [255,0,0];
-					break;
-				case 'silver':
-					return [192,192,192];
-					break;
-				case '238,130,238':
-					return [238,130,238];
-					break;
-				case 'white':
-					return [255,255,255];
-					break;
-				case 'yellow':
-					return [255,255,0]
-				default:
-					return [0,0,0];
-					break;
-			}	
-		};		
-	};
 };
