b39783a30de41fa8b0a2bd9aa85a54b777da40f8
joern <joern@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-01-11 15:49:53 +0000

    A few optimations: Move isDefined/isUndefined to private scope, use custom alias, a bit formating, compute offset only once
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1005 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/fix_events/fix_events.js
+++ plugins/fix_events/fix_events.js
@@ -47,9 +47,13 @@
  *  ** 145: Scroll Lock
  */
 
-(function() {
-  var oldFix = jQuery.event.fix;
-  jQuery.event.fix = function(event) {
+(function($) {
+  function isUndefined(n) { return typeof n == "undefined" }
+  function isDefined(n) { return typeof n != "undefined" }
+  
+  var oldFix = $.event.fix;
+  
+  $.event.fix = function(event) {
     // Fix target, pageX/pageY, stopPropagation, and preventDefault
     event = oldFix(event) 
   
@@ -59,39 +63,37 @@
     
     // Fix offsetX/offsetY
     if ( isUndefined(event.offsetX) && isDefined(event.pageX) ) {
-      event.offsetX = event.pageX - jQuery(event.target).offset(false).left;
-      event.offsetY = event.pageY - jQuery(event.target).offset(false).top;
+      var offset = $(event.target).offset(false);
+      event.offsetX = event.pageX - offset.left;
+      event.offsetY = event.pageY - offset.top;
     }  
       
     // Fix metaKey
-    if( isUndefined(event.metaKey) && isDefined(event.ctrlKey) )
+    if ( isUndefined(event.metaKey) && isDefined(event.ctrlKey) )
       event.metaKey = event.ctrlKey;
       
     // Add modifiers
-    if( isUndefined(event.modifiers) && isDefined(event.ctrlKey) )
+    if ( isUndefined(event.modifiers) && isDefined(event.ctrlKey) )
       event.modifiers = (event.altKey ? 1 : 0) + (event.ctrlKey ? 2 : 0) + (event.shiftKey ? 4 : 0);
       
     // Add which for click: 1 == left; 2 == middle; 3 == right
     // Note: button is not normalized, so don't use it
-    if( isUndefined(event.which) && isDefined(event.button) )
+    if ( isUndefined(event.which) && isDefined(event.button) )
       event.which = (event.button & 1 ? 1 : ( event.button & 2 ? 3 : ( event.button & 4 ? 2 : 0 ) ));
   
     // Add which for keypresses: keyCode
-    if( isUndefined(event.which) && isDefined(event.keyCode) )
+    if ( isUndefined(event.which) && isDefined(event.keyCode) )
       event.which = event.keyCode;
       
     // Add timeStamp if none exists
-    if( isUndefined(event.timeStamp) || event.timeStamp.constructor != Date )
+    if ( isUndefined(event.timeStamp) || event.timeStamp.constructor != Date )
       event.timeStamp = new Date();
       
     // If it's a keypress event, add charCode to IE
-    if( isUndefined(event.charCode) && event.type == "keypress" )
+    if ( isUndefined(event.charCode) && event.type == "keypress" )
       event.charCode = event.keyCode
       
   	return event;  
   }
   
-})()
-
-isUndefined = function(n) { return typeof n == "undefined" }
-isDefined   = function(n) { return typeof n != "undefined" }
+})(jQuery)
