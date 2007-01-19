c836ff77b0daa946189f25a78b3e7d5d03426913
joern <joern@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-01-19 11:03:50 +0000

    More refactoring of treeview plugin
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1128 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/treeview/jquery.treeview.js
+++ plugins/treeview/jquery.treeview.js
@@ -16,7 +16,7 @@
  *
  * Adds invisible that can be clicked and classes to apply background-images.
  *
- * @example $("ul.makeMeATree").Tree();
+ * @example $("ul.makeMeATree").Treeview();
  * @before <ul class="makeMeATree">
  * 	<li>Item 1
  *  	<ul>
@@ -39,7 +39,9 @@
  *
  * @param Map options
  * @option speed String|Number Speed of animation, see animate() for details. Default: null, no animation
- * @option closedClass Mark li elements with this class to collapse them at first. Default: "closed"
+ * @option closed String Mark li elements with this class to collapse them at first. Default: "closed"
+ * @name Treeview
+ * @cat Plugins/Treeview
  */
 (function($) {
 
@@ -55,16 +57,18 @@
 	};
 
 	// define plugin method, currently no options
-	$.fn.Tree = function(settings) {
-		// create functions for toggle, the only difference is the direction to slide
-		function toggle(direction) {
+	$.fn.Treeview = function(settings) {
+	
+		// handle toggle event
+		function toggler() {
 			$(this).parent()
-				.swapClass("collapsable", "expandable")
-				.swapClass("lastCollapsable", "lastExpandable")
+				.swapClass(settings.collapsable, settings.expandable)
+				.swapClass(settings.lastCollapsable, settings.lastExpandable)
 				.find(">ul")
 				.toggle(settings.speed);
 		}
 	
+		// allow everyone to define their own classes, and animations
 		settings = $.extend({
 			speed: null,
 			closed: "closed",
@@ -75,20 +79,33 @@
 			last: "last",
 			hitarea: "hitarea"
 		}, settings);
+		
+		// mark last tree items
 		$("li:last-child", this).addClass("last");
-		$("li." + settings.closed + ">ul").hide();
-		this.find("li[ul]")
+		
+		// hide items marked as closed
+		$("li." + settings.closed + ">ul", this).hide();
+		
+		// find all tree items with child lists
+		$("li[ul]", this)
+			// handle closed ones first
 			.filter("." + settings.closed)
 				.addClass(settings.expandable)
 				.swapClass(settings.last, settings.lastExpandable)
 				.end()
+			// handle open ones
 			.not("." + settings.closed)
 				.addClass(settings.collapsable)
 				.swapClass(settings.last, settings.lastCollapsable)
 				.end()
+			// append hitarea
 			.append("<div class=\"" + settings.hitarea + "\">")
+			// find hitarea
 			.find("div." + settings.hitarea)
-			.toggle( toggle, toggle );
+			// apply toggle event to hitarea
+			.toggle( toggler, toggler );
+		
+		// return unmodified this for chaining
 		return this;
 	}
 })(jQuery);
