8639ff17c636169ea292a212cc9f59eecff09def
kelvin <kelvin@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-01-22 23:43:26 +0000

    Added jScrollPane demo pages and assets.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1162 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ plugins/jScrollPane/demo/jScrollPane.js
@@ -0,0 +1,297 @@
+/* Copyright (c) 2006 Kelvin Luck (kelvin AT kelvinluck DOT com || http://www.kelvinluck.com)
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) 
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ * 
+ * See http://kelvinluck.com/assets/jquery/jScrollPane/
+ * $Id: jScrollPane.js 1042 2007-01-12 22:30:27Z kelvin $
+ */
+
+/**
+ * Replace the vertical scroll bars on any matched elements with a fancy
+ * styleable (via CSS) version. With JS disabled the elements will
+ * gracefully degrade to the browsers own implementation of overflow:auto.
+ * If the mousewheel plugin has been included on the page then the scrollable areas will also
+ * respond to the mouse wheel.
+ *
+ * @example jQuery(".scroll-pane").jScrollPane();
+ *
+ * @name jScrollPane
+ * @type jQuery
+ * @param Object	settings	hash with options, described below.
+ *								scrollbarWidth - the width of the generated scrollbar in pixels
+ *								scrollbarMargin - the amount of space to leave on the side of the scrollbar in pixels
+ *								wheelSpeed - The speed the pane will scroll in response to the mouse wheel in pixels
+ *								showArrows - Whether to display arrows for the user to scroll with
+ * @return jQuery
+ * @cat Plugins/jScrollPane
+ * @author Kelvin Luck (kelvin AT kelvinluck DOT com || http://www.kelvinluck.com)
+ */
+jQuery.fn.jScrollPane = function(settings)
+{
+	settings = jQuery.extend(
+		{
+			scrollbarWidth : 10,
+			scrollbarMargin : 5,
+			wheelSpeed : 18,
+			showArrows : false
+		}, settings
+	);
+	return this.each(
+		function()
+		{
+			var $this = jQuery(this);
+			
+			if (jQuery(this).parent().is('.jScrollPaneContainer')) {
+				var $c = jQuery(this).parent();
+				var paneWidth = $c.innerWidth();
+				var paneHeight = $c.outerHeight();
+				var trackHeight = paneHeight;
+				if ($c.unmousewheel) {
+					$c.unmousewheel();
+				}
+				jQuery('>.jScrollPaneTrack', $c).remove();
+				$this.css({'top':0});
+			} else {
+				this.originalPadding = $this.css('paddingTop') + ' ' + $this.css('paddingRight') + ' ' + $this.css('paddingBottom') + ' ' + $this.css('paddingLeft');
+				this.originalSidePaddingTotal = (parseInt($this.css('paddingLeft')) || 0) + (parseInt($this.css('paddingRight')) || 0);
+				var paneWidth = $this.innerWidth();
+				var paneHeight = $this.innerHeight();
+				var trackHeight = paneHeight;
+				$this.wrap(
+					jQuery('<div>').attr(
+						{'className':'jScrollPaneContainer'}
+					).css(
+						{
+							'height':paneHeight+'px', 
+							'width':paneWidth+'px'
+						}
+					)
+				);
+			}
+			var p = this.originalSidePaddingTotal;
+			$this.css(
+				{
+					'height':'auto',
+					'width':paneWidth - settings.scrollbarWidth - settings.scrollbarMargin - p + 'px',
+					'paddingRight':settings.scrollbarMargin + 'px'
+				}
+			);
+			var contentHeight = $this.outerHeight();
+			var percentInView = paneHeight / contentHeight;
+
+			if (percentInView < .98) {
+				var $container = $this.parent();
+				$container.append(
+					jQuery('<div>').attr({'className':'jScrollPaneTrack'}).css({'width':settings.scrollbarWidth+'px'}).append(
+						jQuery('<div>').attr({'className':'jScrollPaneDrag'}).css({'width':settings.scrollbarWidth+'px'})
+					)
+				);
+				
+				var $track = jQuery('>.jScrollPaneTrack', $container);
+				var $drag = jQuery('>.jScrollPaneTrack .jScrollPaneDrag', $container);
+				
+				if (settings.showArrows) {
+					
+					var currentArrowButton;
+					var currentArrowDirection;
+					var currentArrowInterval;
+					var currentArrowInc;
+					var whileArrowButtonDown = function()
+					{
+						if (currentArrowInc > 8 || currentArrowInc%4==0) {
+							positionDrag(dragPosition + currentArrowDirection * mouseWheelMultiplier);
+						}
+						currentArrowInc ++;
+					};
+					var onArrowMouseUp = function(event)
+					{
+						jQuery('body').unbind('mouseup', onArrowMouseUp);
+						currentArrowButton.removeClass('jScrollActiveArrowButton');
+						clearInterval(currentArrowInterval);
+						//console.log($(event.target));
+						//currentArrowButton.parent().removeClass('jScrollArrowUpClicked jScrollArrowDownClicked');
+					};
+					var onArrowMouseDown = function() {
+						//console.log(direction);
+						//currentArrowButton = $(this);
+						jQuery('body').bind('mouseup', onArrowMouseUp);
+						currentArrowButton.addClass('jScrollActiveArrowButton');
+						currentArrowInc = 0;
+						currentArrowInterval = setInterval(whileArrowButtonDown, 100);
+					};
+					$container
+						.append(
+							jQuery('<a>')
+								.attr({'href':'javascript:;', 'className':'jScrollArrowUp'})
+								.css({'width':settings.scrollbarWidth+'px'})
+								.html('Scroll up')
+								.bind('mousedown', function()
+								{
+									currentArrowButton = $(this);
+									currentArrowDirection = -1;
+									onArrowMouseDown();
+									this.blur();
+									return false;
+								}),
+							jQuery('<a>')
+								.attr({'href':'javascript:;', 'className':'jScrollArrowDown'})
+								.css({'width':settings.scrollbarWidth+'px'})
+								.html('Scroll down')
+								.bind('mousedown', function()
+								{
+									currentArrowButton = $(this);
+									currentArrowDirection = 1;
+									onArrowMouseDown();
+									this.blur();
+									return false;
+								})
+						);
+					var topArrowHeight = jQuery('>.jScrollArrowUp', $container).height();
+					trackHeight = paneHeight - topArrowHeight - jQuery('>.jScrollArrowDown', $container).height();
+					$track
+						.css({'height': trackHeight+'px', top:topArrowHeight+'px'})
+				}
+				
+				var $pane = jQuery(this).css({'position':'absolute', 'overflow':'visible'});
+				
+				var currentOffset;
+				var maxY;
+				var mouseWheelMultiplier;
+				// store this in a seperate variable so we can keep track more accurately than just updating the css property..
+				var dragPosition = 0;
+				var dragMiddle = percentInView*paneHeight/2;
+				
+				// pos function borrowed from tooltip plugin and adapted...
+				var getPos = function (event, c) {
+					var p = c == 'X' ? 'Left' : 'Top';
+					return event['page' + c] || (event['client' + c] + (document.documentElement['scroll' + p] || document.body['scroll' + p])) || 0;
+				};
+				
+				var ignoreNativeDrag = function() {	return false; };
+				
+				var initDrag = function()
+				{
+					currentOffset = $drag.offset(false);
+					currentOffset.top -= dragPosition;
+					maxY = trackHeight - $drag[0].offsetHeight;
+					mouseWheelMultiplier = 2 * settings.wheelSpeed * maxY / contentHeight;
+				};
+				
+				var onStartDrag = function(event)
+				{
+					initDrag();
+					dragMiddle = getPos(event, 'Y') - dragPosition - currentOffset.top;
+					jQuery('body').bind('mouseup', onStopDrag).bind('mousemove', updateScroll);
+					if (jQuery.browser.msie) {
+						jQuery('body').bind('dragstart', ignoreNativeDrag).bind('selectstart', ignoreNativeDrag);
+					}
+					return false;
+				};
+				var onStopDrag = function()
+				{
+					jQuery('body').unbind('mouseup', onStopDrag).unbind('mousemove', updateScroll);
+					dragMiddle = percentInView*paneHeight/2;
+					if (jQuery.browser.msie) {
+						jQuery('body').unbind('dragstart', ignoreNativeDrag).unbind('selectstart', ignoreNativeDrag);
+					}
+				};
+				var positionDrag = function(destY)
+				{
+					destY = destY < 0 ? 0 : (destY > maxY ? maxY : destY);
+					dragPosition = destY;
+					$drag.css({'top':destY+'px'});
+					var p = destY / maxY;
+					$pane.css({'top':((paneHeight-contentHeight)*p) + 'px'});
+				};
+				var updateScroll = function(e)
+				{
+					positionDrag(getPos(e, 'Y') - currentOffset.top - dragMiddle);
+				};
+				
+				$drag.css(
+					{'height':(percentInView*paneHeight)+'px'}
+				).bind('mousedown', onStartDrag);
+				
+				var trackScrollInterval;
+				var trackScrollInc;
+				var trackScrollMousePos;
+				var doTrackScroll = function()
+				{
+					if (trackScrollInc > 8 || trackScrollInc%4==0) {
+						positionDrag((dragPosition - ((dragPosition - trackScrollMousePos) / 2)));
+					}
+					trackScrollInc ++;
+				};
+				var onStopTrackClick = function()
+				{
+					clearInterval(trackScrollInterval);
+					jQuery('body').unbind('mouseup', onStopTrackClick).unbind('mousemove', onTrackMouseMove);
+				};
+				var onTrackMouseMove = function(event)
+				{
+					trackScrollMousePos = getPos(event, 'Y') - currentOffset.top - dragMiddle;
+				};
+				var onTrackClick = function(event)
+				{
+					initDrag();
+					onTrackMouseMove(event);
+					trackScrollInc = 0;
+					trackScrollInterval = setInterval(doTrackScroll, 100);
+					jQuery('body').bind('mouseup', onStopTrackClick).bind('mousemove', onTrackMouseMove);
+				};
+				
+				$track.bind('mousedown', onTrackClick);
+				
+				// if the mousewheel plugin has been included then also react to the mousewheel
+				if ($container.mousewheel) {
+					$container.mousewheel(
+						function (event, delta) {
+							initDrag();
+							var d = dragPosition;
+							positionDrag(dragPosition - delta * mouseWheelMultiplier);
+							var dragOccured = d != dragPosition;
+							if (!dragOccured) {
+								// try and apply the mouse event to parents?
+								var $p = $(this).parent();
+								if ($p.length == 0) {
+									return true;
+								}
+								var p = $p[0];
+								var r = p != document;
+								while (r) {
+									if (p._mwHandlers) {
+										r = p._mwHandlers[0](event, delta);
+									}
+									p = $(p).parent()[0];
+									if (p == document) {
+										break;
+									}
+								}
+							} else {
+								if (event.preventDefault) {
+									event.preventDefault();
+								} else {
+									event.returnValue = false;
+								}
+								return false;
+							}
+							return true;
+						},
+						false
+					);					
+				}
+				
+				initDrag();
+				
+			} else {
+				$this.css(
+					{
+						'height':paneHeight+'px',
+						'width':paneWidth-this.originalSidePaddingTotal+'px',
+						'padding':this.originalPadding
+					}
+				);
+			}
+		}
+	)
+}
---
+++ plugins/jScrollPane/demo/mousewheel.js
@@ -0,0 +1,231 @@
+/* Copyright (c) 2006 Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ * Thanks to: http://adomas.org/javascript-mouse-wheel/ for some pointers.
+ * Thanks to: Mathias Bank(http://www.mathias-bank.de) for a scope bug fix.
+ *
+ * $LastChangedDate: 2007-01-12 22:57:21 +0000 (Fri, 12 Jan 2007) $
+ * $Rev: 1043 $
+ */
+
+jQuery.fn.extend({
+	/**
+	 * Apply the mousewheel event to the elements in the jQuery object.
+	 * The handler function should be prepared to take the event object
+	 * and a param called 'delta'. The 'delta' param is a number
+	 * either > 0 or < 0. > 0 = up and < 0 = down.
+	 *
+	 * Firefox, Opera and Safari has some issues preventing default 
+	 * and just using event.preventDefault() will not work. By
+	 * default this method will attempt to prevent the browser's
+	 * default action. You can allow the default action by passing
+	 * in false for the preventDefault paramater.
+	 *
+	 * @example $("p").mousewheel(function(event, delta){
+	 *   if (delta > 0)
+	 *     // do something on mousewheel scroll up
+	 *   else if (delta < 0)
+	 *     //do something on mousewheel scroll down
+	 * });
+	 *
+	 * @name mousewheel
+	 * @type jQuery
+	 * @param Function handler A function to call when onmousewheel fires. Should take two params: event and delta.
+	 * @param Boolean preventDefault Should the default action be prevented? By default the browser's default action is prevented.
+	 * @cat Plugins/Mousewheel
+	 * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
+	 */
+	mousewheel: function(fn, preventDefault) {
+		return this.each(function() {
+			
+			if (!fn.guid)
+				fn.guid = jQuery.event.guid++;
+			
+			// set preventDefault option
+			if (preventDefault !== false) this._mwPreventDefault = true;
+			
+			// if the element already has a handler
+			if (this._mwHandlers) {
+				this._mwHandlers.push(fn); // add function
+				return;                    // short-circuit
+			}
+			
+			var self = this;
+			
+			// create array of handler functions
+			if (!this._mwHandlers) this._mwHandlers = [];
+			this._mwHandlers.push(fn);
+			
+			// create the mouseover and mouseout handler methods for easy removal later
+			this._mwOver = function() {
+				jQuery.event.mousewheel.giveFocus(self);
+			};
+			this._mwOut = function() {
+				jQuery.event.mousewheel.removeFocus(self);
+			};
+			
+			// create mousewheel handler function
+			this._handleMousewheel = function(event) {
+				if (!event) event = window.event;
+
+				// cannot call any methods before preventing default (firefox)
+				if (self._mwPreventDefault) 
+					if (event.preventDefault) event.preventDefault();
+					else event.returnValue = false;
+
+				// fix event
+				jQuery.event.fix(event);
+
+				// normalize delta property
+				var delta = 0;
+				if (event.wheelDelta)
+					delta = event.wheelDelta/120;
+				else if (event.detail)
+					delta = -event.detail/3;
+				else if (window.opera)
+					delta = -(event.wheelDelta/120);
+
+				// call the handler
+				for (var i=0; i<self._mwHandlers.length; i++)
+					if (self._mwHandlers[i])
+						self._mwHandlers[i].call(self, event, delta);
+				
+				return self._mPreventDefault ? false : true;
+			};
+			
+			// add the hover event
+			jQuery(this).bind('mouseover', jQuery.event.mousewheel.handleHover).bind('mouseout', jQuery.event.mousewheel.handleHover);
+			
+			// add element to cache to remove expandos at window.unload to avoid memory leaks in IE
+			if (jQuery.browser.msie) jQuery.event.mousewheel.$elementCache.push(jQuery(this));
+		});
+	},
+
+	/**
+	 * This method removes one or all applied mousewheel events from the elements.
+	 * You can remove a single handler function by passing it as the first param.
+	 * If you do not pass anything, it will remove all handlers.
+	 *
+	 * @name unmousewheel
+	 * @param Function handler The handler function to remove from the mousewheel event.
+	 * @type jQuery
+	 * @cat Plugins/Mousewheel
+	 * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
+	 */
+	unmousewheel: function(fn) {
+		return this.each(function() {
+			if (fn && this._mwHandlers.length > 1) {
+				for (var i=0; i<this._mwHandlers.length; i++)
+					if (this._mwHandlers[i] && this._mwHandlers[i].guid == fn.guid)
+						delete this._mwHandlers[i];
+			} else {
+				jQuery(this).unbind('mouseover', jQuery.event.mousewheel.handleHover).unbind('mouseout', jQuery.event.mousewheel.handleHover);
+				this._mwPreventDefault = this._mwHandlers = this._mwOver = this._mwOut = null;
+				jQuery.event.mousewheel.removeFocus(this);
+			}
+		});
+	}
+});
+
+
+/**
+ * The manager for the mousewheel event.
+ * Handles the switching of focus for the
+ * different elements that have the
+ * mousewheel event attached to them.
+ *
+ * @private
+ * @cat Plugins/Mousewheel
+ * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
+ */
+jQuery.event.mousewheel = {	
+	/**
+	 * This method handles which element is to have
+	 * focus of the mousewheel event.
+	 *
+	 * @private
+	 * @cat Plugins/Mousewheel
+	 * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
+	 */
+	handleHover: function(event) {
+		// Check if mouse(over|out) are still within the same parent element
+		var to = p = (event.type == "mouseover" ? event.fromElement : event.toElement) || event.relatedTarget;
+
+		// Traverse up the tree
+		while ( p && p != this ) try { p = p.parentNode; } catch(e) { p = this; };
+		
+		// If we actually just moused on to a sub-element 
+		// that doesn't have a mousewheel event attached, ignore it
+		if ( p == this ) {
+			
+			if (event.type == 'mouseout') {
+				if (!to._mwHandlers)
+					while (to && !to._mwHandlers) try { to = to.parentNode; } catch(e) { to = null; }
+				
+				if (to && to._mwHandlers)
+					jQuery.event.mousewheel.giveFocus(to);
+			}
+			return false;
+		}
+		
+		// Execute the right function
+		return (event.type == "mouseover" ? this._mwOver : this._mwOut).call(this, event);
+	},
+	
+	/**
+	 * Give the element passed the mousewheel event focus.
+	 * If the element already has mousewheel events attached
+	 * to it, they will be removed before adding them back.
+	 *
+	 * @private
+	 * @name giveFocus
+	 * @param HTMLElement element The DOM node to apply the mousewheel event too.
+	 * @param Function handler The function to call when onmousewheel fires.
+	 * @param Boolean preventDefault Should the default action be prevented?.
+	 * @see jQuery.fn.mousewheel
+	 * @cat Plugins/Mousewheel
+	 * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
+	 */
+	giveFocus: function(el) {
+		if (jQuery.event.mousewheel.currentFocused)
+			jQuery.event.mousewheel.removeFocus(jQuery.event.mousewheel.currentFocused);
+		jQuery.event.mousewheel.currentFocused = el;
+		if (window.addEventListener)
+			window.addEventListener('DOMMouseScroll', el._handleMousewheel, false);
+		window.onmousewheel = document.onmousewheel = el._handleMousewheel;
+	},
+
+	/**
+	 * Removes the mousewheel event focus from the
+	 * element passed in.
+	 *
+	 * @private
+	 * @name removeFocus
+	 * @see jQuery.fn.unmousewheel
+	 * @cat Plugins/Mousewheel
+	 * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
+	 */
+	removeFocus: function(el) {
+		jQuery.event.mousewheel.currentFocused = null;
+		if (window.removeEventListener)
+			window.removeEventListener('DOMMouseScroll', el._handleMousewheel, false);
+		window.onmousewheel = document.onmousewheel = null;
+	},
+	
+	/**
+	 * Used to store a cache of elements for IE to aid in preventing memory leaks
+	 * It is an array of jQuery objects
+	 *
+	 * @private
+	 */
+	$elementCache: []
+};
+
+// Avoid memory leaks by removing mousewheel events and expandos on each cached element
+if (jQuery.browser.msie) 
+	jQuery(window)
+		.bind('unload', function() {
+			var els = jQuery.event.mousewheel.$elementCache; 
+			for (var i=0; i<els.length; i++)
+				els[i].unmousewheel();
+		});
