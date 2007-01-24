0feb704f088ccddf0b93f7fe911d7e38702075c8
joern <joern@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-01-24 15:57:30 +0000

    Added black tree images, implemented collapsed option, moved classes and hitarea styles into statics, started docs
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1183 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/treeview/jquery.treeview.js
+++ plugins/treeview/jquery.treeview.js
@@ -1,5 +1,5 @@
 /*
- * Tree - jQuery plugin expandable tree view
+ * Treeview - jQuery plugin to hide and show branches of a tree
  *
  * Copyright (c) 2006 Jörn Zaefferer, Myles Angell
  *
@@ -12,18 +12,45 @@
  */
 
 /**
- * Select an unordered list (without list-style) and apply.
+ * Takes an unordered list and makes all branches collapsable.
  *
- * Adds invisible that can be clicked and classes to apply background-images.
+ * Mark initially closed branches with "closed", open with "open".
  *
- * @example $("ul.makeMeATree").Treeview();
- * @before <ul class="makeMeATree">
+ * The following styles are necessary in your stylesheet. Modify them to fit your needs.
+ *
+ * .treeview, .treeview ul { 
+ * 	padding: 0;
+ * 	margin: 0;
+ * 	list-style: none;
+ * }	
+ * 
+ * .treeview li { 
+ * 	position: relative;
+ * 	margin: 0;
+ * 	padding: 4px 0 3px 20px;
+ * 	z-index: 10;
+ * }
+ * 
+ * .treeview li { background: url(images/tv-item.gif) 0 0 no-repeat; }
+ *
+ * .treeview .collapsable { background-image: url(images/tv-collapsable.gif); }
+ *
+ * .treeview .expandable { background-image: url(images/tv-expandable.gif); }
+ *
+ * .treeview .last { background-image: url(images/tv-item-last.gif); }
+ *
+ * .treeview .lastCollapsable { background-image: url(images/tv-collapsable-last.gif); }
+ *
+ * .treeview .lastExpandable { background-image: url(images/tv-expandable-last.gif); }
+ *
+ * @example $("ul").Treeview();
+ * @before <ul>
  * 	<li>Item 1
  *  	<ul>
  * 			<li>Item 1.1</li>
  *    	</ul>
  *  </li>
- *  <li>Item 2
+ *  <li class="closed">Item 2 (starts as closed)
  * 		<ul>
  * 			<li>Item 2.1
  * 				<ul>
@@ -38,48 +65,45 @@
  * </ul>
  *
  * @param Map options
- * @option speed String|Number Speed of animation, see animate() for details. Default: null, no animation
- * @option closed String Mark li elements with this class to collapse them at first. Default: "closed"
+ * @option String|Number speed Speed of animation, see animate() for details. Default: null, no animation
+ * @option String closed Mark li elements with this class to collapse them at first. Default: "closed"
+ * @option Boolean collapsed Start with all branches collapsed. Default: false, all expanded
  * @type TreeController
  * @name Treeview
  * @cat Plugins/Treeview
  */
- 
-/**
- * Takes a TreeController and applies it to a group of three links.
- *
- * TODO: Example
- *
- * @name TreeControl
- * @type jQuery
- * @cat Plugins/Treeview
- */ 
- 
-/**
- * Hides all branches of the tree.
- *
- * @name TreeController.collapseAll
- * @type TreeController
- * @cat Plugins/Treeview
- */
- 
-/**
- * Shows all branches of the tree.
- *
- * @name TreeController.expandAll
- * @type TreeController
- * @cat Plugins/Treeview
- */
- 
-/**
- * Toggles all branches of the tree, showing collapsed and hiding expanded.
- *
- * @name TreeController.toggleAll
- * @type TreeController
- * @cat Plugins/Treeview
- */
- 
+
 (function($) {
+
+	// classes used by the plugin, need to be styled via external stylesheet
+	var CLASSES = {
+		open: "open",
+		closed: "closed",
+		expandable: "expandable",
+		collapsable: "collapsable",
+		lastCollapsable: "lastCollapsable",
+		lastExpandable: "lastExpandable",
+		last: "last",
+		hitarea: "hitarea"
+	}
+	
+	// styles for hitareas
+	var hitareaCSS = {
+		height: 15,
+		width: 15,
+		position: "absolute",
+		top: 1,
+		left: -1,
+		cursor: "pointer",
+		zIndex: 50
+	}
+	// ie specific stlyes for hitareas
+	if( $.browser.msie )
+		$.extend( hitareaCSS, {
+			background: "#fff",
+			filter: "alpha(opacity=0)",
+			left: -21
+		});
 
 	$.fn.swapClass = function(c1,c2) {
 		return this.each(function() {
@@ -92,89 +116,63 @@
 		});
 	};
 	
-	$.fn.TreeControl = function(controller) {
-		function build(method) {
-			return function() {
-				controller[method]();
-				return false;
-			}
-		}
-		$(":eq(0)", this).click( build("collapseAll") );
-		$(":eq(1)", this).click( build("expandAll") );
-		$(":eq(2)", this).click( build("toggleAll") ); 
-		return this;
-	}
-	
 	// define plugin method, currently no options
 	$.fn.Treeview = function(settings) {
 	
-		function TreeController(tree) {
+		function treeController(tree, control) {
 			function build(filter) {
 				return function() {
-					toggler( $("div." + settings.hitarea, tree).filter(function() {
+					toggler( $("div." + CLASSES.hitarea, tree).filter(function() {
 						return filter ? $(this).parent("." + filter).length : true;
 					}) );
-					return this;
+					return false;
 				}
 			}
-			this.collapseAll = build(settings.collapsable);
-			this.expandAll = build(settings.expandable);
-			this.toggleAll = build();
+			$(":eq(0)", control).click( build(CLASSES.collapsable) );
+			$(":eq(1)", control).click( build(CLASSES.expandable) );
+			$(":eq(2)", control).click( build() ); 
 		}
 	
 		// handle toggle event
 		function toggler(start) {
 			$(start.type ? this : start).parent()
-				.swapClass(settings.collapsable, settings.expandable)
-				.swapClass(settings.lastCollapsable, settings.lastExpandable)
+				.swapClass(CLASSES.collapsable, CLASSES.expandable)
+				.swapClass(CLASSES.lastCollapsable, CLASSES.lastExpandable)
 				.find(">ul")
 				.toggle(settings.speed);
 		}
 	
-		// allow everyone to define their own classes, and animations
-		settings = $.extend({
-			speed: null,
-			control: null,
-			closed: "closed",
-			expandable: "expandable",
-			collapsable: "collapsable",
-			lastCollapsable: "lastCollapsable",
-			lastExpandable: "lastExpandable",
-			last: "last",
-			hitarea: "hitarea"
-		}, settings);
-		
 		this.addClass("treeview");
 		
 		// mark last tree items
-		$("li:last-child", this).addClass("last");
+		$("li:last-child", this).addClass(CLASSES.last);
 		
-		// hide items marked as closed
-		$("li." + settings.closed + ">ul", this).hide();
+		// collapse whole tree or only those marked as closed, except those marked as open
+		$( (settings.collapsed ? "li" : "li." + CLASSES.closed) + ":not(." + CLASSES.open + ") > ul", this).hide();
 		
 		// find all tree items with child lists
 		$("li[ul]", this)
 			// handle closed ones first
-			.filter("." + settings.closed)
-				.addClass(settings.expandable)
-				.swapClass(settings.last, settings.lastExpandable)
-				.end()
+			.filter("[ul:hidden], ." + CLASSES.closed).not("[ul:visible]")
+				.addClass(CLASSES.expandable)
+				.swapClass(CLASSES.last, CLASSES.lastExpandable)
+				.end().end()
 			// handle open ones
-			.not("." + settings.closed)
-				.addClass(settings.collapsable)
-				.swapClass(settings.last, settings.lastCollapsable)
+			.not("." + CLASSES.closed)
+				.addClass(CLASSES.collapsable)
+				.swapClass(CLASSES.last, CLASSES.lastCollapsable)
 				.end()
 			// append hitarea
-			.append("<div class=\"" + settings.hitarea + "\">")
+			.append("<div class=\"" + CLASSES.hitarea + "\">")
 			// find hitarea
-			.find("div." + settings.hitarea)
+			.find("div." + CLASSES.hitarea)
+			.css(hitareaCSS)
 			// apply toggle event to hitarea
 			.toggle( toggler, toggler );
 		
-		var control = new TreeController(this);
 		if(settings.control)
-			$(settings.control).TreeControl(control)
+			treeController(this, settings.control);
 		
-		return control;
+		return this;
 	}
 })(jQuery);
