1dde9b0cdd850f96a81e706b0b66b3c794487117
joern <joern@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-01-29 20:45:54 +0000

    Minor refactorings, fixed missing semicolons
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1219 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/quicksearch/jquery.quicksearch.js
+++ plugins/quicksearch/jquery.quicksearch.js
@@ -13,161 +13,150 @@
  
 (function($) {
 
-	$.fn.quicksearch = function (options) {
+$.fn.quicksearch = function (options) {
+	
+	this.timeout = null;
+	
+	this.settings = $.extend({
+		position: 'prepend',
+		attached: 'body',
+		formId: 'quicksearch',
+		labelText: 'Quick Search',
+		labelClass: 'qs_label',
+		inputText: null,
+		inputClass: 'qs_input',
+		loaderId: 'loader',
+		loaderClass: 'loader',
+		loaderImg: null,
+		loaderText: 'Loading...',
+		stripeRowClass: null,
+		hideElement: null,
+		delay: 250,
+		focusOnLoad: false,
+		randomElement: 'qs'+Math.floor(Math.random()*1000000)
+	}, options);
+	
+	var el 		= this;
 		
-		this.timeout = null;
-		
-		this.settings = $.extend({
-			position: 'prepend',
-			attached: 'body',
-			formId: 'quicksearch',
-			labelText: 'Quick Search',
-			labelClass: 'qs_label',
-			inputText: null,
-			inputClass: 'qs_input',
-			loaderId: 'loader',
-			loaderClass: 'loader',
-			loaderImg: null,
-			loaderText: 'Loading...',
-			stripeRowClass: null,
-			hideElement: null,
-			delay: 250,
-			focusOnLoad: false,
-			randomElement: 'qs'+Math.floor(Math.random()*1000000)
-		}, options);
-		
-		var el 		= this;
-			
-		var form1 	= new $._form(this.settings);
-		var key1	= new $._key(this.settings);
-		var loader	= new $._loader(this.settings);
-		var stripes = new $._stripe(this.settings.stripeRowClass);	
-		
-		form1.initialize();
-		loader.setTo('hide');	
-		
-		if(this.settings.stripeRowClass != null) {
-			$(el).each(function () {			
-				if(el.settings.hideElement == "grandparent") {
-					stripes.go(this.parentNode.parentNode);
-				} else if (el.settings.hideElement == "parent") {
-					stripes.go(this.parentNode);
-				} else {
-					stripes.go(this);
-				}
-			})
-			stripes.reset();
-		}		
-		
-		$('form.quicksearch').submit( function () { return false; });
-		
-		$('input[@rel="'+this.settings.randomElement+'"]').keydown(function(e) {
-			switch(e.keyCode) {
-				case 9: 
-				case 13:
-				case 38: 
-				case 40:
-					e.preventDefault();
-					break;
-				default:
-					clearTimeout(this.timeout);				
-				
-					this.timeout = setTimeout(function () {
-							loader.setTo('show');
-							setTimeout( function () {
-							
-								key1.setKey();
-								
-								$(el).each( function () {
-								
-									if(el.settings.hideElement == "grandparent") {
-										var hide = $(this).parent().parent();
-									} else if (el.settings.hideElement == "parent") {
-										var hide = $(this).parent();
-									} else {
-										var hide = $(this)
-									}
-								
-									if(key1.test(this)) {
-												
-										$(hide).show();
-									} else {
-										$(hide).hide();
-									}	
-									if(el.settings.stripeRowClass != null) {									
-										if(el.settings.hideElement == "grandparent") {
-											stripes.go(this.parentNode.parentNode);
-										} else if (el.settings.hideElement == "parent") {
-											stripes.go(this.parentNode);
-										} else {
-											stripes.go(this);
-										}
-									}				
-								});						
-								stripes.reset();
-							}, el.settings.delay);						
-							setTimeout( function () { 
-								loader.setTo('hide');
-							}, el.settings.delay);			
-					}, el.settings.delay);
-					break;
+	var form1 	= new Form(this.settings);
+	var key1	= new Key(this.settings);
+	var loader	= new Loader(this.settings);
+	var stripes = new Stripe(this.settings.stripeRowClass);	
+	
+	form1.initialize();
+	loader.setTo('hide');	
+	
+	if(this.settings.stripeRowClass != null) {
+		$(el).each(function () {			
+			if(el.settings.hideElement == "grandparent") {
+				stripes.go(this.parentNode.parentNode);
+			} else if (el.settings.hideElement == "parent") {
+				stripes.go(this.parentNode);
+			} else {
+				stripes.go(this);
 			}
 		});
+		stripes.reset();
+	}		
 	
-		
-		
-		
-	}
+	$('form.quicksearch').submit( function () { return false; });
 	
+	$('input[@rel="'+this.settings.randomElement+'"]').keydown(function(e) {
+		switch(e.keyCode) {
+			case 9: 
+			case 13:
+			case 38: 
+			case 40:
+				e.preventDefault();
+				break;
+			default:
+				clearTimeout(this.timeout);				
+			
+				this.timeout = setTimeout(function () {
+						loader.setTo('show');
+						setTimeout( function () {
+						
+							key1.setKey();
+							
+							$(el).each( function () {
+							
+								if(el.settings.hideElement == "grandparent") {
+									var hide = $(this).parent().parent();
+								} else if (el.settings.hideElement == "parent") {
+									var hide = $(this).parent();
+								} else {
+									var hide = $(this)
+								}
+							
+								$(hide)[ key1.test(this) ? "show" : "hide" ]();
+								
+								if(el.settings.stripeRowClass != null) {									
+									if(el.settings.hideElement == "grandparent") {
+										stripes.go(this.parentNode.parentNode);
+									} else if (el.settings.hideElement == "parent") {
+										stripes.go(this.parentNode);
+									} else {
+										stripes.go(this);
+									}
+								}				
+							});						
+							stripes.reset();
+						}, el.settings.delay);						
+						setTimeout( function () { 
+							loader.setTo('hide');
+						}, el.settings.delay);
+				}, el.settings.delay);
+				break;
+		}
+	});
+};
 	
-	$._key = function (set) {
-		
-		this.key = "";
-		this.settings = set;
-		this.getKey = function ()  {
-			return this.key;
-		};
-		this.setKey = function () {
-			var input = $('input[@rel="'+this.settings.randomElement+'"]').val();
-			var string = input.replace(/\s{2,}/g, " ").toLowerCase();
-			var arr = string.split(" ");
-			for(var i in arr) {
-				var regexp = new RegExp(/([^A-Za-z0-9])/gi);
-				if(arr[i] == "") {
-					arr.splice(i,1);
+function Key(set) {
+	this.key = "";
+	this.settings = set;
+	this.getKey = function ()  {
+		return this.key;
+	};
+	this.setKey = function () {
+		var input = $('input[@rel="'+this.settings.randomElement+'"]').val();
+		var string = input.replace(/\s{2,}/g, " ").toLowerCase();
+		var arr = string.split(" ");
+		for(var i in arr) {
+			var regexp = new RegExp(/([^A-Za-z0-9])/gi);
+			if(arr[i] == "") {
+				arr.splice(i,1);
+			}
+		}
+		this.key = arr;
+	};
+	this.test = function (el) {
+		if( this.getKey() == '' ) {
+			return true;
+		} else {			
+			var searchStrings = this.getKey();
+			var text = $(el).html().replace(/<\/?[^>]+>/g, '').toLowerCase();	
+			for (var i = 0; i < searchStrings.length; i++) {
+				var test = text.indexOf(searchStrings[i]);	
+				if (test == -1) {	
+					return false;
 				}
-			}
-			this.key = arr;
-		};
-		this.test = function (el) {
-			if( this.getKey() == '' ) {
-				return true;
-			} else {			
-				var searchStrings = this.getKey();
-				var text = $._stripHtml( $(el).html() );	
-				for (var i = 0; i < searchStrings.length; i++) {
-					var test = text.indexOf(searchStrings[i]);	
-					if (test == -1) {	
-						return false;
-					}
-				}			
-				return true;
-			}
-		}	
-	}
+			}			
+			return true;
+		}
+	}	
+}
 	
-	$._form = function (set) {	
-		
-		this.settings = set;
-		
-		this.initialize = function () {
-			this.placeForm();
-			if(this.settings.focusOnLoad) {
-				this.focusOnLoad();
-			}
-			if(this.settings.inputText != "" && this.settings.inputText != null) {
-				this.toggleText();
-			}
+function Form(set) {	
+	this.settings = set;
+	
+	this.initialize = function () {
+		this.placeForm();
+		if(this.settings.focusOnLoad) {
+			this.focusOnLoad();
+		}
+		if(this.settings.inputText != "" && this.settings.inputText != null) {
+			this.toggleText();
+		}
 
 	};
 	this.placeForm = function () {
@@ -175,13 +164,13 @@
 		var formAttached 	= this.settings.attached;
 					
 		if(formPosition == 'before') {
-			$(formAttached).before( $._makeForm(this.settings) );
+			$(formAttached).before( makeForm(this.settings) );
 		} else if(formPosition == 'prepend') {
-			$(formAttached).prepend( $._makeForm(this.settings) );
+			$(formAttached).prepend( makeForm(this.settings) );
 		} else if(formPosition == 'append') {
-			$(formAttached).append( $._makeForm(this.settings) );
+			$(formAttached).append( makeForm(this.settings) );
 		} else {
-			$(formAttached).after( $._makeForm(this.settings) );
+			$(formAttached).after( makeForm(this.settings) );
 		}
 	};
 	this.focusOnLoad = function () {
@@ -193,7 +182,7 @@
 			if($(this).val() == el.settings.inputText) {
 				$(this).val('');
 			}
-		}),
+		});
 		$('input[@rel="'+this.settings.randomElement+'"]').blur(function () {
 			if($(this).val() == "") {
 				$(this).val(el.settings.inputText);
@@ -202,7 +191,7 @@
 	};	
 };
 
-$._loader = function (set) {
+function Loader(set) {
 	this.settings = set;
 	this.setTo = function (to) {
 		if(this.settings.loaderId) {
@@ -212,10 +201,32 @@ $._loader = function (set) {
 				$('input[@rel="'+this.settings.randomElement+'"]').parent().find('.loader').show();
 			}			
 		}
-	}
+	};
 }
 
-$._makeForm = function (set) {
+function Stripe(set) {
+	this.i = 0;
+	this.set = set;
+	this.go = function (el) {
+		this.removeClasses(el);
+		if(el.getAttribute('style') != "display: none;") {				
+			$(el).addClass(this.set[this.i%this.set.length]);
+			this.i += 1;
+		}
+	}; 
+	this.removeClasses = function (el) {
+		for(var j = 0; j < this.set.length; j++) {
+			if(this.i%this.set.length != j) {
+				$(el).removeClass(this.set[j]);
+			}
+		}
+	};
+	this.reset = function () {
+		this.i = 0;
+	};
+}
+
+function makeForm(set) {
 	this.settings = set;
 	this.mform = function () {
 		return '<form action="#" ' + 
@@ -236,7 +247,7 @@ $._makeForm = function (set) {
 		return '';
 	};
 	this.minput = function () {			
-		var val = (!this.isEmpty(this.settings.inputText)) ? this.settings.inputText : ""
+		var val = (!this.isEmpty(this.settings.inputText)) ? this.settings.inputText : "";
 		return '<input type="text" ' +
 					'value="' + val + '" ' + 
 					'rel="' + this.settings.randomElement  + '" ' +
@@ -254,35 +265,6 @@ $._makeForm = function (set) {
 		return (input == null || input == undefined || input == '' || input == 0) ? true: false;
 	};
 	return this.mform();
-}
-
-$._stripHtml = function (input) {
-	var regexp = new RegExp( /\<[^\<]+\>/g );
-	var output = input.replace(regexp, "");
-	output = output.toLowerCase();
-	return output;
-}
-
-$._stripe = function (set) {
-	this.i = 0;
-	this.set = set;
-	this.go = function (el) {
-		this.removeClasses(el);
-		if(el.getAttribute('style') != "display: none;") {				
-			$(el).addClass(this.set[this.i%this.set.length]);
-			this.i += 1;
-		}
-	}; 
-	this.removeClasses = function (el) {
-		for(var j = 0; j < this.set.length; j++) {
-			if(this.i%this.set.length != j) {
-				$(el).removeClass(this.set[j]);
-			}
-		}
-	};
-	this.reset = function () {
-		this.i = 0;
-	}
 }
 
 })(jQuery);
