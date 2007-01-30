ec1577a69072e42a56e63745b28c5e1c71546779
malsup <malsup@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-01-30 01:41:28 +0000

    Improve modality support.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1228 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/blockUI/jquery.block.js
+++ plugins/blockUI/jquery.block.js
@@ -1,7 +1,7 @@
 /*
  * jQuery blockUI plugin
- * Version 0.8 (01/25/2007)
- * @requires jQuery v1.0
+ * Version 0.9 (01/29/2007)
+ * @requires jQuery v1.1.1
  *
  * Examples at: http://malsup.com/jquery/block/
  * Copyright (c) 2007 M. Alsup
@@ -74,9 +74,9 @@ $.blockUI = function(msg, css) {
         msg = null;
     }
     msg = msg ? (msg.nodeType ? $(msg) : msg) : '<h1>Please wait...</h1>';
-    impl.init(msg, css || {});
+    $.blockUI.impl.init(msg, css || {});
 }; 
-  
+
 /**
  * unblockUI removes the UI block that was put in place by blockUI
  *
@@ -90,27 +90,48 @@ $.blockUI = function(msg, css) {
  * @see blockUI
  */
 $.unblockUI = function(options) {
-    if (impl.g) impl.show(0);
+    if ($.blockUI.impl.g) $.blockUI.impl.show(0);
 };
 
-var impl = {
+$.blockUI.impl = {
     vis: 0, g: 0, m: 0,
     ie6: $.browser.msie && typeof XMLHttpRequest == 'function',
     noalpha: (window.opera && window.opera.version() < 9) || ($.browser.mozilla && /Linux/.test(navigator.platform)),
-    show: function(s) { s ? this.g.show() : this.g.hide(); this.vis = s },
+    show: function(s) { 
+        if (s) {
+            this.g.show();
+            setTimeout(this.focus, 100);
+        }
+        else {
+            this.g.hide(); 
+        }
+        this.vis = s 
+    },
+    focus: function() {
+        var v = $('input:visible', $.blockUI.impl.m)[0];
+        if (v) v.focus();
+    },
     init: function(msg, css) {
-        if (impl.g) {
-            m.empty().append(msg).css(css);
+        if ($.blockUI.impl.g) {
+            $.blockUI.impl.m.empty().append(msg).css(css);
             if (msg.jquery) msg.show();
             return this.show(1);
         }
         $('html,body').css('height','100%');
-        var h = function() { return !impl.vis };
+        var h = function(e) { 
+            if (!$.blockUI.impl.vis) return true;
+            // allow event if target is within the block msg
+            var set = $('*',$.blockUI.impl.m);
+            for (var i=0, max=set.length; i < max; i++)
+                if (set[i] == e.target) return true;
+            return false;
+        };
         $().bind('keypress',h).bind('keydown',h).bind('mousedown',h);
 
         var f = $('<iframe style="z-index:1000;background-color:#fff;border:none"></iframe>');
         var w = $('<div style="z-index:2000;cursor:wait"></div>');
-            m = $('<div id="blockUI" style="z-index:3000;cursor:wait;padding:0;position:fixed;top:50%;left:50%;width:250px;margin:-50px 0 0 -125px;text-align:center;background-color:#fff;border:3px solid #aaa"></div>');
+        m = $('<div id="blockUI" style="z-index:3000;cursor:wait;padding:0;position:fixed;top:50%;left:50%;width:250px;margin:-50px 0 0 -125px;text-align:center;background-color:#fff;border:3px solid #aaa"></div>');
+        $.blockUI.impl.m = m;
         $([f[0],w[0]]).css({position:'fixed',width:'100%',height:'100%',top:'0',left:'0'});
         this.g  = $([f[0],w[0],m[0]]).appendTo('body');
         m.append(msg).css(css);
@@ -132,6 +153,7 @@ var impl = {
                 }
             });
         }
+        this.show(1);
     }
 };
 
