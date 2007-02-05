8f9030a74166b0cc644419122eabb0d849d4cc28
joern <joern@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-02-05 19:26:39 +0000

    Moved/renamed searchField plugin
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1282 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ plugins/searchField/jquery.searchField.js
@@ -0,0 +1,37 @@
+/*
+ * searchField - jQuery plugin to display and remove
+ * a default value in a searchvalue on blur/focus
+ *
+ * Copyright (c) 2006 Jörn Zaefferer
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ *
+ */
+
+/**
+ * Clear the help text in a search field when focused,
+ * a restore it on blur if nothing was entered.
+ *
+ * @example $('#quicksearch').searchField();
+ * @before <input id="quicksearch" value="Enter search here" name="quicksearch" />
+ *
+ * @name searchField
+ * @type jQuery
+ * @cat Plugins/Utilities
+ * @author Jörn Zaefferer (http://bassistance.de)
+ */
+$.fn.searchField = function() {
+	return this.focus(function() {
+		// remove default value, but nothing else
+		if( this.value == this.defaultValue ) {
+			this.value = "";
+		}
+	}).blur(function() {
+		// check for length, for value == 0
+		if( !this.value.length ) {
+			this.value = this.defaultValue;
+		}
+	});
+};
