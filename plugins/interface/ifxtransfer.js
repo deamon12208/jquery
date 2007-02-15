f908ca8178da72f7f57d23a8c6f6774cf9d4e82b
stefan <stefan@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-02-15 11:22:51 +0000

    Changed to allow transfer to multiple elements and from multiple elements. Animation is not queued anymore.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1344 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/interface/ifxtransfer.js
+++ plugins/interface/ifxtransfer.js
@@ -28,71 +28,61 @@ jQuery.transferHelper = null;
  */
 jQuery.fn.TransferTo = function(o)
 {
-	return this.queue('interfaceFX', function(){
-		new jQuery.fx.itransferTo(this, o);
+	return this.each(function(){
+		if(!o || !o.to) {
+			return;
+		}
+		var el = this;
+		jQuery(o.to).each(function() {
+			new jQuery.fx.itransferTo(el, this, o);
+		});
 	});
 };
-jQuery.fx.itransferTo = function(e, o)
+jQuery.fx.itransferTo = function(e, targetEl, o)
 {
-	
-	if(jQuery.transferHelper == null)
-	{
-		jQuery('body', document).append('<div id="transferHelper"></div>');
-		jQuery.transferHelper = jQuery('#transferHelper');
-	}
-	jQuery.transferHelper.css('display', 'block').css('position', 'absolute');
-	
 	var z = this;
 	z.el = jQuery(e);
-	if(!o || !o.to) {
-		return;
-	}
-	
-	if (o.to.constructor == String && document.getElementById(o.to)) {
-		o.to = document.getElementById(o.to);
-	} else if ( !o.to.childNodes ) {
-		return;
-	}
+	z.targetEl = targetEl;
+	z.transferEl = document.createElement('div');
+	jQuery(z.transferEl)
+		.css({
+			position: 'absolute'
+		}).addClass(o.className);
 	
 	if (!o.duration) {
 		o.duration = 500;
 	}
 	z.duration = o.duration;
-	z.to = o.to;
-	z.classname = o.className;
 	z.complete = o.complete;
-	if (z.classname) {
-		jQuery.transferHelper.addClass(z.classname);
-	}
 	z.diffWidth = 0;
 	z.diffHeight = 0;
 	
 	if(jQuery.boxModel) {
-		z.diffWidth = (parseInt(jQuery.transferHelper.css('borderLeftWidth')) || 0 )
-					+ (parseInt(jQuery.transferHelper.css('borderRightWidth')) || 0)
-					+ (parseInt(jQuery.transferHelper.css('paddingLeft')) || 0)
-					+ (parseInt(jQuery.transferHelper.css('paddingRight')) || 0);
-		z.diffHeight = (parseInt(jQuery.transferHelper.css('borderTopWidth')) || 0 )
-					+ (parseInt(jQuery.transferHelper.css('borderBottomWidth')) || 0)
-					+ (parseInt(jQuery.transferHelper.css('paddingTop')) || 0)
-					+ (parseInt(jQuery.transferHelper.css('paddingBottom')) || 0);
+		z.diffWidth = (parseInt(jQuery.curCSS(z.transferEl, 'borderLeftWidth')) || 0 )
+					+ (parseInt(jQuery.curCSS(z.transferEl, 'borderRightWidth')) || 0)
+					+ (parseInt(jQuery.curCSS(z.transferEl, 'paddingLeft')) || 0)
+					+ (parseInt(jQuery.curCSS(z.transferEl, 'paddingRight')) || 0);
+		z.diffHeight = (parseInt(jQuery.curCSS(z.transferEl, 'borderTopWidth')) || 0 )
+					+ (parseInt(jQuery.curCSS(z.transferEl, 'borderBottomWidth')) || 0)
+					+ (parseInt(jQuery.curCSS(z.transferEl, 'paddingTop')) || 0)
+					+ (parseInt(jQuery.curCSS(z.transferEl, 'paddingBottom')) || 0);
 	}
 	z.start = jQuery.extend(
 		jQuery.iUtil.getPosition(z.el.get(0)),
 		jQuery.iUtil.getSize(z.el.get(0))
 	);
 	z.end = jQuery.extend(
-		jQuery.iUtil.getPosition(z.to),
-		jQuery.iUtil.getSize(z.to)
+		jQuery.iUtil.getPosition(z.targetEl),
+		jQuery.iUtil.getSize(z.targetEl)
 	);
 	z.start.wb -= z.diffWidth;
 	z.start.hb -= z.diffHeight;
 	z.end.wb -= z.diffWidth;
 	z.end.hb -= z.diffHeight;
 	z.callback = o.complete;
-
+	jQuery('body').append(z.transferEl);
 	// Execute the transfer
-	jQuery.transferHelper
+	jQuery(z.transferEl)
 		.css('width', z.start.wb + 'px')
 		.css('height', z.start.hb + 'px')
 		.css('top', z.start.y + 'px')
@@ -107,17 +97,12 @@ jQuery.fx.itransferTo = function(e, o)
 			z.duration,
 			function()
 			{
-				// Set correct classname
-				if(z.classname)
-					jQuery.transferHelper.removeClass(z.classname);
-				jQuery.transferHelper.css('display', 'none');
+				jQuery(z.transferEl).remove();
 	
 				// Callback
 				if (z.complete && z.complete.constructor == Function) {
 					z.complete.apply(z.el.get(0), [z.to]);
 				}
-				// Done
-				jQuery.dequeue(z.el.get(0), 'interfaceFX');
 			}
 		);
 };
