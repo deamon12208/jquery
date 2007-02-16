867be2832643e12fda0d5134dfcf8de834a6d09c
john <john@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-02-16 01:15:33 +0000

    Added a plugin that provides OpenAjax support for jQuery.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1354 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ plugins/openajax/lib/jQuery/openajax.js
@@ -0,0 +1,13 @@
+/*
+ * Adds OpenAjax Alliance Support to jQuery
+ *
+ * Include this plugin in a page to hook jQuery into the OpenAjax platform.
+ * For example:
+ *   <script src="jquery.js"></script>
+ *   <script src="openajax.js"></script>
+ */
+
+if ( typeof OpenAjax != "undefined" ) {
+	OpenAjax.registerLibrary("jQuery", "http://jquery.com/", "1.1");
+	OpenAjax.registerGlobals("jQuery", ["jQuery"]);
+}
