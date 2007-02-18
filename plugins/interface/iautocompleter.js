ede2d2dc470b3f6ec5efe9e66d112442064f1fa9
stefan <stefan@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-02-18 08:48:15 +0000

    Fixed pressed key detection
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1368 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/interface/iautocompleter.js
+++ plugins/interface/iautocompleter.js
@@ -302,7 +302,7 @@ jQuery.iAuto = {
 		var subject = jQuery.iAuto.getFieldValues(this);
 				
 		var pressedKey = e.charCode || e.keyCode || -1;
-		if (/13|27|35|36|38|40|9/.test(pressedKey) && jQuery.iAuto.items) {
+		if (/^13$|27$|35$|36$|38$|40$|^9$/.test(pressedKey) && jQuery.iAuto.items) {
 			if (window.event) {
 				window.event.cancelBubble = true;
 				window.event.returnValue = false;
