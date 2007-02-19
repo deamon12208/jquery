d5a6d8a9df0af2f75d21bc726579f29246e54e53
john <john@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-02-19 06:17:20 +0000

    Added support for passing window/document onload/onunload events back to OpenAjax.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1380 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/openajax/lib/jQuery/openajax.js
+++ plugins/openajax/lib/jQuery/openajax.js
@@ -10,4 +10,17 @@
 if ( typeof OpenAjax != "undefined" ) {
 	OpenAjax.registerLibrary("jQuery", "http://jquery.com/", "1.1");
 	OpenAjax.registerGlobals("jQuery", ["jQuery"]);
+
+	var old_add = jQuery.event.add;
+
+	jQuery.event.add = function( element, type, handler ) {
+		if ( element == window || element == document ) {
+			if ( type == "load" )
+				return OpenAjax.addOnLoad( handler );
+			else if ( type == "unload" )
+				return OpenAjax.addOnUnload( handler );
+		}
+
+		return old_add.apply( this, arguments );
+	};
 }
