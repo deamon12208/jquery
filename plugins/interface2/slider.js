71778e542fc8fbd0357b9bb0b6b33c8b657047e8
stefan <stefan@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-04-12 05:14:51 +0000

    Improved some performance
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1669 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/interface2/slider.js
+++ plugins/interface2/slider.js
@@ -11,10 +11,10 @@
 						//end
 						case 35:
 							if (this.DB.axis != 'y') {
-								position.x = this.DB.limitOffset.x + this.DB.limitOffset.w - this.DB.size.wb;
+								position.x = this.DB.limitOffset.x + this.DB.limitOffset.w;
 							}
 							if (this.DB.axis != 'x') {
-								position.y = this.DB.limitOffset.y + this.DB.limitOffset.h - this.DB.size.hb;
+								position.y = this.DB.limitOffset.y + this.DB.limitOffset.h;
 							}
 							break;
 						//home
@@ -38,7 +38,7 @@
 						//down
 						case 40:
 							if (this.DB.axis != 'x') {
-								position.y = Math.min(this.DB.limitOffset.y + this.DB.limitOffset.h - this.DB.size.hb,
+								position.y = Math.min(this.DB.limitOffset.y + this.DB.limitOffset.h,
 												this.DB.ticks && ( this.DB.ticksOn || e.ctrlKey||e.shiftKey||e.altKey) ?
 												(parseInt(position.y/this.DB.grid.y,10) + 1) * this.DB.grid.y
 												:  position.y + 1);
@@ -56,7 +56,7 @@
 						//right
 						case 39:
 							if (this.DB.axis != 'y') {
-								position.x = Math.min(this.DB.limitOffset.x + this.DB.limitOffset.w - this.DB.size.wb,
+								position.x = Math.min(this.DB.limitOffset.x + this.DB.limitOffset.w,
 												this.DB.ticks && ( this.DB.ticksOn || e.ctrlKey||e.shiftKey||e.altKey) ?
 												(parseInt(position.x/this.DB.grid.x,10) + 1) * this.DB.grid.x
 												:  position.x + 1);
@@ -76,8 +76,8 @@
 					left: position.x + 'px',
 					top: position.y + 'px'
 				});
-				var xproc = parseInt((position.x/(this.DB.maximum.w - this.DB.size.wb))*100, 10)||0;
-				var yproc = parseInt((position.y/(this.DB.maximum.h - this.DB.size.hb))*100, 10)||0;
+				var xproc = parseInt((position.x/this.DB.maximum.w)*100, 10)||0;
+				var yproc = parseInt((position.y/this.DB.maximum.h)*100, 10)||0;
 				
 				this.DB.onSlide.apply(
 					this,
@@ -96,9 +96,17 @@
 					y: this.DB.draggedEls.offsetTop
 				};
 				this.DB.maximum = {
-					w: $.iUtil.getInnerWidth(this),
-					h: $.iUtil.getInnerHeight(this)
+					w: $.iUtil.getInnerWidth(this) - this.DB.size.wb,
+					h: $.iUtil.getInnerHeight(this) - this.DB.size.hb
 				};
+				/*if (this.DB.ticks) {
+					this.DB.grid = {
+						x: this.DB.maximum.w/this.DB.ticks,
+						y: this.DB.maximum.h/this.DB.ticks
+					};
+					this.DB.maximum.w = parseInt(Math.floor(this.DB.maximum.w/this.DB.grid.x)*this.DB.grid.x, 10);
+					this.DB.maximum.h = parseInt(Math.floor(this.DB.maximum.h/this.DB.ticks)*this.DB.ticks, 10);
+				}*/
 				this.DB.limitOffset = {
 					x: 0,
 					y: 0,
@@ -113,10 +121,10 @@
 					nextThumb = this.DB.thumbs.get(this.DB.currentThumb + 1);
 					if (nextThumb) {
 						if (this.DB.axis != 'y') {
-							this.DB.limitOffset.w = nextThumb.offsetLeft + this.DB.size.wb;
+							this.DB.limitOffset.w = nextThumb.offsetLeft;
 						}
 						if (this.DB.axis != 'x') {
-							this.DB.limitOffset.h = nextThumb.offsetTop + this.DB.size.hb;
+							this.DB.limitOffset.h = nextThumb.offsetTop;
 						}
 					}
 					prevThumb = this.DB.thumbs.get(this.DB.currentThumb - 1);
@@ -154,13 +162,13 @@
 						el.DB.limitOffset.x,
 						Math.min(
 							el.DB.offset.x + delta.x,
-							el.DB.limitOffset.x + el.DB.limitOffset.w - el.DB.size.wb
+							el.DB.limitOffset.x + el.DB.limitOffset.w
 						)) - el.DB.offset.x,
 					y: Math.max(
 						el.DB.limitOffset.y,
 						Math.min(
 							el.DB.offset.y + delta.y,
-							el.DB.limitOffset.y + el.DB.limitOffset.h - el.DB.size.hb
+							el.DB.limitOffset.y + el.DB.limitOffset.h
 						))-el.DB.offset.y
 				};
 			},
@@ -190,8 +198,8 @@
 					x: this.DB.draggedEls.offsetLeft,
 					y: this.DB.draggedEls.offsetTop
 				};
-				var xproc = parseInt((thumbOffset.x/(this.DB.maximum.w - this.DB.size.wb))*100, 10)||0;
-				var yproc = parseInt((thumbOffset.y/(this.DB.maximum.h - this.DB.size.hb))*100, 10)||0;
+				var xproc = parseInt((thumbOffset.x/this.DB.maximum.w)*100, 10)||0;
+				var yproc = parseInt((thumbOffset.y/this.DB.maximum.h)*100, 10)||0;
 				
 				this.DB.onStop.apply(
 					this,
@@ -321,8 +329,8 @@
 				};
 				el.DB.thumbs.each(function(nr){
 					values[values.length] = {
-						x: parseInt((this.offsetLeft / (el.DB.maximum.w - this.offsetWidth)) * 100, 10)||0,
-						y: parseInt((this.offsetTop / (el.DB.maximum.h - this.offsetHeight)) * 100, 10)||0
+						x: parseInt((this.offsetLeft / el.DB.maximum.w) * 100, 10)||0,
+						y: parseInt((this.offsetTop / el.DB.maximum.h) * 100, 10)||0
 					};
 				});
 			}
