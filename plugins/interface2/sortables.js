fd3c3e1cd585fb5e634a723f05cdfe6a1bbae830
stefan <stefan@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-04-17 15:34:38 +0000

    Added ghostly option to sortables
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1726 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/interface2/sortables.js
+++ plugins/interface2/sortables.js
@@ -4,16 +4,14 @@
 		dragOptions = $.extend({
 			startDrag : function() {
 					if (this.DB.proxy) {
-						var parOff = $.iUtil.getPosition(this.offsetParent);
-						this.DB.offset = {
-							x : this.DB.position.x - parOff.x,
-							y : this.DB.position.y - parOff.y
-						};
-						$(this.DB.proxy).css({
+						this.DB.ghost = this.DB.proxy;
+						this.DB.proxy = false;
+						$(this.DB.ghost).css({
 							position: 'absolute',
-							left: this.DB.offset.x + 'px',
-							top: this.DB.offset.y + 'px',
-							display: ''
+							left: this.offsetLeft + 'px',
+							top: this.offsetTop + 'px',
+							display: '',
+							backgroundColor: '#09f'
 						});
 					}
 					this.DB.changed = false;
@@ -21,7 +19,7 @@
 						x: $.DDM.pointer.x - this.DB.position.x,
 						y: $.DDM.pointer.y - this.DB.position.y
 					};
-					this.DB.onStart.apply(this.DB.draggedEls, [this.DB.proxy||this.DB.draggedEls, $.DDM.dragged.DB.targets]);
+					this.DB.onStart.apply(this, [this, $.DDM.dragged.DB.targets]);
 					return false;
 				},
 			stopDrag : function() {
@@ -29,18 +27,24 @@
 					top: 0,
 					left: 0
 				});
+				if (this.DB.ghost) {
+					$(this.DB.ghost).remove();
+					this.DB.ghost = false;
+					this.DB.proxy = true;
+				}
 				if (this.DB.changed == true) {
 					this.DB.changed = false;
 					this.DB.lastSortable.DrB.onUpdate.apply(this.DB.lastSortable,[this]);
 				}
 			},
 			drag : function() {
-				var el = $.DDM.dragged.DB.lastSortable, after, before, position = false, overlaped = false;
-				if (el && $.DDM.dragged.DB.overSortable) {
+				var dragged = $.DDM.dragged;
+				var el = dragged.DB.lastSortable, after, before, position = false, overlaped = false;
+				if (el && dragged.DB.overSortable) {
 					var scrollT = el.scrollTop;
 					el.DrB.subjects.each(function(nr){
 						if (el.DrB.positions[nr] && overlaped === false) {
-							var overlap = ($.DDM.pointer.y - el.DrB.positions[nr].y + scrollT - $.DDM.dragged.DB.cusorOffset.y) / el.DrB.positions[nr].h;
+							var overlap = ($.DDM.pointer.y - el.DrB.positions[nr].y + scrollT - dragged.DB.cusorOffset.y) / el.DrB.positions[nr].h;
 							if (overlap >= 0 && overlap <= 1) {
 								overlaped = true;
 								if (overlap > 0.5) {
@@ -52,24 +56,30 @@
 						}
 					});
 					if (after || before) {
-						if (after && after.nextSibling != $.DDM.dragged.DB.draggedEls) {
-							$(after).after($.DDM.dragged);
+						if (after && after.nextSibling != dragged.DB.draggedEls) {
+							$(after).after(dragged);
 							position = true;
-						} else if (before && before.previousSibling != $.DDM.dragged.DB.draggedEls) {
-							$(before).before($.DDM.dragged);
+						} else if (before && before.previousSibling != dragged.DB.draggedEls) {
+							$(before).before(dragged);
 							position = true;
 						}
 						if (position) {
-							$($.DDM.dragged.DB.draggedEls).css({
+							$(dragged).css({
 								top: 0,
 								left: 0
 							});
-							$.DDM.dragged.DB.changed = true;
-							$.DDM.dragged.DB.position = $.iUtil.getPosition($.DDM.dragged.DB.draggedEls);
-							$.DDM.dragged.DB.pointer = $.DDM.dragged.DB.position;
-							$.DDM.dragged.DB.pointer.x += $.DDM.dragged.DB.cusorOffset.x;
-							$.DDM.dragged.DB.pointer.y += $.DDM.dragged.DB.cusorOffset.y;
-							$.DDM.dragged.DB.beforeDrag.apply($.DDM.dragged);
+							if (dragged.DB.ghost) {
+								$(dragged.DB.ghost).css({
+									left: dragged.offsetLeft + 'px',
+									top: dragged.offsetTop + 'px'
+								});
+							}
+							dragged.DB.changed = true;
+							dragged.DB.position = $.iUtil.getPosition(dragged.DB.draggedEls);
+							dragged.DB.pointer = dragged.DB.position;
+							dragged.DB.pointer.x += dragged.DB.cusorOffset.x;
+							dragged.DB.pointer.y += dragged.DB.cusorOffset.y;
+							dragged.DB.beforeDrag.apply(dragged);
 							el.DrB.onChange.apply(el,[this]);
 						}
 					}
@@ -81,18 +91,28 @@
 		
 		dropOptions = $.extend({
 			beforeHover : function() {
-				if ($.DDM.dragged.DB.lastSortable != this) {
-					$.DDM.dragged.DB.changed = true;
-					$.DDM.dragged.DB.lastSortable = this;
-					var el = this;
-					$(el).append($.DDM.dragged.DB.draggedEls);
-					$($.DDM.dragged.DB.draggedEls).css({
+				var dragged = $.DDM.dragged;
+				var el = this;
+				if ($.DDM.dragged.DB.lastSortable !=el) {
+					dragged.DB.changed = true;
+					dragged.DB.lastSortable = this;
+					$(el).append(dragged);
+					$(dragged.DB.draggedEls).css({
 						top: 0,
 						left: 0
 					});
+					if (dragged.DB.ghost) {
+						$(dragged).after(dragged.DB.ghost);
+						$(dragged.DB.ghost).css({
+							left: dragged.offsetLeft + 'px',
+							top: dragged.offsetTop + 'px'
+						});
+					}
+					el.DrB.size.wb = el.offsetWidth;
+					el.DrB.size.hb = el.offsetHeight;
 					el.DrB.positions = [];
 					el.DrB.subjects = $(el.DrB.accept, el).each(function(nr){
-						if (this != $.DDM.dragged.DB.draggedEls && this != $.DDM.dragged.DB.proxy) {
+						if (this != dragged.DB.draggedEls && this != dragged.DB.proxy) {
 							el.DrB.positions[nr] = {
 								x: this.offsetLeft + el.DrB.position.x,
 								y: this.offsetTop + el.DrB.position.y,
@@ -101,27 +121,13 @@
 							};
 						}
 					});
-					$.DDM.dragged.DB.position = $.iUtil.getPosition($.DDM.dragged.DB.draggedEls);
-					if ($.DDM.dragged.DB.proxy) {
-						$.DDM.dragged.DB.offset = {
-							x : $.DDM.dragged.DB.position.x - el.DrB.position.x,
-							y : $.DDM.dragged.DB.position.y - el.DrB.position.y
-						};
-						$($.DDM.dragged.DB.proxy).css({
-							position: 'absolute',
-							left: $.DDM.dragged.DB.offset.x + 'px',
-							top: $.DDM.dragged.DB.offset.y + 'px',
-							display: ''
-						});
-					} else {
-					}
-					this.DrB.size = $.iUtil.getSize(this);
-					$.DDM.dragged.DB.pointer = $.DDM.dragged.DB.position;
-					$.DDM.dragged.DB.pointer.x += $.DDM.dragged.DB.cusorOffset.x;
-					$.DDM.dragged.DB.pointer.y += $.DDM.dragged.DB.cusorOffset.y;
-					el.DrB.onChange.apply(el,[$.DDM.dragged]);
+					dragged.DB.position = $.iUtil.getPosition(dragged.DB.draggedEls);
+					dragged.DB.pointer = dragged.DB.position;
+					dragged.DB.pointer.x += dragged.DB.cusorOffset.x;
+					dragged.DB.pointer.y += dragged.DB.cusorOffset.y;
+					el.DrB.onChange.apply(el,[dragged]);
 				}
-				$.DDM.dragged.DB.overSortable = true;
+				dragged.DB.overSortable = true;
 			},
 			beforeOut : function() {
 				$.DDM.dragged.DB.overSortable = false;
@@ -145,8 +151,6 @@
 	};
 	$.fn.sortableSerialize = function(attr, strip) {
 		var hash = {};
-		if (!strip)
-			strip = '';
 		this.each(function(){
 			if (this.DrB && this.DrB.sortable) {
 				var cur = [];
