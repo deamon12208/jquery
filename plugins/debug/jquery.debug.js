e2f85547a31672b36df5208b07771d90d768b60f
jakecigar <jakecigar@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-05-22 23:53:02 +0000

    more  firebug-like functionality.
    added console.time & console.timeEnd
    
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1960 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/debug/jquery.debug.js
+++ plugins/debug/jquery.debug.js
@@ -4,19 +4,20 @@ var DEBUG = true;
 		if (!("console" in window) || !("firebug" in console)){
 			if (DEBUG)
 				$('<div id="DEBUG" style ="background-color:#c1ebeb;position: fixed;left: 0px;height: 0px;overflow: auto;bottom: 0px;z-index: 1000;width: 100%;border-top: 5px solid #0000FF"><ol></ol></div>')
-				.attr('title','Click to show; Shift Click to hide')
-				.click(function(e){$(this).height( e.shiftKey ?0 : 100)})
+				.attr('title','Click to show, shift click to hide the DEBUG div')
+				.click(function(e){$(this).animate({height: e.shiftKey ?0 : 100}, "slow");this.scrollTop = this.scrollHeight})
 				.appendTo(document.body);
 		}
 	});
 	var debug = function(msg){ $('#DEBUG ol').append( '<li>' + msg + '</li>' )};
 	$.fn.debug = function(message) {
 		if (DEBUG) {
-			$.log.apply(this,[(message || 'debug') + ':',this]);
+			$.log.apply(this,[txt(message) + ':',this]);
 			$("#DEBUG").each(function() {this.scrollTop = this.scrollHeight})
 		}
 		return this
 	};
+	$.fn.debug.version = '0.1';
 	$.log =$.fn.log = function() {
 		if(DEBUG )
 			if("console" in window && 'firebug' in console)
@@ -28,8 +29,12 @@ var DEBUG = true;
 		return this
 	};
 	if (!(("console" in window) && (("open" in console)||("firebug" in console)))){
-		window.console = {};
-		$.each(("log,debug,info,warn,error,assert,dir,dirxml,group,groupEnd,time,timeEnd,count,trace,profile,profileEnd".split(/,/)), function(i,o){
+		window.console = 
+		{timer:{}
+		,time:function(timer) {console.timer[timer] = new Date()}
+		,timeEnd:function(timer) {console.timer[timer] && $.log('timer',timer + ':' , (new Date()-console.timer[timer]) , 'ms');delete console.timer[timer]}
+		};
+		$.each("log,debug,info,warn,error,assert,dir,dirxml,group,groupEnd,count,trace,profile,profileEnd".split(/,/), function(i,o){
 			window.console[o] = $.log
 		})
 	};
@@ -50,7 +55,7 @@ var DEBUG = true;
 		if (tagShow) res+= ">";
 		if (obj.nodeType == 8)
 			res += "<!-- " + obj.nodeValue + " -->";
-			 if (obj.nodeValue != null)
+		if (obj.nodeValue != null)
 			res +=  obj.nodeValue;
 		if (obj.hasChildNodes && obj.hasChildNodes())
 			res += $.map(obj.childNodes,function(child){return $.xhtml(child)}).join('');
@@ -65,6 +70,11 @@ var DEBUG = true;
 			return "'" + s.substr(0,8).replace("'","\\'") + "…'";
 		return  '"' + s.replace('"','\\"') + '"'
 	};
+	var txt = function(obj){
+		if (typeof obj != "object")
+			return obj || 'debug';
+		return tiv(obj,jsO(obj,false))
+	};
 	var tiv = function(obj,title) {
 		var $obj = $(obj);
 		var res = $obj[0].tagName.toLowerCase();
@@ -74,7 +84,7 @@ var DEBUG = true;
 		return "<span title='" + title + "'>" +res + "</span>"
 	};
 	var jsO = function(obj,bare) {
-		if (typeof obj != "object") 
+		if (typeof obj != "object")
 			return bare || typeof obj != "string" ? obj : '"' + obj.toString().replace('"','\\"') + '"';
 		if (obj.nodeName)
 			return $.xhtml(obj).replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
