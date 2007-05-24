7294721b6ab128407ace9adb1d98e29e5d6be4cc
brandon.aaron <brandon.aaron@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-05-24 13:25:25 +0000

    adding fxqueue plugin to svn
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1969 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ plugins/fxqueue/jquery.fxqueue.pack.js
@@ -0,0 +1,7 @@
+/* Copyright (c) 2006 Brandon Aaron (http://brandonaaron.net || brandon.aaron@gmail.com)
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) 
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ *
+ * Version: TOTALLY ALPHA
+ */
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(2($){$.g.1=2(b,e,c,d){6 i.p(2(){9 a=2(){$.1.7();5(d)d()}$.1.3.q([i,b,e,c,a]);$.1.h()})};$.1={3:[],4:0,7:2(){5(!$.1.3[$.1.4])6;9 a=$($.1.3[$.1.4].o()),8=$.1.3[$.1.4];$.g.n.m(a,8);$.1.4++},h:2(){5($.1.f)6;l $.1.f=k;$.1.7()}}})(j);',27,27,'|fxqueue|function|queue|current|if|return|next|args|var||||||playing|fn|play|this|jQuery|true|else|apply|animate|shift|each|push'.split('|'),0,{}))
