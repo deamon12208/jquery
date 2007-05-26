77f738c4c68d3d30fe10809d0e0af69174983f1c
wycats <wycats@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-05-26 00:50:38 +0000

    Moving jQuery on Rails into a demo app
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@1984 c715fcbe-d12f-0410-84c4-316a508785bb

--- tools/jquery_on_rails/init.rb
+++
@@ -1,8 +0,0 @@
-# Include hook code here
-gem 'hpricot', '0.5.142'
-require 'hpricot'
-require 'jquery_on_rails'
-require 'jquery_helpers'
-require 'jqor_control'
-JQueryOnRails::JQUERY_LIBRARY = ["jquery", "interface", "form", "dimensions"]
-ActionView::Base.send :include, JQueryHelper
