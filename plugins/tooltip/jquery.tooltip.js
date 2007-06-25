1777232d76005785aae678295475968bcadc1758
joern.zaefferer <joern.zaefferer@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-06-25 22:20:32 +0000

    cleaning up to get 1.1 out
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2158 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/tooltip/jquery.tooltip.js
+++ plugins/tooltip/jquery.tooltip.js
@@ -1,5 +1,5 @@
 /*
- * Tooltip 2.0 Alpha - jQuery plugin  for styled tooltips
+ * Tooltip 1.1 alpha - jQuery plugin for styled tooltips
  *
  * Copyright (c) 2006 Jörn Zaefferer, Stefan Petre
  *
@@ -13,11 +13,10 @@
 
 /*
 TODO
-- more testing need for Modaltip and Foottip, maybe rename both
 - provide a nice approach to show URLs only for external links
 - change p.body and p.url to div.body and div.url
 - finally: release 2.0, being as backwards compatible as possible
-- build example similar to http://codylindley.com/blogstuff/js/tooltip/tooltip.htm
+- build example similar to http://codylindley.com/blogstuff/js/tooltip/tooltip.htm or http://g-academy.tweak.se/
 */
 
 /**
@@ -105,50 +104,6 @@ TODO
 				})
 				.bind("mouseover", save)
 				.bind(settings.event, handle);
-		},
-		Foottip: function(settings) {
-			this.each(function() {
-				$($(this).attr('href')).hide();
-			});
-			settings = $.extend({
-				bodyHandler: function() {
-					return $($(this).attr('href')).html();
-				},
-				showURL: false
-			}, settings);
-			return this.Tooltip(settings).click(function() {
-				return false;
-			});
-		},
-		Modaltip: function(settings) {
-			settings = $.extend({
-				drag: "h3"
-			}, settings);
-			return this.click(function() {
-				var self = this;
-				blocked = true;
-				helper.parent
-				.addClass("modal")
-				.append("<span class='resize' />")
-				.append("<span class='close' />")
-				.jqDrag(settings.drag)
-				.jqResize("span.resize")
-				.click(function(event) {
-					event.stopPropagation();
-				}).find("span.close").add(document.body).one('click', function(event) {
-					blocked = false;
-					helper.parent
-					.unbind('click')
-					.css({
-						height: "",
-						width: ""
-					})
-					.removeClass("modal")
-					.find("span.resize, span.close").remove();
-					hide.call(self, event);
-				});
-				return false;
-			});
 		},
 		fixPNG: IE ? function() {
 			return this.each(function () {
@@ -308,7 +263,6 @@ TODO
 				top: top + 'px'
 			});
 		}
-		
 		var v = viewport(),
 			h = helper.parent[0];
 		// check horizontal position
@@ -378,33 +332,4 @@ TODO
 		extraClass: ""
 	};
 
-})(jQuery);
-
-/*
- * jqDnR - Minimalistic Drag'n'Resize for jQuery.
- *
- * Copyright (c) 2007 Brice Burgess <bhb@iceburg.net>, http://www.iceburg.net
- * Licensed under the MIT License:
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * $Version: 2007.02.09 +r1
- */
-(function($){
-$.fn.jqDrag=function(r){$.jqDnR.init(this,r,'d'); return this;};
-$.fn.jqResize=function(r){$.jqDnR.init(this,r,'r'); return this;};
-$.jqDnR={
-init:function(w,r,t){ r=(r)?$(r,w):w;
-	r.bind('mousedown',{w:w,t:t},function(e){ var h=e.data; var w=h.w;
-	hash=$.extend({oX:f(w,'left'),oY:f(w,'top'),oW:f(w,'width'),oH:f(w,'height'),pX:e.pageX,pY:e.pageY,o:w.css('opacity')},h);
-	h.w.css('opacity',0.8); $().mousemove($.jqDnR.drag).mouseup($.jqDnR.stop);
-	return false;});
-},
-drag:function(e) {var h=hash; var w=h.w[0];
-	if(h.t == 'd') h.w.css({left:h.oX + e.pageX - h.pX,top:h.oY + e.pageY - h.pY});
-	else h.w.css({width:Math.max(e.pageX - h.pX + h.oW,0),height:Math.max(e.pageY - h.pY + h.oH,0)});
-	return false;},
-stop:function(){var j=$.jqDnR; hash.w.css('opacity',hash.o); $().unbind('mousemove',j.drag).unbind('mouseup',j.stop);},
-h:false};
-var hash=$.jqDnR.h;
-var f=function(w,t){return parseInt(w.css(t)) || 0};
 })(jQuery);
