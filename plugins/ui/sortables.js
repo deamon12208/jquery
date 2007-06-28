f5ea10659ac992a5d6ca319fa99854f82cd600b1
paul.bakaus <paul.bakaus@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-06-28 13:22:10 +0000

    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2172 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/ui/sortables.js
+++ plugins/ui/sortables.js
@@ -27,8 +27,14 @@
 		});
 		o = this.options;
 		
+		//Get the items
+		var items = $('> '+o.items, el);
+		
+		//Let's determine the floating mode
+		o.floating = items.css('float') == 'left' || items.css('float') == 'right' ? true : false;
+		
 		//Make draggable
-		$('> '+o.items, el).draggable({
+		items.draggable({
 			revert: true,
 			helper: 'clone',
 			onStart: function(el) {
@@ -49,35 +55,64 @@
 						for(var i=0;i<m.length;i++) {
 							
 							var cO = $(m[i][0]).offset({ border: false });
-							var overlap = ((cO.top - (this.rpos[1]-this.options.cursorAt.top))/m[i][0].offsetHeight);
-
-							//Don't execute if not touching the sortables in horizontal direction
-							if(!(cO.left < this.rpos[0]-this.options.cursorAt.left + m[i][0].offsetWidth/2 && cO.left + m[i][0].offsetWidth > this.rpos[0]-this.options.cursorAt.left + m[i][0].offsetWidth/2)) return;
-
-							//Caching...
-							if(self.curTarget == m[i][0]) continue;
-							if($.ui.intersect(this,{ offset: cO, item: { element: m[i][0] } }, 'intersect')) {
-								self.curTarget = m[i][0];
-							}
-
-							if(overlap >= 0 && overlap <= 0.5) { //Overlapping at top	
+							
+							if(o.floating) {
 								
-								if(!$(m[i][0]).prev().length) { //If it's the first, move it to the top
-									$(m[i][0]).before(this.element);
-								} else {
-									$(m[i][0]).after(this.element);
+								var overlapX = ((cO.left - (this.rpos[0]-this.options.cursorAt.left))/this.helper.offsetWidth);
+
+								if(!(cO.top < this.rpos[1]-this.options.cursorAt.top + m[i][0].offsetHeight/2 && cO.top + m[i][0].offsetHeight > this.rpos[1]-this.options.cursorAt.top + m[i][0].offsetHeight/2)) continue;
+	
+								if(overlapX >= 0 && overlapX <= 0.5) { //Overlapping at left	
+									
+									if(!$(m[i][0]).prev().length) { //If it's the first, move it to the top
+										$(m[i][0]).before(this.element);
+									} else {
+										$(m[i][0]).after(this.element);
+									}
 								}
+	
+								if(overlapX < 0 && overlapX > -0.5) { //Overlapping at right
+	
+									if($(m[i][0]).next()[0] == this.element) {
+										$(m[i][0]).before(this.element);
+									} else {
+										$(m[i][0]).after(this.element);
+									}
+	
+								}								
+								
+							} else {
+
+								var overlap = ((cO.top - (this.rpos[1]-this.options.cursorAt.top))/this.helper.offsetHeight);
+							
+								//Don't execute if not touching the sortables in horizontal direction
+								if(!(cO.left < this.rpos[0]-this.options.cursorAt.left + m[i][0].offsetWidth/2 && cO.left + m[i][0].offsetWidth > this.rpos[0]-this.options.cursorAt.left + m[i][0].offsetWidth/2)) continue;
+	
+								//TODO: Cache this step!
+	
+								if(overlap >= 0 && overlap <= 0.5) { //Overlapping at top	
+									
+									if(!$(m[i][0]).prev().length) { //If it's the first, move it to the top
+										$(m[i][0]).before(this.element);
+									} else {
+										$(m[i][0]).after(this.element);
+									}
+								}
+	
+								if(overlap < 0 && overlap > -0.5) { //Overlapping at bottom
+	
+									if($(m[i][0]).next()[0] == this.element) {
+										$(m[i][0]).before(this.element);
+									} else {
+										$(m[i][0]).after(this.element);
+									}
+	
+								}
+								
 							}
 
-							if(overlap < 0 && overlap > -0.5) { //Overlapping at bottom
 
-								if($(m[i][0]).next()[0] == this.element) {
-									$(m[i][0]).before(this.element);
-								} else {
-									$(m[i][0]).after(this.element);
-								}
 
-							}
 
 						}
 
@@ -90,7 +125,7 @@
 		});
 		
 		//Add current items to the set
-		$('> '+o.items, el).each(function() {
+		items.each(function() {
 			self.set.push([this]);
 		});
 			
