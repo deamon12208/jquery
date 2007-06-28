484edb10f660a50d3bf430a42aff096d10a6777a
joern.zaefferer <joern.zaefferer@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-06-28 21:01:53 +0000

    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2176 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/tooltip/jquery.tooltip.js
+++ plugins/tooltip/jquery.tooltip.js
@@ -17,6 +17,7 @@ TODO
 - change p.body and p.url to div.body and div.url
 - finally: release 2.0, being as backwards compatible as possible
 - build example similar to http://codylindley.com/blogstuff/js/tooltip/tooltip.htm or http://g-academy.tweak.se/
+- use bgiframe plugin if available: check if its need to be applied each time the tooltip is shown or only once on creation
 */
 
 /**
