91e079cdb91a0984eabb86372f0c65dd6ad7cff7
kswedberg <kswedberg@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-07-04 05:01:24 +0000

    wrote more inline documentation prevented multiple ajax requests for the same cluetip content added hideLocal option and defaulted it to true
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2227 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/cluetip/jquery.cluetip.js
+++ plugins/cluetip/jquery.cluetip.js
@@ -1,6 +1,6 @@
 /*
  * jQuery clueTip plugin
- * Version 0.2  (05/16/2007)
+ * Version 0.4  (07/04/2007)
  * @depends jQuery v1.1.1
  * @depends Dimensions plugin 
  *
@@ -8,11 +8,17 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  *
+ *
+ * @name clueTip
+ * @type jQuery
+ * @cat Plugins/tooltip
+ * @return jQuery
+ * @author Karl Swedberg
  */
  
  /*
  * @credit Inspired by Cody Lindley's jTip (http://www.codylindley.com)
- * @credit Thanks to Shelane Enos for the feature ideas 
+ * @credit Thanks to Shelane Enos and Glen Lipka for the feature ideas 
  * @credit Thanks to Jonathan Chaffer, as always, for help with the hard parts. :-)
  */
  
@@ -20,10 +26,26 @@
  // TODO: more testing needed
  // TODO: finish writing inline documentation!
  // TODO: get pngFix working for IE
- // TODO: prevent multiple AJAX requests
+ // CHANGED: preventing multiple AJAX requests
  // CHANGED: added hoverIntent option 
+ 
  /**
+ * 
+ * Displays a highly customizable tooltip when the user hovers (default) or clicks (optional) the matched element. 
+ * By default, the clueTip plugin loads a page indicated by the "rel" attribute via ajax and displays its contents.
+ * If a "title" attribute is specified, its value is used as the clueTip's heading.
+ * The attribute to be used for both the body and the heading of the clueTip is user-configurable. 
+ * Optionally, the clueTip's body can display content from an element on the same page.
+ * * Just indicate the element's id (e.g. "#some-id") in the rel attribute.
+ * Optionally, the clueTip's body can display content from the title attribute, when a delimiter is indicated. 
+ * * The string before the first instance of the delimiter is set as the clueTip's heading.
+ * * All subsequent strings are wrapped in separate DIVs and placed in the clueTip's body.
+ * The clueTip plugin allows for many, many more options. Pleasee see the examples and the option descriptions below...
+ * 
+ * 
  * @example $('#tip).cluetip();
+ * @desc This is the most basic clueTip. It displays a 275px-wide clueTip on mouseover of the element with an ID of "tip." On mouseout, the clueTip is hidden.
+ *
  *
  * @example $('a.clue').cluetip({
  *  hoverClass: 'highlight',
@@ -35,36 +57,30 @@
  *    type: 'POST'
  *  }
  * });
- *
- * TODO: DESCRIBE PLUGIN
- * @desc 
- *
+ * @desc Displays a clueTip on mouseover of all <a> elements with class="clue". The hovered element gets a class of "highlight" added to it (so that it can be styled appropriately. This is esp. useful for non-anchor elements.). The clueTip is "sticky," which means that it will not be hidden until the user either clicks on its "close" text/graphic or displays another clueTip. The "close" text/graphic is set to diplay at the bottom of the clueTip (default is top) and display an image rather than the default "Close" text. Moreover, the body of the clueTip is truncated to the first 60 characters, which are followed by an ellipsis (...). Finally, the clueTip retrieves the content using POST rather than the $.ajax method's default "GET."
+ * 
  *
  *
  * @param Object defaults (optional) Customize your clueTips
  * @option Number delay The number of milliseconds before a tooltip is display, default is 250
  * @option Number width The width of the clueTip, default is 275,
  * @option Boolean local Whether to use content from the same page (using ID) for clueTip body, default is false,
+ * @option Boolean hoverIntent Whether to use the hoverIntent plugin to determine when a clueTip is to be displayed, default is false,
  * @option String attribute The attribute to be used for the URL of the ajaxed content, default is 'rel'
  * @option String titleAttribute 'title
  * @option String splitTitle A character used to split the title attribute into the clueTip title and divs within the clueTip body; if used, the clueTip will be populated only by the title attribute, default is ''
- * @option String hoverClass: default is empty string, so no class''
+ * @option String hoverClass: default is empty string, so no class
  * @option String waitImage: default is 'wait.gif'
  * @option Boolean sticky: default is false
  * @option String activation: default is 'hover'
  * @option String closePosition: default is 'top'
  * @option String closeText: default is 'Close'
  * @option Number truncate: default is 0
- * @option Boolean pngFix: default is false
- * @option Object ajaxProcess
- * @option Object ajaxSettings
+ * @option Boolean pngFix: default is false FIXME: pngFix is not working yet.
+ * @option Object ajaxProcess: when getting clueTip content via ajax, allows processing of it before it's displayed. Default is function(data) { data = $(data).not('style, meta, link, script, title); return data; } . This strips out elements typically found in the <head> that might interfere with current page.
+ * @option Object ajaxSettings: allows you to pass in standard $.ajax() parameters for specifying dataType, error, success, etc. Default is { dataType: 'html'}
  *
- *
- * @name clueTip
- * @type jQuery
- * @cat Plugins/tooltip
- * @return jQuery
- * @author Karl Swedberg
+
  */
 
 (function($) { 
@@ -106,7 +122,8 @@
     $.extend(defaults, options);
     
     return this.each(function() {
-
+      // start out with no contents (for ajax activation)
+      var cluetipContents = false;
       // create the cluetip divs
       if (!$cluetip) {
         $cluetipInner = $('<div id="cluetip-inner"></div>');
@@ -120,7 +137,7 @@
         .appendTo('body')
         .hide();
       }
-      // FIXME: pngFix is broken ;) possible get Jörn's version from toolt
+      // FIXME: pngFix is broken ;) possibly get Jörn's version from toolt
       if (defaults.pngFix) {
         $('#cluetip').pngfix();
       }
@@ -128,6 +145,10 @@
       var tipAttribute = $this.attr(defaults.attribute);
       if (!tipAttribute) return true;
       
+      // if hideLocal is set to true, initially hide the local content that will be displayed in the clueTip
+      if (defaults.local && defaults.hideLocal) {
+        $(tipAttribute).hide();
+      }
       // vertical measurement variables
       var tipHeight, wHeight;
       var sTop, offTop, posY;
@@ -192,7 +213,7 @@
       };
 
 /***************************************      
-* ACTIVATION ...
+* ACTIVATION
 ****************************************/
 
 // activate by click
@@ -222,7 +243,7 @@
       });
     }
     
-//activate clueTip      
+//activate clueTip
       var activate = function(event) {
         if (tipAttribute == $this.attr('href')) {
           $this.css('cursor', 'help');
@@ -272,17 +293,29 @@
           };
           cluetipShow(pY);
 
-// load external file via ajax          
-        } else if (!defaults.local && tipAttribute.indexOf('#') != 0) {
-          var ajaxSettings = defaults.ajaxSettings;
-          ajaxSettings.url = tipAttribute;
-          ajaxSettings.success = function(data) {
-            $cluetipInner.html(defaults.ajaxProcess(data));
-            cluetipShow(pY);
-          };
-          $.ajax(ajaxSettings);
 
-// load an element from the same page
+/***************************************
+* load external file via ajax          
+***************************************/
+        } else if (!defaults.local && tipAttribute.indexOf('#') != 0) {
+          if (cluetipContents) {
+            $cluetipInner.html(cluetipContents);
+            cluetipShow(pY);
+          }
+          else {
+            var ajaxSettings = defaults.ajaxSettings;
+            ajaxSettings.url = tipAttribute;
+            ajaxSettings.success = function(data) {
+              cluetipContents = defaults.ajaxProcess(data);
+              $cluetipInner.html(cluetipContents);
+              cluetipShow(pY);
+            };
+            $.ajax(ajaxSettings);
+          }
+
+/***************************************
+* load an element from the same page
+***************************************/
         } else if (defaults.local && tipAttribute.indexOf('#') == 0){
           var localContent = $(tipAttribute).html();
           $cluetipInner.html(localContent);
