e3e3383d79fb06814bc0e3a3042804ea7f16b345
rdworth <rdworth@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-07-16 07:10:30 +0000

    Removed unneeded checks for options.hoverClass (handled by execPlugins)
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2358 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/ui/ui.drop.ext.js
+++ plugins/ui/ui.drop.ext.js
@@ -1,18 +1,15 @@
 (function($) {
 	
 	$.ui.plugin("droppable", "over", "hoverClass", function() {
-		if(this.options.hoverClass)
-			$(this.element).addClass(this.options.hoverClass);
+		$(this.element).addClass(this.options.hoverClass);
 	});
 	
 	$.ui.plugin("droppable", "out", "hoverClass", function() {
-		if(this.options.hoverClass)
-			$(this.element).removeClass(this.options.hoverClass);
+		$(this.element).removeClass(this.options.hoverClass);
 	});
 	
 	$.ui.plugin("droppable", "drop", "hoverClass", function() {
-		if(this.options.hoverClass)
-			$(this.element).removeClass(this.options.hoverClass);
+		$(this.element).removeClass(this.options.hoverClass);
 	});
 
 })(jQuery);
