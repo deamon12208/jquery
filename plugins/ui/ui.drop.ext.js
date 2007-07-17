044d3d0dcc678a05b6e9dedc8def3c0f2b6542bc
rdworth <rdworth@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-07-17 01:35:42 +0000

    Added droppable.activeClass.Added more droppables tests: activeClass (pass), greedy (fail), hoverClass (fail)
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2366 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/ui/ui.drop.ext.js
+++ plugins/ui/ui.drop.ext.js
@@ -1,13 +1,23 @@
 (function($) {
 	
+	// options.activeClass
+	$.ui.plugin("droppable", "activate", "activeClass", function() {
+		$(this.element).addClass(this.options.activeClass);
+	});
+	$.ui.plugin("droppable", "deactivate", "activeClass", function() {
+		$(this.element).removeClass(this.options.activeClass);
+	});
+	$.ui.plugin("droppable", "drop", "activeClass", function() {
+		$(this.element).removeClass(this.options.activeClass);
+	});
+
+	// options.hoverClass
 	$.ui.plugin("droppable", "over", "hoverClass", function() {
 		$(this.element).addClass(this.options.hoverClass);
 	});
-	
 	$.ui.plugin("droppable", "out", "hoverClass", function() {
 		$(this.element).removeClass(this.options.hoverClass);
 	});
-	
 	$.ui.plugin("droppable", "drop", "hoverClass", function() {
 		$(this.element).removeClass(this.options.hoverClass);
 	});
