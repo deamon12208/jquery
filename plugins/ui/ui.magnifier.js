2f3c62151ead14a4bd114f4f1f28335ecb9241fe
paul.bakaus <paul.bakaus@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-07-18 09:47:00 +0000

    -ain't it smooth?
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2389 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/ui/ui.magnifier.js
+++ plugins/ui/ui.magnifier.js
@@ -9,17 +9,25 @@ $.ui.magnifier = function(el,options) {
 	var self = this;
 	this.items = [];
 	this.options = options ? options : {};
-	this.options.distance = this.options.distance ? this.options.distance : 150;
-	this.options.magnification = this.options.magnification ? this.options.magnification : 2;
-	this.options.baseline = this.options.baseline ? this.options.baseline : 0;
+	this.element = el;
+	var o = this.options;
+	
+	o.distance = o.distance ? o.distance : 150;
+	o.magnification = o.magnification ? o.magnification : 2;
+	o.baseline = o.baseline ? o.baseline : 0;
+	
+	this.pp = $(el).offset({ border: false });
 	
 	$('> *', el).each(function() {
 		var co = $(this).offset({ border: false });
 		if(self.options.overlap) var cp = $(this).position();
-		self.items.push([this, co, [$(this).width(),$(this).height()], (cp ? cp : null)]);	
+		self.items.push([this, co, [$(this).width(),$(this).height()], (cp ? cp : null)]);
+		
+		if(o.opacity)
+			$(this).css('opacity', o.opacity.min);	
 	});
 
-	if(this.options.overlap) {
+	if(o.overlap) {
 		for(var i=0;i<this.items.length;i++) {
 			//Absolute stuff
 			$(this.items[i][0]).css({
@@ -34,6 +42,12 @@ $.ui.magnifier = function(el,options) {
 		self.magnify.apply(self, [e]);
 	});
 	
+	if(o.click) { //If onclick callback is available
+		$(el).bind('click', function(e) {
+			o.click.apply(this, [e, { options: self.options, current: self.current[0] }]);	
+		})
+	}
+	
 }
 
 $.extend($.ui.magnifier.prototype, {
@@ -41,35 +55,57 @@ $.extend($.ui.magnifier.prototype, {
 		var p = $.ui.getPointer(e);
 		var o = this.options;
 		var c;
+		this.current = this.items[0];
+		var distance = 1;
+
+		//Compute the parents distance, because we don't need to fire anything if we are not near the parent
+
+		var overlap = ((p[0] > this.pp.left-o.distance && p[0] < this.pp.left + this.element.offsetWidth + o.distance) && (p[1] > this.pp.top-o.distance && p[1] < this.pp.top + this.element.offsetHeight + o.distance));
+		if(!overlap) return false;
+
+		
 		for(var i=0;i<this.items.length;i++) {
 			c = this.items[i];
 			
-			var distance = Math.sqrt(
-				  Math.pow(p[0] - c[1].left - (c[0].offsetWidth/2), 2)
-				+ Math.pow(p[1] - c[1].top  - (c[0].offsetHeight/2), 2)
+			var olddistance = distance;
+			distance = Math.sqrt(
+				  Math.pow(p[0] - ((c[3] ? this.pp.left : c[1].left) + parseInt(c[0].style.left)) - (c[0].offsetWidth/2), 2)
+				+ Math.pow(p[1] - ((c[3] ? this.pp.top  : c[1].top ) + parseInt(c[0].style.top )) - (c[0].offsetHeight/2), 2)
 			);
-			
-			//console.log(distance);
+			this.current = distance < olddistance ? this.items[i] : this.current;
 			
 			if(distance < o.distance) {
 
 				$(c[0]).css({
 					width: c[2][0]+ (c[2][0] * (o.magnification-1)) - (((distance/o.distance)*c[2][0]) * (o.magnification-1)),
-					top: (o.baseline-0.5) * ((c[2][0] * (o.magnification-1)) - (((distance/o.distance)*c[2][0]) * (o.magnification-1))),
+					top: (c[3] ? c[3].top : 0) + (o.baseline-0.5) * ((c[2][0] * (o.magnification-1)) - (((distance/o.distance)*c[2][0]) * (o.magnification-1))),
 					left: (c[3] ? (c[3].left + -0.5 * ((c[2][1] * (o.magnification-1)) - (((distance/o.distance)*c[2][1]) * (o.magnification-1)))) : 0)
 				});
+				
+				if(o.opacity)
+					$(c[0]).css('opacity', o.opacity.max-(distance/o.distance) < o.opacity.min ? o.opacity.min : o.opacity.max-(distance/o.distance));
 				
 			} else {
 				
 				$(c[0]).css({
 					width: c[2][0],
-					top: 0,
+					top: (c[3] ? c[3].top : 0),
 					left: (c[3] ? c[3].left : 0)
-				});	
+				});
+				
+				if(o.opacity)
+					$(c[0]).css('opacity', o.opacity.min);
 							
 			}
+			
+			if(o.zIndex)
+				$(c[0]).css("z-index", "");
 
 		}
+		
+		if(this.options.zIndex)
+			$(this.current[0]).css("z-index", this.options.zIndex);
+		
 	}
 });
 
