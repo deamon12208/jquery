f27b44eabda593e90693d4c8444dc125823ad06e
grabanski <grabanski@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-09 20:42:07 +0000

    German Translation
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2639 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ plugins/calendar/jquery-calendar-ge.js
@@ -0,0 +1,12 @@
+/* German initialisation for the jQuery calendar extension. */
+/* Written by Milian Wolff (mail@milianw.de). */ $(document).ready(function(){
+        popUpCal.clearText = 'Löschen';
+        popUpCal.closeText = 'Schließen';
+        popUpCal.prevText = '&lt;Zurück';
+        popUpCal.nextText = 'Vor&gt;';
+        popUpCal.currentText = 'Heute';
+        popUpCal.dateFormat = 'DMY.';
+        popUpCal.dayNames = ['So','Mo','Di','Mi','Do','Fr','Sa'];
+        popUpCal.monthNames =
+['Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'];
+});
