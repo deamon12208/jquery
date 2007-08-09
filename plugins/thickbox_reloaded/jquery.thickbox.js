0ae12f2cf5f0a18a83ec236bfcdd5bb4ab3b6203
klaus.hartl <klaus.hartl@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-09 09:27:03 +0000

    Fixed IE 6 detection
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2631 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/thickbox_reloaded/jquery.thickbox.js
+++ plugins/thickbox_reloaded/jquery.thickbox.js
@@ -9,7 +9,7 @@
 
 (function($) { // simulate block scope
 
-    $.browser.msie6 = $.browser.msie && ($.browser.version && $.browser.version < 7 || /6.0/.test(navigator.userAgent));
+    $.browser.msie6 = $.browser.msie && ($.browser.version && $.browser.version < 7 || /MSIE 6.0/.test(navigator.userAgent)); // do not check for 6.0 alone, userAgent in Windows Vista has "Windows NT 6.0"
 
     $.extend({
         thickbox: new function() {
