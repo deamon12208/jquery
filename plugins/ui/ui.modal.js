c3e44d3f075b31eef40330d4aa68faab7bc15f5d
dmitrig01 <dmitrig01@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-13 21:59:45 +0000

    Small modal fixes
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2682 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/ui/ui.modal.js
+++ plugins/ui/ui.modal.js
@@ -8,27 +8,8 @@
  * See the docs for wrap...
  * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
  */
-jQuery.fn.innerWrap = function() {
-	var a, args = arguments;
-	return this.each(function() {
-		if (!a)
-			a = jQuery.clean(args, this.ownerDocument);
-		// Clone the structure that we're using to wrap
-		var b = a[0].cloneNode(true),
-		  c = b;
-		// Find the deepest point in the wrap structure
-		while ( b.firstChild )
-			b = b.firstChild;
-		// append the child nodes to the wrapper
-		jQuery.each(this.childNodes, function(i, node) { 
-			b.appendChild(node); 
-		});
-		jQuery(this)
-			// clear the element
-			.empty()
-			// add the new wrapper with the previous child nodes appeneded
-			.append(c);
-	});
+jQuery.fn.innerWrap = function(u) {
+	return $(this).html($(u).html($(this).html()));
 };
 
 jQuery.fn.uiBox = function(arg){
@@ -66,20 +47,27 @@ $.fn.modal = function(o){
 }
 $.ui.modal = function(el, o) {
   var options = {
+    // Internal name - used in $.ui.get
     name: '',
+    // Title
     title: '',
+    // Location of the buttons
     buttons: 'right',
+    // Open event
     open: function(e, ui) {
+      // If it's an object, pass it to $().animate();
       if(typeof ui.options.animation == 'Object') {
-        // Not sure if this works...
         $(ui.modal).animate(ui.options.animation, ui.options.speed);
       }
+      // No animation
       else if(ui.options.animation == 'none') {
         $(ui.modal).show(0);
       }
+      // Fade
       else if(ui.options.animation == 'fade') {
         $(ui.modal).fadeIn(ui.options.speed);
       }
+      // Scale
       else if(ui.options.animation == 'scale') {
         $(ui.modal).find('*').each(function(i, n) {
           $(n).css('overflow', $(n).attr('modalRestoreOverflow'));
@@ -87,10 +75,12 @@ $.ui.modal = function(el, o) {
         $(ui.modal).show(ui.options.speed);
       }
       else {
+        // Custom animation
         $(ui.modal)[ui.options.animation](ui.options.speed);
       }
     },
     close: function(e, ui) {
+      // Optionally remove it when animation is done
       if(ui.options.outRemove == true) {
         var f = function() {
           $(this).remove();
@@ -99,9 +89,9 @@ $.ui.modal = function(el, o) {
       else {
         var f = function() {};
       }
+      // Refer to the open above for all these options
       if(typeof ui.options.animation == 'Object') {
-        // Not sure if this works...
-        $(ui.modal).apply('animation', ui.options.animation);
+        $(ui.modal).animate(ui.options.animation, ui.options.speed);
       }
       else if(ui.options.animation == 'none') {
         $(ui.modal).remove();
@@ -110,6 +100,8 @@ $.ui.modal = function(el, o) {
         $(ui.modal).fadeOut(ui.options.speed, f);
       }
       else if(ui.options.animation == 'scale') {
+        // We set the overflow to hidden on every element to prevent scrolling when scaling.
+        // The overflow is restored when the window is opened
         $(ui.modal).find('*').each(function(i, n) {
           var c = $(n).css('overflow');
           $(n).attr('modalRestoreOverflow', c);
@@ -118,6 +110,7 @@ $.ui.modal = function(el, o) {
         $(ui.modal).hide(ui.options.speed, f);
       }
       else if(ui.options.animation == 'slide') {
+        // Ditto to above
         $(ui.modal).find('*').each(function(i, n) {
           var c = $(n).css('overflow');
           $(n).attr('modalRestoreOverflow', c);
@@ -129,6 +122,7 @@ $.ui.modal = function(el, o) {
         $(ui.modal)[ui.options.animation](ui.options.speed, f);
       }
     },
+    // Options specific to $().resizable
     resize: {
       handles: {
         s: 'div.bottom.pane',
@@ -136,21 +130,29 @@ $.ui.modal = function(el, o) {
         e: 'div.right.pane' 
       }
     },
+    // Options specific to $().draggable
     drag: {
       handle: 'div.top.pane'
     },
+    // Allow overflow/scrolling
     overflow: true,
+    // Override button markup optionally
     buttonMarkup: '<a class="close">x</a>',
+    // Width of the modal
     width: 400,
+    // Height
     height: 400,
+    // Animation.  Look at open above for possible values
     animation: 'none',
+    // Animation speed
     speed: 'fast',
+    // Remove when exiting
     outRemove: false
   };
   $.extend(options, o);
-  this.options = options;
   var self = this;
-  var bu = $("<span>").addClass('buttons').html(options.buttonMarkup);
+  self.options = options;
+  var bu = $("<span>").addClass('buttons').addClass(options.buttons).html(options.buttonMarkup);
   var ti = $('<span>').addClass("title").html(options.title);
   var t = $('<div>');
   if(options.buttons == 'left') {
