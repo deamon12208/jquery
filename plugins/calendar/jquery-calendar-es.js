60ea15d00c460d20ed8c0bf933906cf8ebe96fd9
kbwood.au <kbwood.au@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-15 13:04:37 +0000

    Update localisations
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2713 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/calendar/jquery-calendar-es.js
+++ plugins/calendar/jquery-calendar-es.js
@@ -1,10 +1,10 @@
 /* Inicialización en español para la extensión 'calendar' para jQuery. */
 /* Traducido por Vester (xvester@gmail.com). */
 $(document).ready(function(){
-	popUpCal.closeText = 'Cerrar';
-	popUpCal.prevText = '&lt;Ant';
-	popUpCal.nextText = 'Sig&gt;';
-	popUpCal.currentText = 'Hoy';
-	popUpCal.dayNames = new Array('Domingo','Lunes','Martes','Mi&eacute;rcoles','Jueves','Viernes','S&aacute;bado');
-	popUpCal.monthNames = new Array('Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
+	popUpCal.regional['es'] = {clearText: '???', closeText: 'Cerrar',
+		prevText: '&lt;Ant', nextText: 'Sig&gt;', currentText: 'Hoy',
+		dayNames: ['Do','Lu','Ma','Mi','Ju','Vi','S&aacute;'],
+		monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
+		'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']};
+	popUpCal.setDefaults(popUpCal.regional['es']);
 });
--- plugins/calendar/jquery-calendar-fr.js
+++ plugins/calendar/jquery-calendar-fr.js
@@ -1,7 +1,7 @@
 /* French initialisation for the jQuery calendar extension. */
 /* Written by Keith Wood (kbwood@iprimus.com.au). */
 $(document).ready(function(){
-	popUpCal.regional['fr'] = {clearText: 'Enlevez', closeText: 'Fermez', 
+	popUpCal.regional['fr'] = {clearText: 'Effacer', closeText: 'Fermer', 
 		prevText: '&lt;Préc', nextText: 'Proch&gt;', currentText: 'En cours',
 		dayNames: ['Di','Lu','Ma','Me','Je','Ve','Sa'],
 		monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin',
