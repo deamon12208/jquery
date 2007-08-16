207d55fc196dba397d7f465aa0580ee8944dd126
christian.bach <christian.bach@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-16 09:00:02 +0000

    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2731 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/tablesorter/2.0/docs/js/examples.js
+++ plugins/tablesorter/2.0/docs/js/examples.js
@@ -6,50 +6,33 @@ $(function() {
 	var url = "assets/" + window.tableFile;
 	var $container = $("#main");
 	
-	// append table to document.
-	$.get(url, function(data){
-   		
-		var $table = $("table").html(data);
-		
-		
-		
+	// get javascript source
+	$("#javascript pre").text($("#js").html());
 	
-		// get javascript source
-		$("#javascript pre").text($("#js").html());
-		
-		// old school chaining...
-		var html = $("#demo").html()
-					.toLowerCase()
-					.replace(/\n|\t|\r/g,'')
-					.replace(/<td/g,'\t\t\t<td')
-					.replace(/<\/td>/g,'</td>\n')
-					.replace(/<th/g,'\t\t\t<th')
-					.replace(/<\/th>/g,'</th>\n')
-					.replace(/<\/tr>/g,'\t\t</tr>')
-					.replace(/<tr>/g,'\n\t\t<tr>\n')
-					.replace(/<thead/g,'\n\t<thead>')
-					.replace(/<\/thead>/g,'\n\t</thead>')
-					.replace(/<tbody/g,'\n\t<tbody')
-					.replace(/<\/tbody>/g,'\n\t</tbody>')
-					.replace(/<\/table>/g,'\n</table>')
-					.replace(/-->/g,'-->\n');
-					
-		$("#html pre").text(html);
-		$table.tablesorter($table[0].config);
-		
-		
-		$("pre.javascript").chili();
-		$("pre.html").chili();
-		$("pre.css").chili();
-		
- 	});
+	// old school chaining...
+	var html = $("#demo").html()
+				.toLowerCase()
+				.replace(/\n|\t|\r/g,'')
+				.replace(/<td/g,'\t\t\t<td')
+				.replace(/<\/td>/g,'</td>\n')
+				.replace(/<th/g,'\t\t\t<th')
+				.replace(/<\/th>/g,'</th>\n')
+				.replace(/<\/tr>/g,'\t\t</tr>')
+				.replace(/<tr>/g,'\n\t\t<tr>\n')
+				.replace(/<thead/g,'\n\t<thead>')
+				.replace(/<\/thead>/g,'\n\t</thead>')
+				.replace(/<tbody/g,'\n\t<tbody')
+				.replace(/<\/tbody>/g,'\n\t</tbody>')
+				.replace(/<\/table>/g,'\n</table>')
+				.replace(/-->/g,'-->\n');
+				
+	$("#html pre").text(html);
 	
+	$("pre.javascript").chili();
+	$("pre.html").chili();
+	$("pre.css").chili();
 	
-	
-	
-	
-	var scriptSrc = $("#script-source").html();
-	
+ 	
 	
 	
 	
