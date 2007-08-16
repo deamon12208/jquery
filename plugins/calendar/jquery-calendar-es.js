2ed6f80926a8a7ee4eeca8bbdaaa2f23f5c3552f
kbwood.au <kbwood.au@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-16 10:22:51 +0000

    Add clear text
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2737 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/calendar/jquery-calendar-es.js
+++ plugins/calendar/jquery-calendar-es.js
@@ -1,7 +1,7 @@
 /* Inicialización en español para la extensión 'calendar' para jQuery. */
 /* Traducido por Vester (xvester@gmail.com). */
 $(document).ready(function(){
-	popUpCal.regional['es'] = {clearText: '???', closeText: 'Cerrar',
+	popUpCal.regional['es'] = {clearText: 'Limpiar', closeText: 'Cerrar',
 		prevText: '&lt;Ant', nextText: 'Sig&gt;', currentText: 'Hoy',
 		dayNames: ['Do','Lu','Ma','Mi','Ju','Vi','S&aacute;'],
 		monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
