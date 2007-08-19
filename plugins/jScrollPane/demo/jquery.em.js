660fb766672dff548aeca7f9adcc67f563ce934e
kelvin.luck <kelvin.luck@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-19 11:56:42 +0000

    Updated to the latest version of jquery.em.js and checked that everything is still working fine...
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2782 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/jScrollPane/demo/jquery.em.js
+++ plugins/jScrollPane/demo/jquery.em.js
@@ -1,27 +1,45 @@
 /**
  * @projectDescription Monitor Font Size Changes with jQuery
  *
- * @version 1.0a
+ * @version 1.0
  * @author Dave Cardwell
  *
- * jQuery-Em - r$Revision: 8 $ ($Date: 2007-08-17 16:49:19 +0100 (Fri, 17 Aug 2007) $)
+ * jQuery-Em - $Revision: 24 $ ($Date: 2007-08-19 11:24:56 +0100 (Sun, 19 Aug 2007) $)
  * http://davecardwell.co.uk/javascript/jquery/plugins/jquery-em/
  *
  * Copyright ©2007 Dave Cardwell <http://davecardwell.co.uk/>
- * Dual licensed under the MIT (MIT-LICENSE.txt) and
- * GPL (GPL-LICENSE.txt) licenses.
+ *
+ * Released under the MIT licence:
+ * http://www.opensource.org/licenses/mit-license.php
  */
 
 // Upon $(document).ready()…
 jQuery(function($) {
+    // Configuration…
+    var eventName = 'emchange';
+    
+    
     // Set up default options.
     $.em = $.extend({
+        /**
+         * The jQuery-Em version string.
+         *
+         * @example $.em.version;
+         * @desc '1.0a'
+         *
+         * @property
+         * @name version
+         * @type String
+         * @cat Plugins/Em
+         */
+        version: '1.0',
+        
         /**
          * The number of milliseconds to wait when polling for changes to the
          * font size.
          *
          * @example $.em.delay = 400;
-         * @desc Defaults to 200ms.
+         * @desc Defaults to 200.
          *
          * @property
          * @name delay
@@ -45,7 +63,7 @@ jQuery(function($) {
         element: $('<div />').css({ left:     '-100em',
                                     position: 'absolute',
                                     width:    '100em' })
-                             .prependTo(document.body)[0],
+                             .prependTo('body')[0],
         
         /**
          * The action to perform when a change in the font size is detected.
@@ -53,7 +71,7 @@ jQuery(function($) {
          * @example $.em.action = function() { ... }
          * @desc The default action is to trigger a global “emchange” event.
          * You probably shouldn’t change this behaviour as other plugins may
-         * rely on it.
+         * rely on it, but the option is here for completion.
          *
          * @example $(document).bind('emchange', function(e, cur, prev) {...})
          * @desc Any functions triggered on this event are passed the current
@@ -73,8 +91,9 @@ jQuery(function($) {
             // If the font size has changed since we last checked…
             if ( currentWidth != $.em.current ) {
                 /**
-                 * The previous value of the user agent’s font size. See
-                 * $.em.current for caveats.
+                 * The previous pixel value of the user agent’s font size. See
+                 * $.em.current for caveats. Will initially be undefined until
+                 * the “emchange” event is triggered.
                  *
                  * @example $.em.previous;
                  * @result 16
@@ -88,10 +107,10 @@ jQuery(function($) {
                 $.em.previous = $.em.current;
                 
                 /**
-                 * The current value of the user agent’s font size. As with
-                 * $.em.previous, this value may be subject to minor browser
-                 * rounding errors that mean it should not be relied upon as
-                 * an absolute value.
+                 * The current pixel value of the user agent’s font size. As
+                 * with $.em.previous, this value *may* be subject to minor
+                 * browser rounding errors that mean you might not want to
+                 * rely upon it as an absolute value.
                  *
                  * @example $.em.current;
                  * @result 14
@@ -104,33 +123,49 @@ jQuery(function($) {
                  */
                 $.em.current = currentWidth;
                 
-                $.event.trigger('emchange', [$.em.current, $.em.previous]);
+                $.event.trigger(eventName, [$.em.current, $.em.previous]);
             }
         }
     }, $.em );
     
+    
+    /**
+     * Bind a function to the emchange event of each matched element.
+     *
+     * @example $("p").emchange( function() { alert("Hello"); } );
+     *
+     * @name emchange
+     * @type jQuery
+     * @param Function fn A function to bind to the emchange event.
+     * @cat Plugins/Em
+     */
+
+    /**
+     * Trigger the emchange event of each matched element.
+     *
+     * @example $("p").emchange()
+     *
+     * @name emchange
+     * @type jQuery
+     * @cat Plugins/Em
+     */
+    $.fn[eventName] = function(fn) { return fn ? this.bind(eventName, fn)
+                                               : this.trigger(eventName); };
+    
+    
     // Store the initial pixel value of the user agent’s font size.
     $.em.current = $.em.element.offsetWidth / 100;
     
-    // Try to use Internet Explorer’s proprietary “setExpression” method…
-    try {
-        $.em.element.style.setExpression(
-            'width', 'function() {jQuery.em.action(); return "100em";}()' 
-        );
-    }
-    // …otherwise fall back to polling for changes.
-    catch(e) {
-        /**
-         * When the polling method is being used, $.em.iid stores the
-         * intervalID should you want to cancel with clearInterval().
-         *
-         * @example window.clearInterval( $.em.iid );
-         * 
-         * @property
-         * @name iid
-         * @type Number
-         * @cat Plugins/Em
-         */
-        $.em.iid = window.setInterval( $.em.action, $.em.delay );
-    }
+    /**
+     * While polling for font-size changes, $.em.iid stores the intervalID in
+     * case you should want to cancel with clearInterval().
+     *
+     * @example window.clearInterval( $.em.iid );
+     * 
+     * @property
+     * @name iid
+     * @type Number
+     * @cat Plugins/Em
+     */
+    $.em.iid = setInterval( $.em.action, $.em.delay );
 });
