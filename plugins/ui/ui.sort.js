0b0e72693370968a9717963e9773ffe8d9a86115
stefan.petre <stefan.petre@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-20 20:31:15 +0000

    Improved the speed. Added animate option.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2804 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/ui/ui.sort.js
+++ plugins/ui/ui.sort.js
@@ -10,6 +10,18 @@ if (window.Node && Node.prototype && !Node.prototype.contains) {
 		return this.each(function() {
 			new $.ui.sortable(this,o);	
 		});
+	},
+	$.fn.stopAll = function() {
+		return this.each(function(){
+			if (this.queue) {
+				if (this.queue['fx']) {
+					if (this.queue['fx'][0]) {
+						this.queue['fx'] = [this.queue['fx'][0]];
+						this.queue['fx'][0].startTime = (new Date()).getTime() - 1000000000;
+					}
+				}
+			}
+		});
 	}
 	
 	$.ui.sortable = function(el,o) {
@@ -37,23 +49,17 @@ if (window.Node && Node.prototype && !Node.prototype.contains) {
 		});
 		
 		//Get the items
-		var items = $(options.items, el);
+		self.set = $(options.items, el).each(function() {
+			new $.ui.mouseInteraction(this,options);
+		});
 		
 		//Let's determine the floating mode
-		options.floating = items.css('float') == 'left' || items.css('float') == 'right' ? true : false;
+		options.floating = /left|right/.test(self.set.css('float'));
+		options.animated = options.animated && self.set.css('position') == 'relative';
 		
 		//Let's determine the parent's offset
 		if($(el).css('position') == 'static') $(el).css('position', 'relative');
 		options.offset = $(el).offset({ border: false });
-
-		items.each(function() {
-			new $.ui.mouseInteraction(this,options);
-		});
-		
-		//Add current items to the set
-		items.each(function() {
-			self.set.push([this,null]);
-		});
 		
 		if(options.name) $.ui.add(options.name, 'sortable', this); //Append to UI manager if a name exists as option
 		this.options = options;
@@ -109,8 +115,23 @@ if (window.Node && Node.prototype && !Node.prototype.contains) {
 		start: function(that, e) {
 			
 			var o = this.options;
+			//cache each elements position. When we insert an element before or after the actual position will not coutn anymore TODO cache elements position for each active sortable
+			that.cachedItemsPos = [];
+			//remember container position
+			that.pos = $(that.element).offset({ border: false});
+			that.set = $(that.options.items, that.element).not(':last').each(function(nr){
+				that.cachedItemsPos[nr] = {
+					x: this.offsetLeft,
+					y: this.offsetTop,
+					w: this.offsetWidth,
+					h: this.offsetHeight
+				};
+			});
+			o.lastOverlap = -1;
+			o.movedElement = null;
+			
+			
 			$.ui.plugin.call('start', this);
-
 			if(o.hoverClass) {
 				that.helper = $('<div class="'+o.hoverClass+'"></div>').appendTo('body').css({
 					height: this.element.offsetHeight+'px',
@@ -118,20 +139,16 @@ if (window.Node && Node.prototype && !Node.prototype.contains) {
 					position: 'absolute'	
 				});
 			}
-			
 			if(o.zIndex) {
 				if($(this.helper).css("zIndex")) o.ozIndex = $(this.helper).css("zIndex");
 				$(this.helper).css('zIndex', o.zIndex);
 			}
-			
 			that.firstSibling = $(this.element).prev()[0];
-				
+			
 			if(o.start) o.start.apply(this.element, [this.helper, this.pos, o.cursorAt, this]);
 			$.ui.trigger('start', this, e, that.prepareCallbackObj(this));
 			$(this.element).css('visibility', 'hidden');
-			
 			return false;
-						
 		},
 		stop: function(that, e) {			
 			
@@ -151,56 +168,91 @@ if (window.Node && Node.prototype && !Node.prototype.contains) {
 			if($(this.element).prev()[0] != that.firstSibling) {
 				$.ui.trigger('update', this, e, that.prepareCallbackObj(this, that));
 			}
+			//clear previous cache TODO: clear al active sortables
+			that.cachedItemsPos = null;
+			that.pos = null;
+			o.lastOverlap = false;
 			return false;
-			
 		},
 		drag: function(that, e) {
-
+			// remember the dragged element. We need this to exclude this one from items we check against
+			draggedEl = this.element;
 			var o = this.options;
-
 			this.pos = [this.pos[0]-(o.cursorAt.left ? o.cursorAt.left : 0), this.pos[1]-(o.cursorAt.top ? o.cursorAt.top : 0)];
 			$.ui.plugin.call('drag', this);
 
 			var nv = $.ui.trigger('drag', this, e, that.prepareCallbackObj(this));
 			var nl = (nv && nv.left) ? nv.left :  this.pos[0];
 			var nt = (nv && nv.top) ? nv.top :  this.pos[1];
+			//overlaped element
+			var targetIndex = null;
+			//overlap index
+			var targetOverlap = null;
 			
-
-			var m = that.set;
-			var p = this.pos[1];
-			
-			for(var i=0;i<m.length;i++) {
-				
-				var ci = $(m[i][0]); var cio = m[i][0];
-				if(this.element.contains(cio)) continue;
-				var cO = ci.offset({ border: false, relativeTo: that.element }); //TODO: Caching
-				cO = { top: o.offset.top+cO.top, left: o.offset.left+cO.left };
-				
-				var mb = function(e) { if(true || o.lba != cio) { ci.before(e); o.lba = cio; } }
-				var ma = function(e) { if(true || o.laa != cio) { ci.after(e); o.laa = cio; } }
-				
-				if(o.floating) {
-					
-					var overlap = ((cO.left - (this.pos[0]+(this.options.po ? this.options.po.left : 0)))/this.helper.offsetWidth);
-
-					if(!(cO.top < this.pos[1]+(this.options.po ? this.options.po.top : 0) + cio.offsetHeight/2 && cO.top + cio.offsetHeight > this.pos[1]+(this.options.po ? this.options.po.top : 0) + cio.offsetHeight/2)) continue;								
-					
+			that.set.each(function(nr){
+				if (o.lastOverlap == this || targetIndex != null)
+					return;
+				if(that.options.floating) {
+					elPosition = that.pos.left + that.cachedItemsPos[nr].x;
+					elDimension = that.cachedItemsPos[nr].w;
+					curPosition = e.pageX;
+					overlapY = (e.pageY - that.pos.top - that.cachedItemsPos[nr].y) / that.cachedItemsPos[nr].h;
+					overlapY = overlapY >= 0 && overlapY <=1;
 				} else {
-
-					var overlap = ((cO.top - (this.pos[1]+(this.options.po ? this.options.po.top : 0)))/this.helper.offsetHeight);
-
-					if(!(cO.left < this.pos[0]+(this.options.po ? this.options.po.left : 0) + cio.offsetWidth/2 && cO.left + cio.offsetWidth > this.pos[0]+(this.options.po ? this.options.po.left : 0) + cio.offsetWidth/2)) continue;
-
+					elPosition = that.pos.top + that.cachedItemsPos[nr].y;
+					elDimension = that.cachedItemsPos[nr].h;
+					curPosition = e.pageY;
+					overlapY = true;
 				}
+				overlap = (curPosition - elPosition) / elDimension;
+				if (overlap >= 0 && overlap <= 1 && overlapY) {
+					targetIndex = nr;
+					targetOverlap = overlap;
+				}
+			});
+			//one element was overlaped
+			if (targetIndex != null) {
+				where = 'after';
+				elementIndex = that.set.index(this.element);
+				o.lastOverlap = that.set[targetIndex];
+				if (elementIndex > targetIndex || that.set[targetIndex] == this.element) {
+					targetIndex --;
+				}
+				if (targetIndex < 0) {
+					targetIndex = 0;
+					where = 'before';
+				}
+				if (o.animated) {
+					toMoveIndex = targetIndex;
+					if (o.lastIndex > targetIndex) {
+						toMoveIndex = o.lastIndex ;
+					}
+					if (o.movedElement && o.movedElement[0] == that.set[toMoveIndex]) {
+						o.movedElement
+							.stopAll()
+							.css({
+								top: 0,
+								left: 0
+							});
+					}
+					o.movedElement = $(that.set[toMoveIndex]);
+					oldPos = {
+						top: o.movedElement[0].offsetTop,
+						left: o.movedElement[0].offsetLeft
+					};
+				}
+				$(that.set[targetIndex])[where](this.element);
 				
-				if(overlap >= 0 && overlap <= 0.5) { //Overlapping at top
-					ci.prev().length ? ma(this.element) : mb(this.element);
+				if (o.animated) {
+					o.movedElement.css({
+						top: oldPos.top - o.movedElement[0].offsetTop + 'px',
+						left: oldPos.left - o.movedElement[0].offsetLeft + 'px'
+					}).animate({
+						top: 0,
+						left: 0
+					}, 500);
+					o.lastIndex = targetIndex;
 				}
-
-				if(overlap < 0 && overlap > -0.5) { //Overlapping at bottom
-					ci.next()[0] == this.element ? mb(this.element) : ma(this.element);
-				}
-
 			}
 			
 			//Let's see if the position in DOM has changed
