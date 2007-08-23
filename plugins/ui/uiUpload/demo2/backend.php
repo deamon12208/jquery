a8ef3e177e777a5f485011fddf8285892b2b9417
gilles0181 <gilles0181@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-23 18:33:16 +0000

    uiUpload code base
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2854 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ plugins/ui/uiUpload/demo2/backend.php
@@ -0,0 +1,79 @@
+<?php
+	define('wcDemo', 'demo2');
+	require_once('../php/shared.php');
+
+	/**
+	 * Actual script processing
+	 */
+	if (isset($_FILES['Filedata']['name'])) {
+		$strFile = $_FILES['Filedata']['tmp_name'];
+		$intError = -1;
+
+		//
+		// Not an uploaded file
+		if (!is_uploaded_file($strFile)) {
+			$intError = 400;
+		}
+/*
+		//
+		// Check filesize
+		if (($intError == -1) && ($_FILES['Filedata']['size'] > (256 * 1024))) {
+			$intError = 413;
+		}
+
+		//
+		// Check image condition
+		if ($intError == -1) {
+			$arrSize = @getimagesize($strFile);
+			if (!$arrSize) {
+				$intError = 409;
+			}
+
+			//
+			// Check image dimensions
+			if (($intError == -1) && (($arrSize[0] < 25) || ($arrSize[1] < 25))) {
+				$intError = 412;
+			}
+
+			//
+			// Check filetype
+			if (($intError == -1) && (!in_array($arrSize[2], array(1, 2, 3, 7, 8)))) {
+				$intError = 415;
+			}
+		}
+*/
+
+		//
+		// Check to see if we have to report a critical error.
+		// This error is sent back to Flash, and Flash fires it back to Javascript.
+		if (!$intError == -1) {
+			//
+			// Find HTTP error
+			$strError = HTTPStatus($intError);
+
+			//
+			// Write log entry
+			writeLog('Upload failed ('.$strError.')', $_FILES['Filedata']['name']);
+
+			//
+			// Send headers
+			header($strError);
+		} else {
+			//
+			// Move file
+			//move_uploaded_file($_FILES['Filedata']['tmp_name'], $_SERVER['DOCUMENT_ROOT'].'/'.$_FILES['Filedata']['name']);
+
+			//
+			// Upload valid and succesfull
+			writeLog('Upload completed', $_FILES['Filedata']['name']);
+		}
+
+		//
+		// For demo: ALWAYS REMOVE UPLOAD
+		@unlink($strFile);
+
+		//
+		// Prevent bug in Mac OS 8 flash player
+		echo ' ';
+	}
+?>
