8421efe9e0e9be54e8c8c6427464843e54389126
littlecooldude <littlecooldude@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-27 21:36:12 +0000

    UI Demo - Updated styles, added some basic examples to work from
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2943 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/ui/demo/demo.js
+++ plugins/ui/demo/demo.js
@@ -1,7 +1,14 @@
-// Menu - to be replaced by Tabs 3
 $(function(){
+  // Menu - to be replaced by Tabs 3
   $("#nav").idTabs(true);
   if(window.location.hash)
     $("#nav a[@href='"+window.location.hash+"']").click();
+
+  // Default Tab - info page (don't clutter the view source)
+  $.ajax({url:"info.html",success:function(r){
+    $(r).hide().prependTo("#demos");
+    if(!$("#demos .demo:visible").length) $("#info").show();
+  }});
+  $("#header h1 span").click(function(){ $("#nav a[@href='#info']").click(); });
 });
 
