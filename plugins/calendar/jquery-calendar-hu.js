0917e3c246c8d0e84f45263342f5e54e298a937a
grabanski <grabanski@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-28 18:33:38 +0000

    Added Hungarian localization.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@2975 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ plugins/calendar/jquery-calendar-hu.js
@@ -0,0 +1,24 @@
+/* Hungarian initialisation for the jQuery calendar extension. */
+/* Written by Istvan Karaszi (jquerycalendar@spam.raszi.hu). */
+$(document).ready(function(){
+	popUpCal.regional['hu'] = {
+		clearText: 'törlés',
+		closeText: 'bezárás',
+		prevText: '&laquo;&nbsp;vissza',
+		nextText: 'előre&nbsp;&raquo;',
+		currentText: 'ma',
+
+		firstDay: 1,
+		dayNames: [
+			'V', 'H', 'K', 'Sze', 'Cs', 'P', 'Szo'
+		],
+		monthNames: [
+			'Január', 'Február', 'Március', 'Április', 'Május', 'Június',
+			'Július', 'Augusztus', 'Szeptember', 'Október', 'November', 'December'
+		],
+
+		dateFormat: 'YMD-'
+	};
+
+	popUpCal.setDefaults(popUpCal.regional['hu']);
+});
