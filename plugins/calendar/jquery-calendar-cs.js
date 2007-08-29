28ee9ad860d3a8a82793fb185b3b5897c81a3b8e
grabanski <grabanski@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-29 21:55:16 +0000

    Added 'Portuguese of Brazil' and 'Czech' translations.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3000 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ plugins/calendar/jquery-calendar-cs.js
@@ -0,0 +1,10 @@
+/* Czech initialisation for the jQuery calendar extension. */
+/* Written by Tomas Muller (tomas@tomas-muller.net). */
+$(document).ready(function(){
+	popUpCal.regional['cs'] = {clearText: 'Smazat', closeText: 'Zavøít', 
+		prevText: '&lt;Døíve', nextText: 'Pozdìji&gt;;', currentText: 'Nyní',
+		dayNames: ['Ne','Po','Út','St','Èt','Pá','So'],
+		monthNames: ['Leden','Únor','Bøezen','Duben','Kvìten','Èerven',
+		'Èervenec','Srpen','Záøí','Øíjen','Listopad','Prosinec']};
+	popUpCal.setDefaults(popUpCal.regional['cs']);
+});
