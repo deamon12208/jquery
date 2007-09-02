058a7e8cbbf5da776cc16352624405f0100835c7
paul.bakaus <paul.bakaus@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-09-02 19:55:43 +0000

    shadow using wrapping
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3068 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/ui/ui.shadow.js
+++ plugins/ui/ui.shadow.js
@@ -6,7 +6,8 @@
 	$.fn.shadow = function(options) {
 		
 		options = options || {};
-		options.offset = options.offset ? Math.round(options.offset/2) : 0;
+		options.offset = options.offset ? options.offset : 0;
+		options.opacity = options.opacity ? options.opacity : 0.2;
 		
 		return this.each(function() {
 			
@@ -16,9 +17,19 @@
 			cur.wrap("<div class='ui-shadow'></div>");
 			var shadow = $(this.parentNode);
 			
+			//Figure the base height and width
+			var baseWidth = cur.outerWidth();
+			var baseHeight = cur.outerHeight();
+			
 			//Append smooth corners
-			$('<div class="ui-shadow-ne"></div>').css("width", 6+(options.offset*2)).appendTo(shadow);
-			$('<div class="ui-shadow-sw"></div>').css("height", 6+(options.offset*2)).appendTo(shadow);
+			$('<div class="ui-shadow-color ui-shadow-layer-1"></div>').css({ opacity: options.opacity-0.05, left: 5+options.offset, top: 5+options.offset, width: baseWidth+1, height: baseHeight+1 }).appendTo(shadow);
+			$('<div class="ui-shadow-color ui-shadow-layer-2"></div>').css({ opacity: options.opacity-0.1, left: 7+options.offset, top: 7+options.offset, width: baseWidth, height: baseHeight-3 }).appendTo(shadow);
+			$('<div class="ui-shadow-color ui-shadow-layer-3"></div>').css({ opacity: options.opacity-0.1, left: 7+options.offset, top: 7+options.offset, width: baseWidth-3, height: baseHeight }).appendTo(shadow);
+			$('<div class="ui-shadow-color ui-shadow-layer-4"></div>').css({ opacity: options.opacity, left: 6+options.offset, top: 6+options.offset, width: baseWidth-1, height: baseHeight-1 }).appendTo(shadow);
+			
+			//If we have a color, use it
+			if(options.color)
+				$("div.ui-shadow-color", shadow).css("background-color", options.color);
 			
 			//Copy the original z-index and position to the clone
 			shadow.css({
@@ -28,25 +39,30 @@
 				top: cur.css("top"),
 				right: cur.css("right"),
 				bottom: cur.css("bottom"),
-				width: cur.outerWidth()+options.offset,
-				height: cur.outerHeight()+options.offset,
+				width: baseWidth,
+				height: baseHeight,
 				marginLeft: cur.css("marginLeft"),
 				marginRight: cur.css("marginRight"),
 				marginBottom: cur.css("marginBottom"),
 				marginTop: cur.css("marginTop")
 			});
+
+
+			if(cur.css("left") != "auto") shadow.css({ left: cur.css("left") });
+			if(cur.css("right") != "auto") shadow.css({ right: cur.css("right") });
+			if(cur.css("top") != "auto") shadow.css({ top: cur.css("top") });
+			if(cur.css("bottom") != "auto") shadow.css({ bottom: cur.css("bottom") });
 			
 			//Remove these values from the inner node
 			cur.css({
-				position: "relative",
-				left: -6-options.offset,
-				top: -6-options.offset,
-				right: 0,
-				bottom: 0,
+				position: "absolute",
+				left: 0,
+				top: 0,
 				marginLeft: 0,
 				marginRight: 0,
 				marginBottom: 0,
-				marginTop: 0
+				marginTop: 0,
+				zIndex: 1
 			});
 				
 		});
