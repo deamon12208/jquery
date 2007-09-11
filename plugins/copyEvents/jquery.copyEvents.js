19141646f28fd8476664baa5e2244d49276618b2
brandon.aaron <brandon.aaron@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-09-11 02:59:51 +0000

    Updating copyEvents for 1.2
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3239 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/copyEvents/jquery.copyEvents.js
+++ plugins/copyEvents/jquery.copyEvents.js
@@ -5,85 +5,26 @@
  * $LastChangedDate$
  * $Rev$
  *
- * Version: 1.0.1
+ * Version: @VERSION
  */
 
 (function($) {
 
 $.fn.extend({
-	/**
-	 * Copies event handlers from the first matched
-	 * element passed in from the jQuery object to all
-	 * the current matched elements in the jQuery object.
-	 *
-	 * @name copyEvents
-	 * @param jQuery|String|DOM Element jQuery object to copy events from. Only uses the first matched element.
-	 * @type jQuery
-	 * @cat Plugins/copyEvents
-	 * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
-	 */
 	copyEvents: function(from) {
-		$.event.copy(from, this);
+		$.event.copy( $(from), this );
 		return this;
 	},
 	
-	/** 
-	 * Copies event handlers to all the matched
-	 * elements from the passed in jQuery object from 
-	 * the first matched element in the jQuery object.
-	 *
-	 * @name copyEventsTo
-	 * @param jQuery|String|DOM Element jQuery object to copy events to. Copies to all matched elements.
-	 * @type jQuery
-	 * @cat Plugins/copyEvents
-	 * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
-	 */
 	copyEventsTo: function(to) {
-		$.event.copy(this, to);
+		$.event.copy( this, $(to) );
 		return this;
-	},
-
-	/**
-	 * Does a .clone() and also copies the events.
-	 * 
-	 * @name cloneWithEvents
-	 * @type jQuery
-	 * @param Boolean deep (Optional) Set to false if you don't want to clone all descendant nodes, in addition to the element itself.
-	 * @cat Plugins/copyEvents
-	 * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
-	 */
-	cloneWithEvents: function(deep) {
-		var r = this.clone( deep ).copyEvents( this );
-		
-		if (deep !== false) {
-		// copy descendant events also
-			var $this = this.find('*');
-			r.find('*').each(function(i) {
-				$.event.copy($this[i], this);
-			});
-		}
-		
-		return r;
 	}
 });
 
-/**
- * Logic for copying events from one jQuery object to another.
- *
- * @private	
- * @name jQuery.events.copy
- * @param jQuery|String|DOM Element jQuery object to copy events from. Only uses the first matched element.
- * @param jQuery|String|DOM Element jQuery object to copy events to. Copies to all matched elements.
- * @type undefined
- * @cat Plugins/copyEvents
- * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
- */
 $.event.copy = function(from, to) {
-	from = (from.jquery) ? from : $(from);
-	to   = (to.jquery)   ? to   : $(to);
-	
-	var events;
-	if (!from.size() || !(events = from[0].events || from[0].$events) || !to.size()) return;
+	var events = $.data(from[0], 'events');
+	if ( !from.size() || !events || !to.size() ) return;
 
 	to.each(function() {
 		for (var type in events)
--- plugins/copyEvents/jquery.copyEvents.pack.js
+++
@@ -1,7 +0,0 @@
-/* Copyright (c) 2006 Brandon Aaron (http://brandonaaron.net)
- * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) 
- * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
- *
- * Version: 1.0.1
- */
-eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(2($){$.v.o({k:2(a){$.4.5(a,1);6 1},q:2(a){$.4.5(1,a);6 1},p:2(b){3 r=1.n(b).k(1);l(b!==s){3 a=1.j(\'*\');r.j(\'*\').h(2(i){$.4.5(a[i],1)})}6 r}});$.4.5=2(d,c){d=(d.f)?d:$(d);c=(c.f)?c:$(c);3 e;l(!d.9()||!(e=d[0].7||d[0].$7)||!c.9())6;c.h(2(){8(3 a g e)8(3 b g e[a])$.4.m(1,a,e[a][b],e[a][b].t)})}})(u);',32,32,'|this|function|var|event|copy|return|events|for|size||||||jquery|in|each||find|copyEvents|if|add|clone|extend|cloneWithEvents|copyEventsTo||false|data|jQuery|fn'.split('|'),0,{}))
