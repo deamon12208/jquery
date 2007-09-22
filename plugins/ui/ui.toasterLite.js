63df587ff7f35fda3f2bb6ec66919c2dd0e5c4d7
dmitrig01 <dmitrig01@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-09-22 15:37:04 +0000

    Adding toasterLite
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3439 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ plugins/ui/ui.toasterLite.js
@@ -0,0 +1,32 @@
+$.toasterLite = function(settings) {
+    settings = $.extend({
+        timeout: 3000,
+        title: '',
+        text: '',
+        animationSpeed: 500,
+        location: 'bl'
+    }, settings);
+    
+    var html = '<div class="ui-toasterLite ui-resizable"><div calss="ui-toasterLite-container">';
+    if(!!settings.title) {
+        html += '<div class="ui-toasterLite-titlebar"><span class="ui-toasterLite-title">'+ settings.title +'</span></div>';
+    }
+    html += '<div class="ui-toasterLite-content">'+ settings.text +'</div></div>'+
+    '<div class="ui-toasterLite-n  ui-toasterLite-corner"/>'+
+    '<div class="ui-toasterLite-s  ui-toasterLite-corner"/>'+
+    '<div class="ui-toasterLite-e  ui-toasterLite-corner"/>'+
+    '<div class="ui-toasterLite-w  ui-toasterLite-corner"/>'+
+    '<div class="ui-toasterLite-ne ui-toasterLite-corner"/>'+
+    '<div class="ui-toasterLite-se ui-toasterLite-corner"/>'+
+    '<div class="ui-toasterLite-sw ui-toasterLite-corner"/>'+
+    '<div class="ui-toasterLite-nw ui-toasterLite-corner"/>'+
+    '</div>';
+    html = $(html);
+    
+    $toasters = $('body > .ui-toasterLite');
+    if ($toasters.length) $toasters.after(html);
+    $('body').append(html);
+    
+    html.slideDown(settings.animationSpeed);
+    window.setTimeout(function() { html.slideUp(settings.animationSpeed, function(){ html.remove(); }); }, settings.timeout);
+}
