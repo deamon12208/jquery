fdf599047fca755683edb7730d330915da68a4db
dmitrig01 <dmitrig01@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-09-22 19:02:43 +0000

    Positions and manual closing
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3443 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/ui/ui.toasterLite.js
+++ plugins/ui/ui.toasterLite.js
@@ -3,13 +3,18 @@ $.toasterLite = function(settings) {
         timeout: 3000,
         title: '',
         text: '',
-        animationSpeed: 500,
-        location: 'bl'
+        animationSpeed: 3000,
+        location: 'tr',
+        container: 'body',
+        manualClose: true
     }, settings);
     
-    var html = '<div class="ui-toasterLite ui-resizable"><div calss="ui-toasterLite-container">';
-    if(!!settings.title) {
-        html += '<div class="ui-toasterLite-titlebar"><span class="ui-toasterLite-title">'+ settings.title +'</span></div>';
+    var html = '<div class="ui-toasterLite ui-resizable ui-toasterLite-pos-'+settings.location +'"><div calss="ui-toasterLite-container">';
+    if(!!settings.title || settings.manualClose) {
+        html += '<div class="ui-toasterLite-titlebar">';
+        if (!!settings.title) html += '<span class="ui-toasterLite-title">'+ settings.title +'</span>';
+        if (settings.manualClose) html += '<div class="ui-toasterLite-titlebar-close"></div>';
+        html += '</div>';
     }
     html += '<div class="ui-toasterLite-content">'+ settings.text +'</div></div>'+
     '<div class="ui-toasterLite-n ui-toasterLite-corner"/>'+
@@ -23,9 +28,14 @@ $.toasterLite = function(settings) {
     '</div>';
     html = $(html);
     
-    $toasters = $('body > .ui-toasterLite');
-    if ($toasters.length) $toasters.after(html[0]);
-    else $('body').append(html);
+    $toasters = $(settings.container).children('.ui-toasterLite');
+    if ($toasters.length) $toasters.before(html[0]);
+    else $(settings.container).append(html);
     
-    window.setTimeout(function() { html.slideUp(settings.animationSpeed, function(){ html.remove(); }); }, settings.timeout);
+    $(html).find('.ui-toasterLite-titlebar-close').hover(function() { $(this).addClass('ui-toasterLite-titlebar-close-hover') }, function() { $(this).removeClass('ui-toasterLite-titlebar-close-hover'); }).click(function() {
+        html.fadeOut(settings.animationSpeed, function(){ html.remove(); });
+    });
+    
+    window.setTimeout(function() { html.slideDown(); }, 0);
+    if (!settings.manualClose) window.setTimeout(function() { html.slideUp(settings.animationSpeed, function(){ html.remove(); }); }, settings.timeout);
 }
