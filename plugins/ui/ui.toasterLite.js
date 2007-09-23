d4bd31388bf9fbfd89e877feff90d84d83feca8a
dmitrig01 <dmitrig01@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-09-23 00:18:03 +0000

    Small changes
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3445 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/ui/ui.toasterLite.js
+++ plugins/ui/ui.toasterLite.js
@@ -6,7 +6,7 @@ $.toasterLite = function(settings) {
         animationSpeed: 3000,
         location: 'tr',
         container: 'body',
-        manualClose: true
+        manualClose: false
     }, settings);
     
     var html = '<div class="ui-toasterLite ui-resizable ui-toasterLite-pos-'+settings.location +'"><div calss="ui-toasterLite-container">';
@@ -29,8 +29,16 @@ $.toasterLite = function(settings) {
     html = $(html);
     
     $toasters = $(settings.container).children('.ui-toasterLite');
-    if ($toasters.length) $toasters.before(html[0]);
-    else $(settings.container).append(html);
+    switch (settings.location) {
+        case 'tr':
+        case 'tl':
+            if ($toasters.length) $toasters.after(html[0]);
+            else $(settings.container).append(html);
+            break;
+        default:
+            if ($toasters.length) $toasters.before(html[0]);
+            else $(settings.container).append(html);
+    }
     
     $(html).find('.ui-toasterLite-titlebar-close').hover(function() { $(this).addClass('ui-toasterLite-titlebar-close-hover') }, function() { $(this).removeClass('ui-toasterLite-titlebar-close-hover'); }).click(function() {
         html.fadeOut(settings.animationSpeed, function(){ html.remove(); });
