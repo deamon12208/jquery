c0ec59def9a3047daff837d37cc87ed02c5e91b4
miksago <miksago@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-10-05 07:21:40 +0000

    Fixed Themes, Fixed Themer, and made default theme flora in toolbars test
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3558 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/ui/tests/js/themer.js
+++ plugins/ui/tests/js/themer.js
@@ -1,32 +1,46 @@
 (function($) {
 $.fn.themer = function(supported){
-  var el = this;
-  // Add Your Themes HERE
-  var themes = ["light", "dark", "flora", "tango", "warm"];
+  // Variables:
+    var el = this;
+    var _t = 0;
   
-  var support = function(i){
-    if(supported.constructor == Array){
-      if(themes.join().indexOf(supported[i]) >= 0){
-        return supported[i];
-      }else{
-        return themes[0];
+  // THEMES: you can add one here.
+    var themes = ["light", "dark", "flora", "tango", "warm"];
+    
+  // Support Checker
+    var support = function(i){
+      if(supported.constructor == Array){
+        if(themes.indexOf(supported[i]) != -1){
+          return supported[i];
+        }else{
+          return themes[0];
+        }
       }
+      else{return themes[0];}
     }
-    else{return themes[0];}
+    
+  // Check Support Of Theme
+    var theme = support(_t);
+  
+  // Initialize..
+    $("body").addClass(theme);
+  // Build...
+    $("body").prepend('<div id="theme"><span>Theme: <a href="#">'+theme+'</a></span></div>');
+    var changer = $("#theme a");
+  // Hook
+    $(changer).bind("click", function(){
+    // Remove Existing
+      $("body").removeClass(theme);
+    // Goto Next Theme
+      _t++;
+    // If next theme has a greater index then the supported themes, goto 0.
+      if(_t == supported.length) _t = 0;
+    // Check Support of Theme
+      theme = support(_t);
+    // Add the ClassName of that theme
+      $("body").addClass(theme);
+    // Update The Text.
+      $(this).text(theme);
+    });
   }
-  var _t = 0;
-  var theme = support(_t);
-  
-  $("body").prepend('<div id="theme"><span>Theme: <a href="#">'+theme+'</a></span></div>');
-  
-  var changer = $("#theme a");
-  $(changer).bind("click", function(){
-    $(el).removeClass(theme);
-    ++_t;
-    theme = support(_t);
-    $(el).addClass(theme);
-    $(this).text(theme);
-    if(_t == supported.length) _t = 0;
-  });
-}
 })(jQuery); 
