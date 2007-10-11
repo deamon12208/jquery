f21a58e6c875ac7e96c1c64cb3ba3eb597beb38b
paul.bakaus <paul.bakaus@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-10-11 13:34:59 +0000

    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3627 c715fcbe-d12f-0410-84c4-316a508785bb

--- fx/experimental/fx.shrink.js
+++ fx/experimental/fx.shrink.js
@@ -1,23 +1,35 @@
 (function($) {
 
-	var save = function(el,props) {
-		var obj = {};
-		for(var i in props) obj[i] = el.css(i) == "auto" ? 0 : el.css(i);
-		return obj;
-	};
+	//Store some stuff for easy reference later on
+	var restoreThis = [
+		"width", "height", "fontSize", "left", "top",
+		"borderLeftWidth", "borderRightWidth", "borderTopWidth", "borderBottomWidth",
+		"paddingLeft", "paddingRight", "paddingTop", "paddingBottom",
+		"marginTop", "marginLeft", "marginBottom", "marginRight"
+	];
 	
 	$.fn.grow = function(speed, easing, callback) {
 		this.each(function() {
 
-			$('*', this).each(function() {
-				if(!$.data(this, 'fx.scale.original')) return;
-				var props = $.data(this, 'fx.scale.original');
-				$(this).animate(props, speed, easing, callback); //TODO: Set overflow back to original state
+			var el = $(this); if(!$.data(this, 'fx.storage.width')) return;
+			
+			//Grow the children
+			$('*', el).each(function() {
+				
+				var ani = {};
+				if($.data(this, 'fx.storage.width')) ani["width"] = $.data(this, 'fx.storage.width');
+				if($.data(this, 'fx.storage.height')) ani["height"] = $.data(this, 'fx.storage.height');
+				
+				if(!ani["width"] && !ani["height"]) return;
+				$(this).animate(ani, speed, easing, function() { $.fx.restore($(this), ["overflow"]); });
+				
 			});
 
-			if(!$.data(this, 'fx.scale.original')) return;
-			var props = $.data(this, 'fx.scale.original');
-			$(this).animate(props, speed, easing, callback); //TODO: Set overflow back to original state
+			//Grow the parent
+			el.animate($.fx.restore(el, restoreThis, true), speed, easing, function() {
+				$.fx.restore(el, ["overflow"]);
+				if(callback) callback.apply(this, arguments);
+			});
 
 		});
 	}
@@ -26,27 +38,26 @@
 		
 		this.each(function() {
 	
-			if($(this).css("position") == "static" || $(this).css("position") == '') $(this).css("position", "relative");
-	
-			$('*', this).each(function() {
-				var props = { width: 0, height: 0 };
-				$.data(this, 'fx.scale.original', save($(this),props)); //Store the original properties in expando
-				$.data(this, 'fx.scale.overflow', $(this).css("overflow")); //Store the original properties in expando					
-				$(this).css("overflow", "hidden").animate(props, speed, easing, callback);
+			var el = $(this); $.fx.relativize(el);
+
+			//Shrink the children	
+			$('*', el).each(function() {
+				var cur = $(this); if(cur.css("width") == "auto" || cur.css("height") == "auto") return; //Don't continue if 'auto' sized element
+				$.fx.save(cur, ["width", "height", "overflow"]);				
+				$(this).css("overflow", "hidden").animate({ width: 0, height: 0 }, speed, easing);
 			});
 			
-			var props = {
-				fontSize: 0,
-				width: 0, height: 0,
+
+			//Save values to restore them later again
+			$.fx.save(el, restoreThis.concat(["overflow"]));
+			
+			//Shrink the parent
+			el.css("overflow", "hidden").animate({
+				fontSize: 0, width: 0, height: 0, left: '+='+(el.width() / 2), top: '+='+(el.height() / 2),
 				borderLeftWidth: 0, borderRightWidth: 0, borderTopWidth: 0, borderBottomWidth: 0,
 				paddingLeft: 0, paddingRight: 0, paddingTop: 0, paddingBottom: 0,
-				marginTop: 0, marginLeft: 0, marginBottom: 0, marginRight: 0,
-				left: '+='+($(this).width() / 2), top: '+='+($(this).height() / 2)
-			};
-			
-			$.data(this, 'fx.scale.original', save($(this),props)); //Store the original properties in expando
-			$.data(this, 'fx.scale.overflow', $(this).css("overflow")); //Store the original properties in expando
-			$(this).css("overflow", "hidden").animate(props, speed, easing, callback);	
+				marginTop: 0, marginLeft: 0, marginBottom: 0, marginRight: 0
+			}, speed, easing, callback);	
 			
 		});
 		
