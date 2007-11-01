95251399cc829909948af31539962d031a39a1cc
grabanski <grabanski@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-11-01 01:24:59 +0000

    Finished updating demo page.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3771 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/uidatepicker/demo/ui.datepicker.demo.js
+++ plugins/uidatepicker/demo/ui.datepicker.demo.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 	tabs.init();
 	// replace script tags with HTML code
 	$(".demojs").each(function () {
-		$(this).before( '<div><a class="togglecode" href="#">Show Code Example</a><br /><pre style="display:none"><code>' + $(this).html() + "</code></pre></div>" );
+		$(this).before( '<div><a class="togglecode" href="#">Show Code Example</a><br /><pre style="display:none; padding-top:0 !important;"><code>' + $(this).html() + "</code></pre></div>" );
 		eval( $(this).html() );
 	});
 	$("a.togglecode")
@@ -22,37 +22,6 @@ $(document).ready(function () {
 		$('#l10nDatepicker').datepicker();
 		localise();
 	}
-	// Date range
-	$('#rangeSelect').datepicker({rangeSelect: true});
-	$('#rangeSelect2Months').datepicker({rangeSelect: true, numberOfMonths: 2});
-	$('#rangeSelect6Months').datepicker({rangeSelect: true, numberOfMonths: [2, 3],
-		stepMonths: 3, prevText: '&lt;&lt; Previous Months', nextText: 'Next Months &gt;&gt;'});
-	// Miscellaneous
-	$('#openDateJan01').datepicker({defaultDate: new Date(2007, 1 - 1, 1)});
-	$('#openDatePlus7').datepicker({defaultDate: +7});
-	$('#noCentury').datepicker({useShortYear: true});
-	$('#addSettings').datepicker({closeAtTop: false,
-		showOtherMonths: true, onSelect: alertDate});
-	$('#linkedDates').datepicker({minDate: new Date(2001, 1 - 1, 1),
-		maxDate: new Date(2010, 12 - 1, 31), beforeShow: readLinked,
-		onSelect: updateLinked});
-	$('#selectMonth,#selectYear').change(checkLinkedDays);
-	// Reconfigure
-	$('#reconfigureCal').datepicker();
-	$('.inlineConfig').datepicker();
-	// Inline
-	$('.dateInline').datepicker({onSelect: updateInlineRange});
-	var nextWeek = new Date();
-	nextWeek.setDate(nextWeek.getDate() + 7);
-	$.datepicker.setDateFor('.dateInline:last', nextWeek);
-	updateInlineRange();
-	$('#rangeInline').datepicker({rangeSelect: true, rangeSeparator: ' to ',
-		numberOfMonths: 2, onSelect: updateInlineRange2});
-	var lastWeek = new Date();
-	lastWeek.setDate(lastWeek.getDate() - 7);
-	$.datepicker.setDateFor('#rangeInline', lastWeek, nextWeek);
-	updateInlineRange2();
-	$('#datepicker_div_26').width(370); // Unfortunately not automatic
 	// Stylesheets
 	$('#altStyle').datepicker({buttonImage: 'img/calendar2.gif'});
 	$('#button3').click(function() { 
@@ -60,11 +29,6 @@ $(document).ready(function () {
 		setAltDateFromDialog, {prompt: 'Choose a date', speed: ''});
 	});
 });
-
-// Display a date selected in a "dialog"
-function setDateFromDialog(date) {
-	$('#invokeDialog').val(date);
-}
 
 // Load and apply a localisation package for the date picker
 function localise() {
@@ -81,65 +45,11 @@ function getDate(value) {
 		new Date(parseInt(fields[2], 10), parseInt(fields[1], 10) - 1, parseInt(fields[0], 10)));
 }
 
-// Demonstrate the callback on select
-function alertDate(date) {
-	alert('The date is ' + date);
-}
-
-// Prepare to show a date picker linked to three select controls
-function readLinked() {
-	$('#linkedDates').val($('#selectDay').val() + '/' +
-		$('#selectMonth').val() + '/' + $('#selectYear').val());
-	return {};
-}
-
-// Update three select controls to match a date picker selection
-function updateLinked(date) {
-	$('#selectDay').val(date.substring(0, 2));
-	$('#selectMonth').val(date.substring(3, 5));
-	$('#selectYear').val(date.substring(6, 10));
-}
-
-// Prevent selection of invalid dates through the select controls
-function checkLinkedDays() {
-	var daysInMonth = 32 - new Date($('#selectYear').val(),
-		$('#selectMonth').val() - 1, 32).getDate();
-	$('#selectDay option').attr('disabled', '');
-	$('#selectDay option:gt(' + (daysInMonth - 1) +')').attr('disabled', 'disabled');
-	if ($('#selectDay').val() > daysInMonth) {
-		$('#selectDay').val(daysInMonth);
-	}
-}
-
-// Change the speed at which the date picker appears
-function setSpeed(select) {
-	$.datepicker.reconfigureFor('#reconfigureCal',
-		{speed: select.options[select.selectedIndex].value});
-}
-
 // Demonstrate a callback from inline configuration
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 function showDay(input) {
 	var date = getDate(input.value);
 	$('#inlineDay').empty().html(date ? days[date.getDay()] : 'blank');
-}
-
-// Synchronise two inline date pickers working together as a date range
-function updateInlineRange() {
-	var inlineFrom = $('#inlineFrom')[0];
-	var inlineTo = $('#inlineTo')[0];
-	var dateFrom = $.datepicker.getDateFor(inlineFrom);
-	var dateTo = $.datepicker.getDateFor(inlineTo);
-	$('#inlineRange').val(formatDate(dateFrom) + ' to ' + formatDate(dateTo));
-	$.datepicker.reconfigureFor(inlineFrom, {maxDate: dateTo}).
-		reconfigureFor(inlineTo, {minDate: dateFrom});
-}
-
-// Display the date range from a multi-month inline date picker
-function updateInlineRange2(dateStr) {
-	var dates = $.datepicker.getDateFor('#rangeInline');
-	$('#inlineRange2').val(dateStr ? dateStr :
-		formatDate(dates[0]) + ' to ' + formatDate(dates[1]));
 }
 
 // Format a date for display
