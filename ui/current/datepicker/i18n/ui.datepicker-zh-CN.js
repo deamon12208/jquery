2160580ea7a1a3cf008987074883c801ac9efa47
kbwood.au <kbwood.au@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-11-13 13:56:49 +0000

    Updated day and month names
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3817 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/current/datepicker/i18n/ui.datepicker-zh-CN.js
+++ ui/current/datepicker/i18n/ui.datepicker-zh-CN.js
@@ -1,14 +1,14 @@
-﻿/* Chinese initialisation for the jQuery UI date picker plugin. */
+/* Chinese initialisation for the jQuery UI date picker plugin. */
 /* Written by Cloudream (cloudream@gmail.com). */
 $(document).ready(function(){
 	$.datepicker.regional['zh-CN'] = {clearText: '清除', closeText: '关闭',
 		prevText: '&lt;上月', nextText: '下月&gt;', 
 		currentText: '今天', weekHeader: '周', 
 		dayNamesMin: ['日','一','二','三','四','五','六'],
-		dayNamesShort: ['日','一','二','三','四','五','六'],
-		dayNames: ['日','一','二','三','四','五','六'],
-		monthNamesShort: ['一月','二月','三月','四月','五月','六月',
-		'七月','八月','九月','十月','十一月','十二月'],
+		dayNamesShort: ['周日','周一','周二','周三','周四','周五','周六'],
+		dayNames: ['星期日','星期一','星期二','星期三','星期四','星期五','星期六'],
+		monthNamesShort: ['一','二','三','四','五','六',
+		'七','八','九','十','十一','十二'],
 		monthNames: ['一月','二月','三月','四月','五月','六月',
 		'七月','八月','九月','十月','十一月','十二月'],
 		dateFormat: 'yy-mm-dd', firstDay: 1};
