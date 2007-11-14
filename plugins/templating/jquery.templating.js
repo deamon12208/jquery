ebf5f0efaeedced86cc762d33331b04afb049680
wycats <wycats@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-11-14 06:35:29 +0000

    Templating finally works well
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3819 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/templating/jquery.templating.js
+++ plugins/templating/jquery.templating.js
@@ -41,37 +41,28 @@
 */
 
 (function ($) {
-    $.makeTemplate = function(template) {
-        var code = "with (_context) { return \'" + template
-          .replace(/\\/g, "\\\\")
-          .replace(/'/g, "\\'")
-          .replace(/\n/g, "\\n")
-          .replace(/<!(--)?\[CDATA\[(.*)]]((--)?>|\&gt;)/, '$2')          
-          .replace(/\{\{(.*?)\}\}/g, "' + $1 + '")
-        + "\' }";
-        return new Function("_context", code);
-    };
+  $.makeTemplate = function (template, begin, end) {
+    var rebegin = begin.replace(/([\]{}[\\])/g, '\\$1');
+    var reend = end.replace(/([\]{}[\\])/g, '\\$1');
 
-    $.fn.updateTemplate = function (json) {
-      this.each(function () {
-        $(this).html(this.$template(json));
-      });
-    };
-    
-    $.fn.templatize = function() {
-      this.each(function () {
-        this.$template = $.makeTemplate($(this).html());
-        $(this).empty();
-      });
-    };
-    
-    $.fn.loadTemplate = function(href, callback) {
-      return this.each(function() { 
-        var self = this;
-        $.getJSON(href, function(json) {
-          $(self).html(self.$template(json));
-          callback(json);
-        });
-      });
-    };    
+    var code = "try { with (_context) {" +
+      "var _result = '';" +
+        template
+          .replace(/[\t\r\n]/g, ' ')
+          .replace(/^(.*)$/, end + '$1' + begin)
+          .replace(new RegExp(reend + "(.*?)" + rebegin, "g"), function (text) {
+            return text
+              .replace(new RegExp("^" + reend + "(.*)" + rebegin + "$"), "$1")
+              .replace(/\\/g, "\\\\")
+              .replace(/'/g, "\\'")
+              .replace(/^(.*)$/, end + "_result += '$1';" + begin);
+          })
+          .replace(new RegExp(rebegin + "=(.*?)" + reend, "g"), "_result += ($1);")
+          .replace(new RegExp(rebegin + "(.*?)" + reend, "g"), ' $1 ')
+          .replace(new RegExp("^" + reend + "(.*)" + rebegin + "$"), '$1') +
+      "return _result;" +
+    "} } catch(e) { return '' } ";
+
+    return new Function("_context", code);
+  };
 })(jQuery);
