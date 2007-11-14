fbd5e2930c5e574c02823f9706d6677645e2364e
kbwood.au <kbwood.au@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-11-14 13:13:59 +0000

    Add extended day and month names
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3820 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/current/datepicker/i18n/ui.datepicker-ca.js
+++ ui/current/datepicker/i18n/ui.datepicker-ca.js
@@ -1,10 +1,16 @@
 /* Inicialització en català per a l'extenció 'calendar' per jQuery. */
 /* Writers: (joan.leon@gmail.com). */
 $(document).ready(function(){
-	popUpCal.regional['ca'] = {clearText: 'Netejar', closeText: 'Tancar',
-		prevText: '&lt;Ant', nextText: 'Seg&gt;', currentText: 'Avui',
-		dayNames: ['Dg','Dl','Dt','Dc','Dj','Dv','Ds'],
+	$.datepicker.regional['ca'] = {clearText: 'Netejar', closeText: 'Tancar',
+		prevText: '&lt;Ant', nextText: 'Seg&gt;',
+		currentText: 'Avui', weekHeader: 'Sm',
+		dayNamesMin: ['Dg','Dl','Dt','Dc','Dj','Dv','Ds'],
+		dayNamesShort: ['Dug','Dln','Dmt','Dmc','Djs','Dvn','Dsb'],
+		dayNames: ['Diumenge','Dilluns','Dimarts','Dimecres','Dijous','Divendres','Dissabte'],
+		monthNamesShort: ['Gen','Feb','Mar','Abr','Mai','Jun',
+		'Jul','Ago','Set','Oct','Nov','Dec'],
 		monthNames: ['Gener','Febrer','Mar&ccedil;','Abril','Maig','Juny',
-		'Juliol','Agost','Setembre','Octubre','Novembre','Decembre']};
-	popUpCal.setDefaults(popUpCal.regional['ca']);
+		'Juliol','Agost','Setembre','Octubre','Novembre','Decembre'],
+		dateFormat: 'mm/dd/yy', firstDay: 0};
+	$.datepicker.setDefaults($.datepicker.regional['ca']);
 });
--- ui/current/datepicker/i18n/ui.datepicker-da.js
+++ ui/current/datepicker/i18n/ui.datepicker-da.js
@@ -2,10 +2,15 @@
 /* Written by Jan Christensen ( deletestuff@gmail.com). */
 $(document).ready(function(){
     $.datepicker.regional['da'] = {clearText: 'Nulstil', closeText: 'Luk',
-        prevText: '&laquo;Tilbage', nextText: 'Frem&raquo;', currentText: 'Idag', 
-        weekHeader: 'Ve', dayNames: ['Sø','Ma','Ti','On','To','Fr','Lø'],
+        prevText: '&laquo;Tilbage', nextText: 'Frem&raquo;',
+		currentText: 'Idag', weekHeader: 'Ve',
+		dayNamesMin: ['Sø','Ma','Ti','On','To','Fr','Lø'],
+		dayNamesShort: ['Søn','Man','Tir','Ons','Tor','Fre','Lør'],
+		dayNames: ['Søndag','Mandag','Tirsdag','Onsdag','Torsdag','Fredag','Lørdag'],
+        monthNamesShort: ['Jan','Feb','Mar','Apr','Maj','Jun', 
+        'Jul','Aug','Sep','Okt','Nov','Dec'],
         monthNames: ['Januar','Februar','Marts','April','Maj','Juni', 
         'Juli','August','September','Oktober','November','December'],
-        dateFormat: 'DMY-', firstDay: 0};
+        dateFormat: 'dd-mm-yy', firstDay: 0};
     $.datepicker.setDefaults($.datepicker.regional['da']); 
 });
