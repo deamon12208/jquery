757c0fe952073bed6a0c3007b9a2b915824e45d4
aaronchi <aaronchi@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-11-19 18:53:36 +0000

    updates to shake
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3861 c715fcbe-d12f-0410-84c4-316a508785bb

--- fx/experimental/fx.shake.js
+++ fx/experimental/fx.shake.js
@@ -11,7 +11,7 @@
       // Set options
       var times = o.options.times || 3;
       speed = o.speed || 75;
-      var direction = o.options.direction || 'right';
+      var direction = o.options.direction || 'left';
       var s = $.ec.findSides(el), refs = { left: s[0], right: s[0], up: s[1], down: s[1] }, ref = refs[direction];
       props.push(ref); // Add side to props
       var motion = (direction == 'up' || direction == 'left') ? 'neg' : 'pos';
@@ -25,20 +25,16 @@
       
       // Animate
       var animation = {};
+      var animation1 = {};
+      var animation2 = {};
       animation[ref] = (motion == 'pos' ? '-=' : '+=')  + distance;
+      animation1[ref] = (motion == 'pos' ? '+=' : '-=')  + distance * 2;
+      animation2[ref] = (motion == 'pos' ? '-=' : '+=')  + distance * 2;
       el.animate(animation, speed, o.options.easing);
-      
       for (var i = 1; i < times; i++) {
-        var animation1 = {};
-        var animation2 = {};
-        animation1[ref] = (motion == 'pos' ? '+=' : '-=')  + distance * 2;
-        animation2[ref] = (motion == 'pos' ? '-=' : '+=')  + distance * 2;
         el.animate(animation1, speed * 2, o.options.easing).animate(animation2, speed * 2, o.options.easing)
       };
-      
-      var animation = {};
-      animation[ref] = (motion == 'pos' ? '+=' : '-=')  + distance;
-      el.animate(animation, speed, o.options.easing, function(){
+      el.animate(animation1, speed * 2, o.options.easing).animate(animation, speed, o.options.easing, function(){
         $.ec.restore(el, props);
         if(o.callback) callback.apply(this, arguments);
       });
