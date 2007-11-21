4b4146140dd2eaee44d179ff673df7c656a2a212
aaronchi <aaronchi@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-11-21 18:37:00 +0000

    fixed
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3892 c715fcbe-d12f-0410-84c4-316a508785bb

--- fx/experimental/fx.blind.js
+++ fx/experimental/fx.blind.js
@@ -3,47 +3,6 @@
   $.ec.blind = function(o) {
 
     this.each(function() {
-    
-      // Create element
-      var el = $(this);
-      
-      // Create a wrapper
-      el.wrap('<div id="fxWrapper"></div>');
-      var wrapper = el.parent();
-      wrapper.css({overflow: 'hidden', height: el.outerHeight(), width: el.outerWidth()});
-      
-      // Set options
-      var mode = o.options.mode || 'hide';
-      var direction = o.options.direction || 'vertical';
-      var ref = (direction == 'vertical') ? 'height' : 'width';
-      var distance = (direction == 'vertical') ? wrapper.height() : wrapper.width();
-      
-      // Adjust
-      if(mode == 'show') wrapper.css(ref, 0);
-      el.show();
-      
-      // Animation
-      var animation = {};
-      animation[ref] = mode == 'show' ? distance : 0;
-      
-      // Animate
-      wrapper.animate(animation, o.speed, o.options.easing, function() {
-        if(mode == 'hide') el.hide();
-        wrapper.replaceWith(el);
-        if(o.callback) o.callback.apply(this, arguments);
-      });   
-  
-    });
-    
-  }
-  
-})(jQuery);
-
-(function($) {
-  
-  $.ec.blind = function(o) {
-
-    this.each(function() {
 
       // Create element
       var el = $(this);
