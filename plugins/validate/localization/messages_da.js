9d0eb560357658cdf15fdf185a620da1b859d0ae
joern.zaefferer <joern.zaefferer@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-11-24 12:30:53 +0000

    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3914 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ plugins/validate/localization/messages_da.js
@@ -0,0 +1,21 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Language: DA
+ * Skipped date/dateISO/number.
+ */
+jQuery.extend(jQuery.validator.messages, {
+	required: "Dette felt er påkrævet.",
+	maxLength: jQuery.format("Indtast højst {0} tegn."),
+	minLength: jQuery.format("Indtast mindst {0} tegn."),
+	rangeLength: jQuery.format("Indtast mindst {0} og højst {1} tegn."),
+	email: "Indtast en gyldig email-adresse.",
+	url: "Indtast en gyldig URL.",
+	dateDE: "Indtast en gyldig dato.",
+	numberDE: "Indtast et tal.",
+	digits: "Indtast kun cifre.",
+	equalTo: "Indtast den samme værdi igen.",
+	rangeValue: jQuery.format("Angiv en værdi mellem {0} og {1}."),
+	maxValue: jQuery.format("Angiv en værdi der højst er {0}."),
+	minValue: jQuery.format("Angiv en værdi der mindst er {0}."),
+	creditcard: "Indtast et gyldigt kreditkortnummer."
+});
