31b0d80f88775f3c23ed79d7c0e5ed292a4a930b
aaronchi <aaronchi@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-11-27 23:12:41 +0000

    added fade - why not?
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3954 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ fx/experimental/fx.fade.js
@@ -0,0 +1,32 @@
+(function($) {
+  
+  $.ec.fade = function(o) {
+
+    this.each(function() {
+      
+      // Create element
+      var el = $(this), props = ['opacity'];
+      
+      // Set options
+      var mode = o.options.mode || 'effect'; // Default mode
+      var opacity = o.options.opacity || 0; // Default fade opacity
+      
+      // Adjust
+      $.ec.save(el, props); el.show(); // Save & Show
+      if(mode == 'show') el.css({opacity: 0}); // Shift
+      
+      // Animation
+      var animation = {opacity: mode == 'show' ? 1 : opacity};
+      
+      // Animate
+      el.animate(animation, o.speed, o.options.easing, function() {
+        if(mode == 'hide') el.hide(); // Hide
+        if(mode != 'effect') $.ec.restore(el, props); // Restore
+        if(o.callback) o.callback.apply(this, arguments); // Callback
+      });
+      
+    });
+    
+  }
+  
+})(jQuery);
