a1c3032bbc6dec0b25268734f83cec45ec3002cf
aaronchi <aaronchi@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-11-27 23:15:56 +0000

    don't restore opacity
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@3955 c715fcbe-d12f-0410-84c4-316a508785bb

--- fx/experimental/fx.fade.js
+++ fx/experimental/fx.fade.js
@@ -5,14 +5,14 @@
     this.each(function() {
       
       // Create element
-      var el = $(this), props = ['opacity'];
+      var el = $(this);
       
       // Set options
       var mode = o.options.mode || 'effect'; // Default mode
       var opacity = o.options.opacity || 0; // Default fade opacity
       
       // Adjust
-      $.ec.save(el, props); el.show(); // Save & Show
+      el.show(); // Show
       if(mode == 'show') el.css({opacity: 0}); // Shift
       
       // Animation
@@ -21,7 +21,6 @@
       // Animate
       el.animate(animation, o.speed, o.options.easing, function() {
         if(mode == 'hide') el.hide(); // Hide
-        if(mode != 'effect') $.ec.restore(el, props); // Restore
         if(o.callback) o.callback.apply(this, arguments); // Callback
       });
       
