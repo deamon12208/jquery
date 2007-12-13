c776f0804cd048ae36e4eac7a9ae9ec285f8dea3
kbwood.au <kbwood.au@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-12-13 11:04:01 +0000

    Add status bar translations
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4136 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/current/datepicker/i18n/ui.datepicker-da.js
+++ ui/current/datepicker/i18n/ui.datepicker-da.js
@@ -1,22 +1,22 @@
 ﻿/* Danish initialisation for the jQuery UI date picker plugin. */
 /* Written by Jan Christensen ( deletestuff@gmail.com). */
 $(document).ready(function(){
-    $.datepicker.regional['da'] = {clearText: 'Nulstil', clearStatus: '',
-		closeText: 'Luk', closeStatus: '',
-        prevText: '&laquo;Tilbage', prevStatus: '',
-		nextText: 'Frem&raquo;', nextStatus: '',
-		currentText: 'Idag', currentStatus: '',
+    $.datepicker.regional['da'] = {clearText: 'Nulstil', clearStatus: 'Nulstil den aktuelle dato',
+		closeText: 'Luk', closeStatus: 'Luk uden ændringer',
+        prevText: '&#x3c;Forrige', prevStatus: 'Vis forrige måned',
+		nextText: 'Næste&#x3e;', nextStatus: 'Vis næste måned',
+		currentText: 'Idag', currentStatus: 'Vis aktuel måned',
         monthNames: ['Januar','Februar','Marts','April','Maj','Juni', 
         'Juli','August','September','Oktober','November','December'],
         monthNamesShort: ['Jan','Feb','Mar','Apr','Maj','Jun', 
         'Jul','Aug','Sep','Okt','Nov','Dec'],
-		monthStatus: '', yearStatus: '',
-		weekHeader: 'Ve', weekStatus: '',
+		monthStatus: 'Vis en anden måned', yearStatus: 'Vis et andet år',
+		weekHeader: 'Uge', weekStatus: 'Årets uge',
 		dayNames: ['Søndag','Mandag','Tirsdag','Onsdag','Torsdag','Fredag','Lørdag'],
 		dayNamesShort: ['Søn','Man','Tir','Ons','Tor','Fre','Lør'],
 		dayNamesMin: ['Sø','Ma','Ti','On','To','Fr','Lø'],
-		dayStatus: 'DD', dateStatus: 'D, M d',
+		dayStatus: 'Sæt DD som første ugedag', dateStatus: 'Vælg D, M d',
         dateFormat: 'dd-mm-yy', firstDay: 0, 
-		initStatus: '', isRTL: false};
+		initStatus: 'Vælg en dato', isRTL: false};
     $.datepicker.setDefaults($.datepicker.regional['da']); 
 });
