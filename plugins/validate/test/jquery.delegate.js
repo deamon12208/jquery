7e01a284fbe1211fb329bd49027e91b35d78e202
joern.zaefferer <joern.zaefferer@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-12-19 21:50:13 +0000

    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4243 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/validate/test/jquery.delegate.js
+++ plugins/validate/test/jquery.delegate.js
@@ -1,7 +1,7 @@
 // provides a cross-browser focusin event
 // IE has native support, in other browsers, capture a focus event (doesn't bubble)
 
-// provides delegate(type, delegate, handler plugin for easier event delegation
+// provides delegate(type, delegate, handler) plugin for easier event delegation
 // handler is only called when $(event.target).is(delegate), in the scope of the jQuery-object for event.target 
 ;(function($) {
 	$.extend($.event.special, {
