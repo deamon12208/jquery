4afc3f872d3e3ab0377f743c3603c6c0d6e9aa8a
grabanski@gmail.com <grabanski@gmail.com@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-12-19 04:30:52 +0000

    Added Norwegian and updated Hebrew localization.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4227 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/current/datepicker/i18n/ui.datepicker-he.js
+++ ui/current/datepicker/i18n/ui.datepicker-he.js
@@ -1,11 +1,22 @@
 /* Hebrew initialisation for the UI Datepicker extension. */
 /* Written by Amir Hardon (ahardon at gmail dot com). */
-jQuery(function($){
-	$.datepicker.regional['he'] = {clearText: 'נקה', closeText: 'סגור',
-		prevText: '&lt;קודם', nextText: 'הבא&gt;', currentText: 'היום',
-		dayNames: ['א\'','ב\'','ג\'','ד\'','ה\'','ו\'','שבת'],
+jQuery(document).ready(function(){
+	jQuery.datepicker.regional['he'] = {clearText: 'נקה', clearStatus: '',
+		closeText: 'סגור', closeStatus: '',
+		prevText: '&lt;הקודם', prevStatus: '',
+		nextText: 'הבא&gt;', nextStatus: '',
+		currentText: 'היום', currentStatus: '',
 		monthNames: ['ינואר','פברואר','מרץ','אפריל','מאי','יוני',
-		'יולי','אוגוסט','ספטמבר','אוקטובר','נובמבר','דצמבר']};
-	$.datepicker.setDefaults($.datepicker.regional['he']);
+		'יולי','אוגוסט','ספטמבר','אוקטובר','נובמבר','דצמבר'],
+		monthNamesShort: ['1','2','3','4','5','6',
+		'7','8','9','10','11','12'],
+		monthStatus: '', yearStatus: '',
+		weekHeader: 'Sm', weekStatus: '',
+		dayNames: ['ראשון','שני','שלישי','רביעי','חמישי','שישי','שבת'],
+		dayNamesShort: ['א\'','ב\'','ג\'','ד\'','ה\'','ו\'','שבת'],
+		dayNamesMin: ['א\'','ב\'','ג\'','ד\'','ה\'','ו\'','שבת'],
+		dayStatus: 'DD', dateStatus: 'D, M d',
+		dateFormat: 'dd/mm/yy', firstDay: 0, 
+		initStatus: '', isRTL: true};
+	jQuery.datepicker.setDefaults($.datepicker.regional['he']);
 });
-
