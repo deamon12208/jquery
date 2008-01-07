c5eed597d95192e85e84ac1139a7635cbd8cef87
joern.zaefferer <joern.zaefferer@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-01-07 17:08:59 +0000

    update version to 1.5pre
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4362 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/treeview/jquery.treeview.js
+++ plugins/treeview/jquery.treeview.js
@@ -1,5 +1,5 @@
 /*
- * Treeview 1.4 - jQuery plugin to hide and show branches of a tree
+ * Treeview 1.5pre - jQuery plugin to hide and show branches of a tree
  * 
  * http://bassistance.de/jquery-plugins/jquery-plugin-treeview/
  *
