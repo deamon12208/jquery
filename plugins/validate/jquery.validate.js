cac383ebc3ddbc10b9d340ce4ae06e2796dfccb1
scott.gonzalez <scott.gonzalez@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-01-09 11:40:58 +0000

    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4377 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/validate/jquery.validate.js
+++ plugins/validate/jquery.validate.js
@@ -4,7 +4,7 @@
  * http://bassistance.de/jquery-plugins/jquery-plugin-validation/
  * http://docs.jquery.com/Plugins/Validation
  *
- * Copyright (c) 2006 - 2007 Jörn Zaefferer
+ * Copyright (c) 2006 - 2008 Jörn Zaefferer
  *
  * $Id$
  *
