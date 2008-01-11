97a5f70d2a56ecbc240b1101dd9fa78d55c7f17f
scott.gonzalez <scott.gonzalez@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-01-11 18:21:41 +0000

    Applied patch for #2126 (sync with jquery.accordion.js from [4424]).
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4425 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/current/ui.accordion.js
+++ ui/current/ui.accordion.js
@@ -32,7 +32,12 @@ $.extend($.ui.accordion, {
 				},
 				duration: settings.duration,
 				easing: settings.easing,
-				complete: settings.complete
+				complete: function() {
+					if ( !settings.autoheight ) {
+						settings.toShow.css("height", "auto");
+					}
+					settings.complete();
+				}
 			});
 		},
 		bounceslide: function(settings) {
