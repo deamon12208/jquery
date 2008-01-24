82378c4c0c41e567826023eca6de4ede26cb3326
braeker <braeker@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-01-24 06:41:03 +0000

    Theme detection global method added
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4525 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/current/ui.mouse.js
+++ ui/current/ui.mouse.js
@@ -26,6 +26,23 @@
 				}
 					
 			}	
+		},
+		cssCache: {},
+		css: function(name) {
+			if ($.ui.cssCache[name]) return $.ui.cssCache[name];
+			
+			var tmp = $("<div class='ui-resizable-gen'>").addClass(name).css(
+				{position:'absolute', top:'-5000px', left:'-5000px', display:'block'}
+			).appendTo('body');
+			
+			//Opera and Safari set width and height to 0px instead of auto
+			//Safari returns rgba(0,0,0,0) when bgcolor is not set
+			$.ui.cssCache[name] = !!(
+				(/^[1-9]/.test(tmp.css('height')) || /^[1-9]/.test(tmp.css('width')) || 
+				!/none/.test(tmp.css('backgroundImage')) || !/transparent|rgba\(0, 0, 0, 0\)/.test(tmp.css('backgroundColor')))
+			);
+			try { $('body').get(0).removeChild(tmp.get(0));	} catch(e){}
+			return $.ui.cssCache[name];
 		}
 	});
 	
