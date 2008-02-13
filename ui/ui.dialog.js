bbcab68405a79c50fc2f45a98dbe8f71062ec5e2
scott.gonzalez <scott.gonzalez@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-02-13 14:24:31 +0000

    Fixed z-index setting on activate.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4749 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/ui.dialog.js
+++ ui/ui.dialog.js
@@ -170,9 +170,9 @@
 		};
 
 		this.activate = function() {
-			var maxZ = 0;
+			var maxZ = initZ = 0;
 			$('.ui-dialog:visible').each(function() {
-				maxZ = Math.max(maxZ, parseInt($(this).css("z-index"),10));
+				maxZ = Math.max(maxZ, parseInt($(this).css("z-index"), 10) || initZ);
 			});
 			overlay.$el && overlay.$el.css('z-index', ++maxZ);
 			uiDialog.css("z-index", ++maxZ);
