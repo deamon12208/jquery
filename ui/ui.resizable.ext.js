c9b6f3f71081dcaea624886ad5ae5384bdf466a4
braeker <braeker@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-02-21 21:29:26 +0000

    Grid Plugin alpha
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4799 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/ui.resizable.ext.js
+++ ui/ui.resizable.ext.js
@@ -9,20 +9,24 @@
 	$.ui.plugin.add("resizable", "grid", {
 		
 		resize: function(e, ui) {
-			var o = ui.options, data = ui.instance.cssData, mp = o.originalMousePosition, a = o.axis, ratio = o._aspectRatio || e.shiftKey;
-			var ox = Math.round((e.pageX - mp.left) / o.grid[0]) * o.grid[0], oy = Math.round((e.pageY - mp.top) / o.grid[1]) * o.grid[1];
-			var rehw = /height|width/, reth = /top|height/, 
+			var o = ui.options, data = ui.instance.cssData, cs = o._currentSize, os = o.originalSize, a = o.axis, ratio = o._aspectRatio || e.shiftKey;
+			o.grid = typeof o.grid == "number" ? [o.grid, o.grid] : o.grid;
+			
+			var ox = Math.round((cs.width - os.width) / o.grid[0]) * o.grid[0], oy = Math.round((cs.height - os.height) / o.grid[1]) * o.grid[1];
+			var rehw = /height|width/, reth = /top|height/, relw = /width|left/, 
 			
 			c = function(e, ui, attr) {
-				var ishw = rehw.test(attr), isth = reth.test(attr), ish = /height/.test(attr);
-				if (/^n/.test(a) && ish) oy = -oy;
-				if (/w$/.test(a) && /width/.test(attr)) ox = -ox;
+				var ishw = rehw.test(attr), isth = reth.test(attr), ish = /height/.test(attr), ist = /top/.test(attr);
+				
+				
 				
 				if (ratio) {
 					
-					/*TODO*/
 					
 				}
+				
+				if (/^n/.test(a) && reth.test(attr)) oy = -oy;
+				if (/w$/.test(a) && (relw.test(attr))) ox = -ox;
 				
 				if (data[attr]) data[attr] = o[ ishw ? 'originalSize' : 'originalPosition' ][attr] + (isth ? oy : ox);
 			};
@@ -36,4 +40,3 @@
 	});
 
 })(jQuery);
-
