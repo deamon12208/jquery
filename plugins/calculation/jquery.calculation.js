7e173cab10deea16429fc2c3f57333777dad55cb
dswitzer <dswitzer@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-03-02 21:54:43 +0000

    - Refactored the aggregate methods (since they all use the same core logic) to use the $.extend() method - Added support for negative numbers in the regex) - Added min/max aggregate methods - Added defaults.onParseError and defaults.onParseClear methods to add logic for parsing errors
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4850 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/calculation/jquery.calculation.js
+++ plugins/calculation/jquery.calculation.js
@@ -7,10 +7,18 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  *
- * Revision: 3
- * Version: 0.2
+ * Revision: 4
+ * Version: 0.3
  *
  * Revision History
+ * v0.3
+ * - Refactored the aggregate methods (since they all use the same core logic)
+ *   to use the $.extend() method
+ * - Added support for negative numbers in the regex)
+ * - Added min/max aggregate methods
+ * - Added defaults.onParseError and defaults.onParseClear methods to add logic for
+ *   parsing errors
+ * 
  * v0.2
  * - Fixed bug in sMethod in calc() (was using getValue, should have been setValue)
  * - Added arguments for sum() to allow auto-binding with callbacks
@@ -27,17 +35,23 @@
 
 	// set the defaults
 	var defaults = {
-		// regular expression used to detect numbers
-		reNumbers: /\d+(,\d{3})*(\.\d{1,})?/g,
+		// regular expression used to detect numbers, if you want to force the field to contain
+		// numbers, you can add a ^ to the beginning or $ to the end of the regex to force the
+		// the regex to match the entire string: /^-?\d+(,\d{3})*(\.\d{1,})?$/g
+		reNumbers: /-?\d+(,\d{3})*(\.\d{1,})?/g
 		// the character that indicates the delimiter used for separating numbers (US 1,000 = comma, UK = 1.000 = period)
-		comma: ",",
+		, comma: ","
 		// should the Field plug-in be used for getting values of :input elements?
-		useFieldPlugin: (!!$.fn.getValue)
+		, useFieldPlugin: (!!$.fn.getValue)
+		// a callback function to run when an parsing error occurs
+		, onParseError: null
+		// a callback function to run once a parsing error has cleared
+		, onParseClear: null
 	};
 
 	// set default options
 	$.Calculation = {
-		version: "0.2",
+		version: "0.3",
 		setDefaults: function(options){
 			$.extend(defaults, options);
 		}
@@ -59,8 +73,10 @@
 	 *
 	 */
 	// the parseNumber() method -- break the chain
-	$.fn.parseNumber = function(){
+	$.fn.parseNumber = function(options){
 		var aValues = [];
+		options = $.extend(options, defaults);
+		
 		this.each(
 			function (){
 				var
@@ -70,9 +86,23 @@
 					sMethod = ($el.is(":input") ? (defaults.useFieldPlugin ? "getValue" : "val") : "text"),
 					// parse the string and get the first number we find
 					v = $el[sMethod]().match(defaults.reNumbers, "");
-				// if the value is null, we need use 0, otherwise we take the number
-				// we found and remove any commas
-				v = (v==null) ? 0 : v[0].replace(new RegExp(defaults.comma, "g"), "");
+
+				// if the value is null, use 0
+				if( v == null ){
+					v = 0; // update value
+					// if there's a error callback, execute it
+					if( jQuery.isFunction(options.onParseError) ) options.onParseError.apply($el, [sMethod]);
+					$.data($el[0], "calcParseError", true);
+				// otherwise we take the number we found and remove any commas
+				} else {
+					v = v[0].replace(new RegExp(options.comma, "g"), "");
+					// if there's a clear callback, execute it
+					if( $.data($el[0], "calcParseError") && jQuery.isFunction(options.onParseClear) ){
+						options.onParseClear.apply($el, [sMethod]);
+						// clear the error flag
+						$.data($el[0], "calcParseError", false);
+					} 
+				}
 				aValues.push(parseFloat(v, 10));
 			}
 		);
@@ -179,12 +209,30 @@
 	};
 
 	/*
+	 * Define all the core aggregate functions. All of the following methods
+	 * have the same functionality, but they perform different aggregate 
+	 * functions.
+	 * 
+	 * If this methods are called without any arguments, they will simple
+	 * perform the specified aggregate function and return the value. This
+	 * will break the jQuery chain. 
+	 * 
+	 * However, if you invoke the method with any arguments then a jQuery
+	 * object is returned, which leaves the chain intact.
+	 * 
+	 * 
 	 * jQuery.fn.sum()
-	 *
 	 * returns Number - the sum of all fields
 	 *
-	 * NOTE: Breaks the jQuery chain, since it returns a Number.
+	 * jQuery.fn.avg()
+	 * returns Number - the avg of all fields
 	 *
+	 * jQuery.fn.min()
+	 * returns Number - the minimum value in the field
+	 *
+	 * jQuery.fn.max()
+	 * returns Number - the maximum value in the field
+	 * 
 	 * Examples:
 	 * $("input[@name='price']").sum();
 	 * > This would return the sum of all the fields named price
@@ -197,134 +245,85 @@
 	 * > fields whose name started with "sum" anytime the keyup event is triggered on
 	 * > those field.
 	 *
-	 */
-	// the sum() method -- break the chain
-	$.fn.sum = function(bind, selector){
-		// if no arguments, return the sum() of the values
-		if( arguments.length == 0 )
-			return sum(this.parseNumber());
-
-		// if the selector is an options object, get the options
-		var bSelOpt = selector && selector.constructor == Object && !(selector instanceof jQuery);
-		
-		// TODO: add arguments to allow automatic binding with a callback
-		var opt = bind && bind.constructor == Object ? bind : {
-			  bind: "keyup"
-			, selector: (!bSelOpt) ? selector : null
-			, oncalc: null
-		};
-
-		// if the selector is an options object, extend	the options
-		if( bSelOpt ) opt = jQuery.extend(opt, selector);
-		
-		// if the selector exists, make sure it's a jQuery object
-		if( !!opt.selector ) opt.selector = $(opt.selector);
-		
-		var self = this
-			, sMethod
-			, calcSum = function (){
-				var value = self.sum();
-				// check to make sure we have a selector				
-				if( !!opt.selector ){
-					// determine how to set the value for the selector
-					sMethod = (opt.selector.is(":input") ? (defaults.useFieldPlugin ? "setValue" : "val") : "text");
-					// update the value
-					opt.selector[sMethod](value);
-				}
-				// if there's a callback, run it now
-				if( jQuery.isFunction(opt.oncalc) ) opt.oncalc.apply(self, [value, opt]);
-			};
-		
-		// run the calcSum function now to make sure we're updated
-		calcSum();
-		
-		// bind the calcSum function to run each time a key is pressed
-		return self.bind(opt.bind, calcSum);
-	};
-
-	/*
-	 * jQuery.fn.avg()
-	 *
-	 * returns Number - the sum of all fields
-	 *
-	 * NOTE: Breaks the jQuery chain, since it returns a Number.
-	 *
-	 * Examples:
-	 * $("input[@name='price']").avg();
-	 * > This would return the average of all the fields named price
-	 *
-	 * $("input[@name='price1'], input[@name='price2'], input[@name='price3']").avg();
-	 * > This would return the average of all the fields named price1, price2 or price3
-	 *
-	 * $("input[@name^=sum]").avg("keyup", "#totalSum");
-	 * > This would update the element with the id "totalSum" with the sum of all the 
-	 * > fields whose name started with "sum" anytime the keyup event is triggered on
-	 * > those field.
+	 * NOTE: The syntax above is valid for any of the aggregate functions
 	 *
 	 */
-	// the avg() method -- break the chain
-	$.fn.avg = function(bind, selector){
-		// if no arguments, return the avg() of the values
-		if( arguments.length == 0 )
-			return avg(this.parseNumber());
-
-		// if the selector is an options object, get the options
-		var bSelOpt = selector && selector.constructor == Object && !(selector instanceof jQuery);
-		
-		var opt = bind && bind.constructor == Object ? bind : {
-			  bind: "keyup"
-			, selector: (!bSelOpt) ? selector : null
-			, oncalc: null
-		};
-
-		// if the selector is an options object, extend	the options
-		if( bSelOpt ) opt = jQuery.extend(opt, selector);
-		
-		// if the selector exists, make sure it's a jQuery object
-		if( !!opt.selector ) opt.selector = $(opt.selector);
-		
-		var self = this
-			, sMethod
-			, calcAvg = function (){
-				var value = self.avg();
-				// check to make sure we have a selector				
-				if( !!opt.selector ){
-					// determine how to set the value for the selector
-					sMethod = (opt.selector.is(":input") ? (defaults.useFieldPlugin ? "setValue" : "val") : "text");
-					// update the value
-					opt.selector[sMethod](value);
-				}
-				// if there's a callback, run it now
-				if( jQuery.isFunction(opt.oncalc) ) opt.oncalc.apply(self, [value, opt]);
+	$.each(["sum", "avg", "min", "max"], function (i, method){
+		$.fn[method] = function (bind, selector){
+			// if no arguments, then return the result of the aggregate function
+			if( arguments.length == 0 )
+				return math[method](this.parseNumber());
+	
+			// if the selector is an options object, get the options
+			var bSelOpt = selector && selector.constructor == Object && !(selector instanceof jQuery);
+			
+			// configure the options for this method
+			var opt = bind && bind.constructor == Object ? bind : {
+				  bind: "keyup"
+				, selector: (!bSelOpt) ? selector : null
+				, oncalc: null
 			};
-		
-		// run the calcAvg function now to make sure we're updated
-		calcAvg();
-		
-		// bind the calcAvg function to run each time a key is pressed
-		return self.bind(opt.bind, calcAvg);
-	};
-
+	
+			// if the selector is an options object, extend	the options
+			if( bSelOpt ) opt = jQuery.extend(opt, selector);
+			
+			// if the selector exists, make sure it's a jQuery object
+			if( !!opt.selector ) opt.selector = $(opt.selector);
+			
+			var self = this
+				, sMethod
+				, doCalc = function (){
+					// preform the aggregate function
+					var value = math[method](self.parseNumber(opt));
+					// check to make sure we have a selector				
+					if( !!opt.selector ){
+						// determine how to set the value for the selector
+						sMethod = (opt.selector.is(":input") ? (defaults.useFieldPlugin ? "setValue" : "val") : "text");
+						// update the value
+						opt.selector[sMethod](value);
+					}
+					// if there's a callback, run it now
+					if( jQuery.isFunction(opt.oncalc) ) opt.oncalc.apply(self, [value, opt]);
+				};
+			
+			// perform the aggregate function now, to ensure init values are updated
+			doCalc();
+			
+			// bind the doCalc function to run each time a key is pressed
+			return self.bind(opt.bind, doCalc);
+		}
+	});
+	
 	/*
 	 * Mathmatical functions
 	 */
-	// sum an array
-	var sum = function (a){
-		var total = 0;
-
-		$.each(a, function (i, v){
-			var v = parseFloat(v, 10);
-			total += (isNaN(v) ? 0 : v);
-		});
-
-		// return the values as a comma-delimited string
-		return total;
+	var math = {
+		// sum an array
+		sum: function (a){
+			var total = 0;
+	
+			$.each(a, function (i, v){
+				// we add 0 to the value to ensure we get a numberic value
+				total += parseFloat("0"+v, 10);
+			});
+	
+			// return the values as a comma-delimited string
+			return total;
+		},
+		// average an array
+		avg: function (a){
+			// return the values as a comma-delimited string
+			return math.sum(a)/a.length;
+		},
+		// lowest number in array
+		min: function (a){
+			return Math.min.apply(Math, a);
+		},
+		// highest number in array
+		max: function (a){
+			return Math.max.apply(Math, a);
+		}
 	};
-
-	// average an array
-	var avg = function (a){
-		// return the values as a comma-delimited string
-		return sum(a)/a.length;
-	};
+	
 
 })(jQuery);
