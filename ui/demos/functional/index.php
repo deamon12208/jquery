c220a9e33a2f4788182f46ef8aca9bc5d77c8664
miksago <miksago@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-03-15 08:07:43 +0000

    added else statement, if($demo)
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4930 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/demos/functional/index.php
+++ ui/demos/functional/index.php
@@ -10,22 +10,22 @@ $demo = $_GET['demo'].".php";
 		<meta http-equiv="Content-Language" content="en" />
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>jQuery UI Demo</title>
-		
+
 		<?php
 			include_once("init.php");
 		?>
-		
+
 		<link rel="stylesheet" href="css/demo.css" type="text/css" />
-		
+
 	</head>
-	
+
 	<body>
 
 	<div id="leftbar">
 		<div class="innertube" style="margin: 2px;">
-			
+
 			<div class="menutitle">Components</div>
-			
+
 			<a class="link" href="?demo=ui.resizable">Resizable</a>
 			<a class="link" href="?demo=ui.draggable">Draggable</a>
 			<a class="link" href="?demo=ui.droppable">Droppable</a>
@@ -35,33 +35,41 @@ $demo = $_GET['demo'].".php";
 			<a class="link" href="?demo=ui.dialog">Dialog</a>
 			<a class="link" href="?demo=ui.slider">Slider</a>
 			<a class="link" href="?demo=ui.tabs">Tabs</a>
-		
+
 			<br/><br/>
-			
+
 		</div>
 	</div>
-	
+
 	<div id="maincontent">
-		
+
 		<div id="topbar">
 			<h1>jQuery UI 1.5 - Demos</h1>
 		</div>
-		
+
 		<div id="main" class="innertube" style="padding: 10px;">
-			<? if (file_exists($demo)) include($demo); ?>
+			<?
+                if (file_exists($demo)){
+                    include($demo);
+                }
+                else {
+                    echo("<p>Welcome to jQuery UI Demo's, an interactive page for looking at and playing with each jQuery UI component.</p>");
+                }
+
+            ?>
 		</div>
 
 	</div>
-		
+
 	</body>
 
 </html>
 
 <script type="text/javascript">
-	
+
 	$(function() {
-		
-		
+
+
 	});
-	
+
 </script>
