4509f28ac07349a8889797177d8b700789ea32c5
miksago <miksago@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-03-15 09:20:50 +0000

    now uses init.php
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4936 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/demos/functional/v2/index.php
+++ ui/demos/functional/v2/index.php
@@ -5,32 +5,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
+    <meta http-equiv="Content-Language" content="en" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>jQuery UI: Demo Viewer</title>
 
     <style type="text/css">
         @import "css/demo.css";
     </style>
-
-
-    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-    <script src="../../../../plugins/dimensions/jquery.dimensions.js" type="text/javascript"></script>
-    <script src="../../../../plugins/corner/jquery.corner.js" type="text/javascript"></script>
-    <!-- jQuery UI -->
-    <script src="../../../../ui/ui.mouse.js" type="text/javascript"></script>
-    <script src="../../../../ui/ui.draggable.js" type="text/javascript"></script>
-    <script src="../../../../ui/ui.draggable.ext.js" type="text/javascript"></script>
-    <script src="../../../../ui/ui.droppable.js" type="text/javascript"></script>
-    <script src="../../../../ui/ui.droppable.ext.js" type="text/javascript"></script>
-    <script src="../../../../ui/ui.resizable.js" type="text/javascript"></script>
-    <script src="../../../../ui/ui.resizable.ext.js" type="text/javascript"></script>
-    <script src="../../../../ui/ui.sortable.js" type="text/javascript"></script>
-    <script src="../../../../ui/ui.sortable.ext.js" type="text/javascript"></script>
-    <script src="../../../../ui/ui.tabs.js" type="text/javascript"></script>
-    <script src="../../../../ui/ui.tabs.ext.js" type="text/javascript"></script>
-    <script src="../../../../ui/ui.dialog.js" type="text/javascript"></script>
-    <script src="../../../../ui/ui.slider.js" type="text/javascript"></script>
-    <script src="../../../../ui/ui.accordion.js" type="text/javascript"></script>
-    <script src="../../../../ui/datepicker/core/ui.datepicker.js" type="text/javascript"></script>
+    <?php
+    	include_once("../init.php");
+    ?>
 </head>
 <body>
     <div id="sidebar">
@@ -62,5 +46,11 @@
             ?>
         </div>
     </div>
+
+    <script type="text/javascript">
+    	$(function() {
+
+    	});
+    </script>
 </body>
 </html>
