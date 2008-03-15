badc2956ff71a6f008693c289910684ae38d9754
braeker <braeker@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-03-15 07:58:05 +0000

    UI Functional Demo - ui.resizable demo
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4926 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ ui/demos/functional/ui.resizable.php
@@ -0,0 +1,46 @@
+<div id="containerDemo"></div>
+
+<script type="text/javascript">
+	
+	var model = {
+		
+		renderAt: '#containerDemo',
+		
+		title: 'Resizable Demos',
+		
+		demos: [
+			
+			{
+				title: 'Image Resizing',
+				html: '<img id="resizebleImage" src="img/puppy.jpg">',
+				destroy: '$("#resizebleImage").resizable("destroy");',
+				
+				options: [ 
+					{	desc: 'Transparent Axis',	source: '$("#resizebleImage").resizable({ transparent:true });' },
+					{	desc: 'Transparent Axis, minWidth',	source: '$("#resizebleImage").resizable({ transparent:true, minWidth: 100 });' },
+					{	desc: 'Transparent Axis 3',	source: '$("#resizebleImage").resizable({ transparent:true });' }
+				]
+			},
+			
+			{
+				title: 'Textarea Resizing',
+				html: '<textarea id="resizebleTextarea">I am a textarea!</textarea>',
+				destroy: '$("#resizebleTextarea").resizable("destroy");',
+				options: [ 
+					{	desc: 'Simple Resizing',	source: '$("#resizebleTextarea").resizable();' },
+					{	desc: 'Transparent Axis 2',	source: '$("#resizebleTextarea").resizable({ transparent:true });' },
+					{	desc: 'Transparent Axis 3',	source: '$("#resizebleTextarea").resizable({ transparent:true });' }
+				]
+			}
+			
+		]
+		
+	};
+	
+	$(function(){
+	
+		uiRenderDemo(model);
+		
+	});
+	
+</script>
