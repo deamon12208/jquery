373cde4ea316251a4c0b4a5baf048eab4a3c05a8
braeker <braeker@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-03-15 07:58:30 +0000

    UI Functional Demo - ui.draggable demo
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@4927 c715fcbe-d12f-0410-84c4-316a508785bb

---
+++ ui/demos/functional/ui.draggable.php
@@ -0,0 +1,59 @@
+<style>
+
+.draggable {
+	width: 100px;
+	height: 40px;
+	top: 10px;
+	right: 10px;
+	background-color: #68BFEF;
+	border: 2px solid #0090DF;
+	padding: 5px;	
+}
+
+</style>
+
+<div id="containerDemo"></div>
+
+<script type="text/javascript">
+	
+	var model = {
+		
+		renderAt: '#containerDemo',
+		
+		title: 'Draggable Demos',
+		
+		demos: [
+			
+			{
+				title: 'Simple div drag',
+				html: '<div id="divDrag" class="draggable">I am a div!</div><br>',
+				destroy: '$("#divDrag").draggable("destroy");',
+				
+				options: [ 
+					{	desc: 'Simple Drag',	source: '$("#divDrag").draggable();' },
+					{	desc: 'Simple Drag on axis x',	source: '$("#divDrag").draggable({axis:"x"});' }
+				]
+			},
+			
+			{
+				title: 'Simple image drag',
+				html: '<img id="dragImage" src="img/puppy.jpg" style="width: 228px; height:157px;">',
+				destroy: '$("#dragImage").draggable("destroy");',
+				
+				options: [ 
+					{	desc: 'Simple Drag',	source: '$("#dragImage").draggable();' },
+					{	desc: 'Simple Drag on axis x',	source: '$("#dragImage").draggable({axis:"x"});' }
+				]
+			}
+			
+		]
+		
+	};
+	
+	$(function(){
+	
+		uiRenderDemo(model);
+		
+	});
+	
+</script>
