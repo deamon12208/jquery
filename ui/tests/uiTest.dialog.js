bd7825244e416999751a8b96047a569a1d57ecef
rdworth <rdworth@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-03-15 19:26:48 +0000

    [UI Tests Dialog] Dialog unit tests - refactored setters into own section
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5003 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/tests/uiTest.dialog.js
+++ ui/tests/uiTest.dialog.js
@@ -17,38 +17,31 @@
 				},
 				"draggable" : {
 					"true" : "$('#foo').dialog({\n\t draggable: true \n});",
-					"false" : "$('#foo').dialog({\n\t draggable: false \n});",
-					"set-true" : "$('#foo').dialog({\n\t draggable: false \n});\nsetTimeout(\n\t\"$('#foo').data('draggable.dialog', true);\"\n, 3000);",
-					"set-false" : "$('#foo').dialog({\n\t draggable: true \n});\nsetTimeout(\n\t\"$('#foo').data('draggable.dialog', false);\"\n, 3000);"
+					"false" : "$('#foo').dialog({\n\t draggable: false \n});"
 				},
 				"height" : {
 					"400" : "$('#foo').dialog({\n\t height: 400 \n});",
-					"600" : "$('#foo').dialog({\n\t height: 600 \n});",
-					"set-600" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('height.dialog', 600);\"\n, 3000);"
+					"600" : "$('#foo').dialog({\n\t height: 600 \n});"
 				},
 				"maxHeight" : {
 					"100" : "$('#foo').dialog({\n\t maxHeight: 100 \n});",
 					"400" : "$('#foo').dialog({\n\t maxHeight: 400 \n});",
-					"600" : "$('#foo').dialog({\n\t maxHeight: 600 \n});",
-					"set-600" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('maxHeight.dialog', 600);\"\n, 3000);"
+					"600" : "$('#foo').dialog({\n\t maxHeight: 600 \n});"
 				},
 				"minHeight" : {
 					"100" : "$('#foo').dialog({\n\t minHeight: 100 \n});",
 					"400" : "$('#foo').dialog({\n\t minHeight: 400 \n});",
-					"600" : "$('#foo').dialog({\n\t minHeight: 600 \n});",
-					"set-400" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('minHeight.dialog', 400);\"\n, 3000);"
+					"600" : "$('#foo').dialog({\n\t minHeight: 600 \n});"
 				},
 				"maxWidth" : {
 					"40" : "$('#foo').dialog({\n\t maxWidth: 40 \n});",
 					"400" : "$('#foo').dialog({\n\t maxWidth: 400 \n});",
-					"600" : "$('#foo').dialog({\n\t maxWidth: 600 \n});",
-					"set-600" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('maxWidth.dialog', 600);\"\n, 3000);"
+					"600" : "$('#foo').dialog({\n\t maxWidth: 600 \n});"
 				},
 				"minWidth" : {
 					"40" : "$('#foo').dialog({\n\t minWidth: 40 \n});",
 					"400" : "$('#foo').dialog({\n\t minWidth: 400 \n});",
-					"600" : "$('#foo').dialog({\n\t minWidth: 600 \n});",
-					"set-400" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('minWidth.dialog', 400);\"\n, 3000);"
+					"600" : "$('#foo').dialog({\n\t minWidth: 600 \n});"
 				},
 				"modal" : {
 					"false" : "$('#foo').dialog({\n\t modal: false \n});",
@@ -66,31 +59,66 @@
 					"Array-10-20" : "$('#foo').dialog({\n\t position: [10, 20] \n});",
 					"Array-300-80" : "$('#foo').dialog({\n\t position: [300, 80] \n});",
 					"Array-80-300" : "$('#foo').dialog({\n\t position: [80, 300] \n});",
-					"foo" : "$('#foo').dialog({\n\t position: 'foo' \n});",
-					"set-center" : "$('#foo').dialog({\n\t position: 'top' \n});\nsetTimeout(\n\t\"$('#foo').data('position.dialog', 'center');\"\n, 3000);",
-					"set-Array-300-80" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('position.dialog', [300, 80]);\"\n, 3000);"
+					"foo" : "$('#foo').dialog({\n\t position: 'foo' \n});"
 				},
 				"resizable" : {
 					"true" : "$('#foo').dialog({\n\t resizable: true \n});",
-					"false" : "$('#foo').dialog({\n\t resizable: false \n});",
-					"set-true" : "$('#foo').dialog({\n\t resizable: false \n});\nsetTimeout(\n\t\"$('#foo').data('resizable.dialog', true);\"\n, 3000);",
-					"set-false" : "$('#foo').dialog({\n\t resizable: true \n});\nsetTimeout(\n\t\"$('#foo').data('resizable.dialog', false);\"\n, 3000);"
+					"false" : "$('#foo').dialog({\n\t resizable: false \n});"
 				},
 				"title" : {
 					"attribute" : "$('#foo')\n  .attr( 'title', 'Dialog Title' )\n  .dialog();",
-					"option" : "$('#foo').dialog({\n\t title: 'Dialog Title' \n});",
-					"set" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('title.dialog', 'Dialog Title');\"\n, 3000);"
+					"option" : "$('#foo').dialog({\n\t title: 'Dialog Title' \n});"
 				},
 				"width" : {
 					"400" : "$('#foo').dialog({\n\t width: 400 \n});",
-					"600" : "$('#foo').dialog({\n\t width: 600 \n});",
-					"set-400" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('width.dialog', 400);\"\n, 3000);",
-					"set-600" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('width.dialog', 600);\"\n, 3000);"
+					"600" : "$('#foo').dialog({\n\t width: 600 \n});"
+				}
+			},
+			"dataSet": {
+				"draggable" : {
+					"true" : "$('#foo').dialog({\n\t draggable: false \n});\nsetTimeout(\n\t\"$('#foo').data('draggable.dialog', true);\"\n, 3000);",
+					"false" : "$('#foo').dialog({\n\t draggable: true \n});\nsetTimeout(\n\t\"$('#foo').data('draggable.dialog', false);\"\n, 3000);"
+				},
+				"height" : {
+					"600" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('height.dialog', 600);\"\n, 3000);"
+				},
+				"maxHeight" : {
+					"600" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('maxHeight.dialog', 600);\"\n, 3000);"
+				},
+				"minHeight" : {
+					"400" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('minHeight.dialog', 400);\"\n, 3000);"
+				},
+				"maxWidth" : {
+					"600" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('maxWidth.dialog', 600);\"\n, 3000);"
+				},
+				"minWidth" : {
+					"400" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('minWidth.dialog', 400);\"\n, 3000);"
+				},
+				"position" : {
+					"center" : "$('#foo').dialog({\n\t position: 'top' \n});\nsetTimeout(\n\t\"$('#foo').data('position.dialog', 'center');\"\n, 3000);",
+					"Array-300-80" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('position.dialog', [300, 80]);\"\n, 3000);"
+				},
+				"resizable" : {
+					"true" : "$('#foo').dialog({\n\t resizable: false \n});\nsetTimeout(\n\t\"$('#foo').data('resizable.dialog', true);\"\n, 3000);",
+					"false" : "$('#foo').dialog({\n\t resizable: true \n});\nsetTimeout(\n\t\"$('#foo').data('resizable.dialog', false);\"\n, 3000);"
+				},
+				"title" : {
+					"" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('title.dialog', 'Dialog Title');\"\n, 3000);"
+				},
+				"width" : {
+					"400" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('width.dialog', 400);\"\n, 3000);",
+					"600" : "$('#foo').dialog();\nsetTimeout(\n\t\"$('#foo').data('width.dialog', 600);\"\n, 3000);"
 				}
 			},
 			"callbacks": {
-				"open" : "$('#foo').dialog({\n  open: function() {\n    alert('opened');\n  }\n});",
-				"close" : "$('#foo').dialog({\n  close: function() {\n    alert('closed');\n  }\n});"
+				"open" : "$('#foo').dialog({\n  open: function(ev, ui) {\n    uiTestLog('opened');\n  }\n});",
+				"close" : "$('#foo').dialog({\n  close: function(ev, ui) {\n    uiTestLog('closed');\n  }\n});",
+				"dragStart" : "$('#foo').dialog({\n  dragStart: function(ev, ui) {\n    uiTestLog('dragStart');\n  }\n});",
+				"drag" : "$('#foo').dialog({\n  drag: function(ev, ui) {\n    uiTestLog('drag');\n  }\n});",
+				"dragStop" : "$('#foo').dialog({\n  dragStop: function(ev, ui) {\n    uiTestLog('dragStop');\n  }\n});",
+				"resizeStart" : "$('#foo').dialog({\n  resizeStart: function(ev, ui) {\n    uiTestLog('resizeStart');\n  }\n});",
+				"resize" : "$('#foo').dialog({\n  resize: function(ev, ui) {\n    uiTestLog('resize');\n  }\n});",
+				"resizeStop" : "$('#foo').dialog({\n  resizeStop: function(ev, ui) {\n    uiTestLog('resizeStop');\n  }\n});"
 			},
 			"methods": {
 				"open" : "$('#foo').dialog({ width: 450, autoOpen: false });\n setTimeout(\"$('#foo').dialog('open');\", 1000)",
