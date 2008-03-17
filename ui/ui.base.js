01eb7fe590a808e7ee37852e2bad7bdc70371f9a
paul.bakaus <paul.bakaus@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-03-17 11:56:16 +0000

    ui: removed ui.mouse.js, the mouse interaction plugin is now part of ui.base.js, since it doesn't add much weight and it's used all over the place, with some exceptions.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5052 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/ui.base.js
+++ ui/ui.base.js
@@ -23,10 +23,7 @@
 		cssCache: {},
 		css: function(name) {
 			if ($.ui.cssCache[name]) return $.ui.cssCache[name];
-			
-			var tmp = $('<div class="ui-resizable-gen">').addClass(name).css(
-				{position:'absolute', top:'-5000px', left:'-5000px', display:'block'}
-			).appendTo('body');
+			var tmp = $('<div class="ui-resizable-gen">').addClass(name).css({position:'absolute', top:'-5000px', left:'-5000px', display:'block'}).appendTo('body');
 			
 			//Opera and Safari set width and height to 0px instead of auto
 			//Safari returns rgba(0,0,0,0) when bgcolor is not set
@@ -38,13 +35,11 @@
 			return $.ui.cssCache[name];
 		},
 		disableSelection: function(e) {
-			if (!e) return;
 			e.unselectable = "on";
 			e.onselectstart = function() {	return false; };
 			if (e.style) e.style.MozUserSelect = "none";
 		},
 		enableSelection: function(e) {
-			if (!e) return;
 			e.unselectable = "off";
 			e.onselectstart = function() { return true; };
 			if (e.style) e.style.MozUserSelect = "";
@@ -56,12 +51,9 @@
       		return has; 
     	}
 	});
-	
-	//Modify native plugins
-	var old = $.fn.remove;
-	$.fn.remove = function(){ this.trigger("remove"); return old.apply(this, arguments ); };
 
-	// Create scrollLeft and scrollTop methods (if not coming from dimensions already)
+	/******* fn scope modifications ********/
+
 	$.each( ['Left', 'Top'], function(i, name) {
 		if(!$.fn['scroll'+name]) $.fn['scroll'+name] = function(v) {
 			return v != undefined ?
@@ -69,8 +61,8 @@
 				this[0] == window || this[0] == document ? self[(name == 'Left' ? 'pageXOffset' : 'pageYOffset')] || $.boxModel && document.documentElement['scroll'+name] || document.body['scroll'+name] : this[0][ 'scroll' + name ];
 		};
 	});
-	
-	//Create position() and offsetParent if not already coming from dimensions
+
+	var old = $.fn.remove;
 	$.fn.extend({
 		position: function() {
 			var offset       = this.offset();
@@ -87,11 +79,96 @@
 			while ( offsetParent && (!/^body|html$/i.test(offsetParent.tagName) && $.css(offsetParent, 'position') == 'static') )
 				offsetParent = offsetParent.offsetParent;
 			return $(offsetParent);
-		}
+		},
+		mouseInteraction: function(o) {
+			return this.each(function() {
+				new $.ui.mouseInteraction(this, o);
+			});
+		},
+		removeMouseInteraction: function(o) {
+			return this.each(function() {
+				if($.data(this, "ui-mouse"))
+					$.data(this, "ui-mouse").destroy();
+			});
+		},
+		remove: function(){ this.trigger("remove"); return old.apply(this, arguments ); }
 	});
 	
 	function num(el, prop) {
 		return parseInt($.curCSS(el.jquery?el[0]:el,prop,true))||0;
 	};
+	
+	
+	/********** Mouse Interaction Plugin *********/
+	
+	$.ui.mouseInteraction = function(element, options) {
+	
+		var self = this;
+		this.element = element;
+		$.data(this.element, "ui-mouse", this);
+		this.options = $.extend({}, options);
+		
+		$(element).bind('mousedown.draggable', function() { return self.click.apply(self, arguments); });
+		if($.browser.msie) $(element).attr('unselectable', 'on'); //Prevent text selection in IE
+		
+	};
+	
+	$.extend($.ui.mouseInteraction.prototype, {
+		
+		destroy: function() { $(this.element).unbind('mousedown.draggable'); },
+		trigger: function() { return this.click.apply(this, arguments); },
+		click: function(e) {
+			
+			if(
+				   e.which != 1 //only left click starts dragging
+				|| $.inArray(e.target.nodeName.toLowerCase(), this.options.dragPrevention || []) != -1 // Prevent execution on defined elements
+				|| (this.options.condition && !this.options.condition.apply(this.options.executor || this, [e, this.element])) //Prevent execution on condition
+			) return true;
+			
+			var self = this;
+			var initialize = function() {
+				self._MP = { left: e.pageX, top: e.pageY }; // Store the click mouse position
+				$(document).bind('mouseup.draggable', function() { return self.stop.apply(self, arguments); });
+				$(document).bind('mousemove.draggable', function() { return self.drag.apply(self, arguments); });
+			};
+
+			if(this.options.delay) {
+				if(this.timer) clearInterval(this.timer);
+				this.timer = setTimeout(initialize, this.options.delay);
+			} else {
+				initialize();
+			}
+			
+			return false;
+			
+		},
+		stop: function(e) {			
+			
+			var o = this.options;
+			if(!this.initialized) return $(document).unbind('mouseup.draggable').unbind('mousemove.draggable');
+
+			if(this.options.stop) this.options.stop.call(this.options.executor || this, e, this.element);
+			$(document).unbind('mouseup.draggable').unbind('mousemove.draggable');
+			this.initialized = false;
+			return false;
+			
+		},
+		drag: function(e) {
+
+			var o = this.options;
+			if ($.browser.msie && !e.button) return this.stop.apply(this, [e]); // IE mouseup check
+			
+			if(!this.initialized && (Math.abs(this._MP.left-e.pageX) >= o.distance || Math.abs(this._MP.top-e.pageY) >= o.distance)) {
+				if(this.options.start) this.options.start.call(this.options.executor || this, e, this.element);
+				this.initialized = true;
+			} else {
+				if(!this.initialized) return false;
+			}
+
+			if(o.drag) o.drag.call(this.options.executor || this, e, this.element);
+			return false;
+			
+		}
+	});
 	
  })(jQuery);
