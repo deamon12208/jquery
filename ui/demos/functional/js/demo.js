b3773c1206c7ef850d096f490708cb9b2bbdf417
braeker <braeker@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-03-17 03:33:38 +0000

    Functional demo html code preview
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5039 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/demos/functional/js/demo.js
+++ ui/demos/functional/js/demo.js
@@ -1,37 +1,67 @@
 var uid = 0;
 
+/**
+ * Render a demo template page
+ * @author Eduardo Lundgren (braeker)
+ * @param {Object} model
+ */
 var uiRenderDemo = function(model) {
 
-	var title = model.title, container = $(model.renderAt);
+	var title = model.title, renderAt = $(model.renderAt);
 
-	container.append('<h1>' + title + '</h1><br>');
+	renderAt.append(
+		'<h1>'+ title +'</h1><br>'
+	);
 
 	$.each(model.demos, function(i, demo) {
+		
+		/**
+		 * Rendering each demo
+		 */
+
 		if (!demo) return;
 		
-		var html = $('<div class="ui-html-demo"></div>');
+		var uiHtmlRendered = $('<div class="ui-html-rendered"></div>');
 		
 		if (model.onRenderStart) model.onRenderStart.apply(window);
 		
-		var _uid = 'ui-gen-' + uid++, generated = $('<div id="'+ _uid +'">');
+		var gid = 'ui-gen-'+uid++, demoBox = $('<div id="'+gid+'">');
 
-		container.append(generated);
+		renderAt.append(demoBox);
 
-		var details = $(
-			'<br><div class="details"><div class="menutitle">' + demo.title + '</div></div>'
+		var detailsHtml = $(
+			'<br><div class="ui-details"><div class="menutitle">'+demo.title+'</div></div>'
 		);
 		
-		var desc = $(
-			'<div class="demo-description">'+ (demo.desc || '') +'</div>'
+		var descBox = $(
+			'<div class="ui-demo-description">'+(demo.desc||'')+'</div>'
 		);
 		
-		var ocontainer = $(
-			'<div class="demo-options"><label for="select-' + _uid + '">More options:</label></div>'
+		var optionsBox = $(
+			'<div class="ui-demo-options"><label for="select-'+gid+'">More options:</label></div>'
 		);
+	
+		var codesBox = $(
+			'<div id="code-'+gid+'">'
+		)
+		.css({display: 'none'});
+
+		var sourceTmpl = $(
+			'<div class="ui-demo-view-code snippet"></div>'
+		);
+		
+		var preTmpl = $(
+			'<pre></pre>'
+		);
+		
+		var htmlCode = '', sourceHtml = sourceTmpl.clone(), sourceJs = sourceTmpl.clone(), entitiesHtml = function(html) {
+			return html.replace(/</g,"&lt;").replace(/>/g,"&gt;");
+		};
 		
 		// Render simple HTML
 		if (typeof demo.html == 'string') {
-			html = demo.html;
+			uiHtmlRendered.html(demo.html);
+			htmlCode = demo.html;
 		}
 		// Render data html by URL
 		if (typeof demo.html == 'object' && demo.html.url) {
@@ -41,66 +71,63 @@ var uiRenderDemo = function(model) {
 				url: demo.html.url,
 				cache: false,
 				success: function(data) {
-					html.html(data);
+					uiHtmlRendered.html(data);
+					htmlCode = data;
+					
+					// set html code view
+					sourceHtml.html(preTmpl.clone().html( entitiesHtml(htmlCode) ));
+					
 					$.each(demo.options, function(x, o) {
 						// eval the first source of <select>
 						if (!x) jQuery.globalEval(o.source);
 					});
+					
+					// fire renderEnd callback to ajax async transactions
 					if (model.onRenderEnd) model.onRenderEnd.apply(window);
 				}
 			});
 			
 		}
+		// set html code view
+		sourceHtml.html(preTmpl.clone().html(entitiesHtml(htmlCode)));
 
-		var spanCode = $(
-			'<span id="code-'+ _uid +'">').css({ display: 'none' }
-		);
-
-		var source = $(
-			'<div class="demo-view-code">Code</div>'
-		);
-		
-		var pre = $(
-			'<pre></pre>'
-		);
-
-		var select = $('<select id="select-' + _uid + '">').change(function() {
+		var select = $('<select id="select-'+ gid+'">').change(function() {
 			var ecode = $(this).val();
 
 			jQuery.globalEval(demo.destroy);
-
 			jQuery.globalEval(ecode);
 
-			source.html(pre.html(ecode));
-
+			sourceJs.html(preTmpl.html(ecode));
 		});
 
 		var a = $('<a>View Source</a>').attr('href', 'javascript:void(0);').addClass('link-view-source').click(function() {
         el = this;
-				spanCode.slideToggle("slow", function(){
+				codesBox.slideToggle("slow", function(){
           var text = $(el).text();
           if(/view source/i.test(text)) $(el).text("Hide Source");
           else $(el).text("View Source");
         });
 		});
 
-		generated.append(
-			details, desc, html, ocontainer.append(
-				select, a, '<br>', spanCode.append('<br>', source)
+		demoBox.append(
+			detailsHtml, descBox, uiHtmlRendered, optionsBox.append(
+				select, a, '<br>', codesBox.append('<br>JavaScript:<br><br>', sourceJs, 'HTML:<br><br>', sourceHtml)
 			)
 		);
-
+		
+		// population select with the demo options
 		$.each(demo.options, function(x, o) {
 			if (o && o.desc) {
 	  		select.append($('<option>' + o.desc + '</option>').val(o.source));
 	  		// eval the first source of <select>
 				if (!x) {
-					source.html(pre.html(o.source));
+					sourceJs.html(preTmpl.html(o.source));
 					jQuery.globalEval(o.source);
 				}
 			}
 		});
 		
+		// fire renderEnd callback to direct-html-render
 		if (typeof demo.html != 'object' && model.onRenderEnd) model.onRenderEnd.apply(window);
 		
 	});
@@ -110,7 +137,7 @@ var loadDemo = function(comp) {
 	$('#containerDemo').html("<img src='img/loading.gif'>");
 	
 	 $("#containerDemo").ajaxError(function(request, settings){ 
-	   $(this).html("<b>Ops!</b> There is no template file for this component."); 
+	   $(this).html("Ops, there is no template file for this component."); 
 	 });
 	
 	$.get(comp+'.html', function(data) {
