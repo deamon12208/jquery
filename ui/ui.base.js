4f03c9e3ddab35ab6704b7e52c8489c30c642e6a
paul.bakaus <paul.bakaus@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-03-17 11:45:16 +0000

    ui.base: added position() method from dimensions (stripped down)
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5051 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/ui.base.js
+++ ui/ui.base.js
@@ -5,7 +5,6 @@
 	
 	//Add methods that are vital for all mouse interaction stuff (plugin registering)
 	$.extend($.ui, {
-		
 		plugin: {
 			add: function(module, option, set) {
 				var proto = $.ui[module].prototype;
@@ -64,11 +63,35 @@
 
 	// Create scrollLeft and scrollTop methods (if not coming from dimensions already)
 	$.each( ['Left', 'Top'], function(i, name) {
-		if(!$.fn['scroll'+name]) $.fn['scroll'+name] = function() {
-			return this[0] == window || this[0] == document ?
-				self[(name == 'Left' ? 'pageXOffset' : 'pageYOffset')] || $.boxModel && document.documentElement['scroll'+name] || document.body['scroll'+name] :
-			this[0][ 'scroll' + name ];
+		if(!$.fn['scroll'+name]) $.fn['scroll'+name] = function(v) {
+			return v != undefined ?
+				this.each(function() { this == window || this == document ? window.scrollTo(name == 'Left' ? v : $(window)['scrollLeft'](), name == 'Top'  ? v : $(window)['scrollTop']()) : this['scroll'+name] = v; }) :
+				this[0] == window || this[0] == document ? self[(name == 'Left' ? 'pageXOffset' : 'pageYOffset')] || $.boxModel && document.documentElement['scroll'+name] || document.body['scroll'+name] : this[0][ 'scroll' + name ];
 		};
 	});
+	
+	//Create position() and offsetParent if not already coming from dimensions
+	$.fn.extend({
+		position: function() {
+			var offset       = this.offset();
+			var offsetParent = this.offsetParent();
+			var parentOffset = offsetParent.offset();
+
+			return {
+				top:  offset.top - num(this[0], 'marginTop')  - parentOffset.top - num(offsetParent, 'borderTopWidth'),
+				left: offset.left - num(this[0], 'marginLeft')  - parentOffset.left - num(offsetParent, 'borderLeftWidth')
+			};
+		},
+		offsetParent: function() {
+			var offsetParent = this[0].offsetParent;
+			while ( offsetParent && (!/^body|html$/i.test(offsetParent.tagName) && $.css(offsetParent, 'position') == 'static') )
+				offsetParent = offsetParent.offsetParent;
+			return $(offsetParent);
+		}
+	});
+	
+	function num(el, prop) {
+		return parseInt($.curCSS(el.jquery?el[0]:el,prop,true))||0;
+	};
 	
  })(jQuery);
