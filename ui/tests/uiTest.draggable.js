e9edafe37ba9bfb966fb130c2e4a760b31545c82
rdworth <rdworth@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-03-18 13:58:11 +0000

    [UI Tests] Draggable - Added some tests for additional (some unexpected) values
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5076 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/tests/uiTest.draggable.js
+++ ui/tests/uiTest.draggable.js
@@ -9,19 +9,21 @@
 			},
 			"options": {
 				"appendTo" : {
-					"body" : "$('#foo').draggable({ helper: 'clone', appendTo: 'body' });",
-					"''" : "$('#foo').draggable({ helper: 'clone' });"
+					"(empty)" : "$('#foo').draggable({ helper: 'clone', appendTo: '' });",
+					"body" : "$('#foo').draggable({ helper: 'clone', appendTo: 'body' });"
 				},
 				"axis" : {
+					"(empty)" : "$('#foo').draggable({ axis: '' });",
 					"x" : "$('#foo').draggable({ axis: 'x' });",
-					"y" : "$('#foo').draggable({ axis: 'y' });"
+					"y" : "$('#foo').draggable({ axis: 'y' });",
+					"invalidvalue" : "$('#foo').draggable({ axis: 'invalidvalue' });"
 				},
 				"cancel" : {
 					"(empty)" : "$('#foo').append('<' + 'input type=\"text\"/>').draggable({ cancel: '' });",
+					"*" : "$('#foo').draggable({ cancel: '*' });",
 					"div" : "$('#foo').draggable({ cancel: 'div' });",
 					"code" : "$('#foo').draggable({ cancel: 'code' });",
-					"div,code" : "$('#foo').draggable({ cancel: 'div,code' });",
-					"*" : "$('#foo').draggable({ cancel: '*' });"
+					"div,code" : "$('#foo').draggable({ cancel: 'div,code' });"
 				},
 				"containment" : {
 					"document" : "$('#foo').draggable({ containment: 'document' });",
@@ -46,12 +48,17 @@
 					"x10y50" : "$('#foo').draggable({ grid: [10, 50] });"
 				},
 				"handle" : {
+					"(empty)" : "$('#foo').draggable({ handle: '' });",
+					"*" : "$('#foo').draggable({ handle: '*' });",
 					"div" : "$('#foo').draggable({ handle: 'div' });",
 					"code" : "$('#foo').draggable({ handle: 'code' });"
 				},
 				"helper" : {
+					"(empty)" : "$('#foo').draggable({ helper: '' });",
+					"original" : "$('#foo').draggable({ helper: 'original' });",
 					"clone" : "$('#foo').draggable({ helper: 'clone' });",
-					"function" : "$('#foo').draggable({ helper: function() {\n\t return $('<'+'div/>').addClass('bar')[0]; \n} });"
+					"invalidvalue" : "$('#foo').draggable({ helper: 'invalidvalue' });",
+					"function" : "$('#foo').draggable({ helper: function() {\n\t return $('<'+'div style=\"background:blue;\" />').addClass('bar')[0]; \n} });"
 				},
 				"opacity" : {
 					"0.6" : "$('#foo').draggable({ opacity: 0.6 });",
@@ -60,7 +67,9 @@
 				},
 				"revert" : {
 					"true" : "$('#foo').draggable({ revert: true });",
-					"truehelperclone" : "$('#foo').draggable({ revert: true, helper: 'clone' });"
+					"truehelperclone" : "$('#foo').draggable({ revert: true, helper: 'clone' });",
+					"false" : "$('#foo').draggable({ revert: false });",
+					"falsehelperclone" : "$('#foo').draggable({ revert: false, helper: 'clone' });"
 				},
 				"scroll" : {
 					"true" : "$('#foo').draggable({ scroll: true });",
