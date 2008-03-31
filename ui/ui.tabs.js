69c6d0a70624288ea63d7a9642a749dacb95fd71
rdworth <rdworth@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-03-31 09:53:07 +0000

    [UI] Moved extensions into main files. Removed ui.plugin.ext.js files
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5148 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/ui.tabs.js
+++ ui/ui.tabs.js
@@ -555,4 +555,52 @@
         }
     });
 
+/*
+ * Tabs Extensions
+ */
+
+    /*
+     * Rotate
+     */
+    $.extend($.ui.tabs.prototype, {
+        rotation: null,
+        rotate: function(ms, continuing) {
+            
+            continuing = continuing || false;
+            
+            var self = this, t = this.options.selected;
+            
+            function start() {
+                self.rotation = setInterval(function() {
+                    t = ++t < self.$tabs.length ? t : 0;
+                    self.select(t);
+                }, ms);    
+            }
+            
+            function stop(e) {
+                if (!e || e.clientX) { // only in case of a true click
+                    clearInterval(self.rotation);
+                }
+            }
+            
+            // start interval
+            if (ms) {
+                start();
+                if (!continuing)
+                    this.$tabs.bind(this.options.event, stop);
+                else
+                    this.$tabs.bind(this.options.event, function() {
+                        stop();
+                        t = self.options.selected;
+                        start();
+                    });
+            }
+            // stop interval
+            else {
+                stop();
+                this.$tabs.unbind(this.options.event, stop);
+            }
+        }
+    });
+
 })(jQuery);
