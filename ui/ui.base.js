c2d8f7a9f3503d0374a8fedbe6ba9b322cca2d10
scott.gonzalez <scott.gonzalez@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-04-29 23:19:03 +0000

    UI Base: Removed dimensions/offset methods (now in core).
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5355 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/ui.base.js
+++ ui/ui.base.js
@@ -60,41 +60,17 @@
 			if (e.style) e.style.MozUserSelect = "";
 		},
 		hasScroll: function(e, a) {
-      		var scroll = /top/.test(a||"top") ? 'scrollTop' : 'scrollLeft', has = false;
-      		if (e[scroll] > 0) return true; e[scroll] = 1;
-      		has = e[scroll] > 0 ? true : false; e[scroll] = 0;
-      		return has; 
-    	}
+			var scroll = /top/.test(a||"top") ? 'scrollTop' : 'scrollLeft', has = false;
+			if (e[scroll] > 0) return true; e[scroll] = 1;
+			has = e[scroll] > 0 ? true : false; e[scroll] = 0;
+			return has;
+		}
 	});
 
 	/******* fn scope modifications ********/
 
-	$.each( ['Left', 'Top'], function(i, name) {
-		if(!$.fn['scroll'+name]) $.fn['scroll'+name] = function(v) {
-			return v != undefined ?
-				this.each(function() { this == window || this == document ? window.scrollTo(name == 'Left' ? v : $(window)['scrollLeft'](), name == 'Top'  ? v : $(window)['scrollTop']()) : this['scroll'+name] = v; }) :
-				this[0] == window || this[0] == document ? self[(name == 'Left' ? 'pageXOffset' : 'pageYOffset')] || $.boxModel && document.documentElement['scroll'+name] || document.body['scroll'+name] : this[0][ 'scroll' + name ];
-		};
-	});
-
 	var _remove = $.fn.remove;
 	$.fn.extend({
-		position: function() {
-			var offset       = this.offset();
-			var offsetParent = this.offsetParent();
-			var parentOffset = offsetParent.offset();
-
-			return {
-				top:  offset.top - num(this[0], 'marginTop')  - parentOffset.top - num(offsetParent, 'borderTopWidth'),
-				left: offset.left - num(this[0], 'marginLeft')  - parentOffset.left - num(offsetParent, 'borderLeftWidth')
-			};
-		},
-		offsetParent: function() {
-			var offsetParent = this[0].offsetParent;
-			while ( offsetParent && (!/^body|html$/i.test(offsetParent.tagName) && $.css(offsetParent, 'position') == 'static') )
-				offsetParent = offsetParent.offsetParent;
-			return $(offsetParent);
-		},
 		mouseInteraction: function(o) {
 			return this.each(function() {
 				new $.ui.mouseInteraction(this, o);
@@ -111,10 +87,6 @@
 			return _remove.apply(this, arguments );
 		}
 	});
-	
-	function num(el, prop) {
-		return parseInt($.curCSS(el.jquery?el[0]:el,prop,true))||0;
-	};
 	
 	
 	/********** Mouse Interaction Plugin *********/
@@ -147,14 +119,14 @@
 				|| $.inArray(e.target.nodeName.toLowerCase(), this.options.dragPrevention || []) != -1 // Prevent execution on defined elements
 				|| (this.options.condition && !this.options.condition.apply(this.options.executor || this, [e, this.element])) //Prevent execution on condition
 			) return true;
-				
+			
 			var self = this;
 			var initialize = function() {
 				self._MP = { left: e.pageX, top: e.pageY }; // Store the click mouse position
 				$(document).bind('mouseup.draggable', function() { return self.stop.apply(self, arguments); });
 				$(document).bind('mousemove.draggable', function() { return self.drag.apply(self, arguments); });
 				
-				if(!self.initalized && Math.abs(self._MP.left-e.pageX) >= self.options.distance || Math.abs(self._MP.top-e.pageY) >= self.options.distance) {				
+				if(!self.initalized && Math.abs(self._MP.left-e.pageX) >= self.options.distance || Math.abs(self._MP.top-e.pageY) >= self.options.distance) {
 					if(self.options.start) self.options.start.call(self.options.executor || self, e, self.element);
 					if(self.options.drag) self.options.drag.call(self.options.executor || self, e, this.element); //This is actually not correct, but expected
 					self.initialized = true;
@@ -171,7 +143,7 @@
 			return false;
 			
 		},
-		stop: function(e) {			
+		stop: function(e) {
 			
 			var o = this.options;
 			if(!this.initialized) return $(document).unbind('mouseup.draggable').unbind('mousemove.draggable');
@@ -187,7 +159,7 @@
 			var o = this.options;
 			if ($.browser.msie && !e.button) return this.stop.apply(this, [e]); // IE mouseup check
 			
-			if(!this.initialized && (Math.abs(this._MP.left-e.pageX) >= o.distance || Math.abs(this._MP.top-e.pageY) >= o.distance)) {				
+			if(!this.initialized && (Math.abs(this._MP.left-e.pageX) >= o.distance || Math.abs(this._MP.top-e.pageY) >= o.distance)) {
 				if(this.options.start) this.options.start.call(this.options.executor || this, e, this.element);
 				this.initialized = true;
 			} else {
