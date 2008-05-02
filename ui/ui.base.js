6a4b4dedad8c5d664273032be66985ee4f16afba
scott.gonzalez <scott.gonzalez@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-05-02 21:16:57 +0000

    UI Base: Fixed a bunch of warnings.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5388 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/ui.base.js
+++ ui/ui.base.js
@@ -61,16 +61,20 @@
 					proto.plugins[i].push([option, set[i]]);
 				}
 			},
-			call: function(instance, name, arguments) {
-				var set = instance.plugins[name]; if(!set) return;
+			call: function(instance, name, args) {
+				var set = instance.plugins[name];
+				if(!set) { return; }
+				
 				for (var i = 0; i < set.length; i++) {
-					if (instance.options[set[i][0]]) set[i][1].apply(instance.element, arguments);
+					if (instance.options[set[i][0]]) {
+						set[i][1].apply(instance.element, args);
+					}
 				}
 			}	
 		},
 		cssCache: {},
 		css: function(name) {
-			if ($.ui.cssCache[name]) return $.ui.cssCache[name];
+			if ($.ui.cssCache[name]) { return $.ui.cssCache[name]; }
 			var tmp = $('<div class="ui-resizable-gen">').addClass(name).css({position:'absolute', top:'-5000px', left:'-5000px', display:'block'}).appendTo('body');
 			
 			//if (!$.browser.safari)
@@ -79,7 +83,7 @@
 			//Opera and Safari set width and height to 0px instead of auto
 			//Safari returns rgba(0,0,0,0) when bgcolor is not set
 			$.ui.cssCache[name] = !!(
-				(!/auto|default/.test(tmp.css('cursor')) || (/^[1-9]/).test(tmp.css('height')) || (/^[1-9]/).test(tmp.css('width')) || 
+				(!(/auto|default/).test(tmp.css('cursor')) || (/^[1-9]/).test(tmp.css('height')) || (/^[1-9]/).test(tmp.css('width')) || 
 				!(/none/).test(tmp.css('backgroundImage')) || !(/transparent|rgba\(0, 0, 0, 0\)/).test(tmp.css('backgroundColor')))
 			);
 			try { $('body').get(0).removeChild(tmp.get(0));	} catch(e){}
@@ -87,13 +91,13 @@
 		},
 		disableSelection: function(e) {
 			e.unselectable = "on";
-			e.onselectstart = function() {	return false; };
-			if (e.style) e.style.MozUserSelect = "none";
+			e.onselectstart = function() { return false; };
+			if (e.style) { e.style.MozUserSelect = "none"; }
 		},
 		enableSelection: function(e) {
 			e.unselectable = "off";
 			e.onselectstart = function() { return true; };
-			if (e.style) e.style.MozUserSelect = "";
+			if (e.style) { e.style.MozUserSelect = ""; }
 		},
 		hasScroll: function(e, a) {
 			var scroll = /top/.test(a||"top") ? 'scrollTop' : 'scrollLeft', has = false;
@@ -114,8 +118,9 @@
 		},
 		removeMouseInteraction: function(o) {
 			return this.each(function() {
-				if($.data(this, "ui-mouse"))
+				if($.data(this, "ui-mouse")) {
 					$.data(this, "ui-mouse").destroy();
+				}
 			});
 		},
 		remove: function() {
@@ -136,11 +141,14 @@
 		this.options = $.extend({}, options);
 		
 		$(element).bind('mousedown.draggable', function() { return self.click.apply(self, arguments); });
-		if($.browser.msie) $(element).attr('unselectable', 'on'); //Prevent text selection in IE
+		if($.browser.msie) {
+			//Prevent text selection in IE
+			$(element).attr('unselectable', 'on');
+		}
 		
 		// prevent draggable-options-delay bug #2553
 		$(element).mouseup(function() {
-			if(self.timer) clearInterval(self.timer);
+			if(self.timer) { clearInterval(self.timer); }
 		});
 	};
 	
@@ -154,7 +162,7 @@
 				   e.which != 1 //only left click starts dragging
 				|| $.inArray(e.target.nodeName.toLowerCase(), this.options.dragPrevention || []) != -1 // Prevent execution on defined elements
 				|| (this.options.condition && !this.options.condition.apply(this.options.executor || this, [e, this.element])) //Prevent execution on condition
-			) return true;
+			) { return true; }
 			
 			var self = this;
 			var initialize = function() {
@@ -163,14 +171,19 @@
 				$(document).bind('mousemove.draggable', function() { return self.drag.apply(self, arguments); });
 				
 				if(!self.initalized && Math.abs(self._MP.left-e.pageX) >= self.options.distance || Math.abs(self._MP.top-e.pageY) >= self.options.distance) {
-					if(self.options.start) self.options.start.call(self.options.executor || self, e, self.element);
-					if(self.options.drag) self.options.drag.call(self.options.executor || self, e, this.element); //This is actually not correct, but expected
+					if(self.options.start) {
+						self.options.start.call(self.options.executor || self, e, self.element);
+					}
+					if(self.options.drag) {
+						//This is actually not correct, but expected
+						self.options.drag.call(self.options.executor || self, e, this.element);
+					}
 					self.initialized = true;
 				}
 			};
 
 			if(this.options.delay) {
-				if(this.timer) clearInterval(this.timer);
+				if(this.timer) { clearInterval(this.timer); }
 				this.timer = setTimeout(initialize, this.options.delay);
 			} else {
 				initialize();
@@ -182,9 +195,15 @@
 		stop: function(e) {
 			
 			var o = this.options;
-			if(!this.initialized) return $(document).unbind('mouseup.draggable').unbind('mousemove.draggable');
+			if(!this.initialized) {
+				return $(document)
+					.unbind('mouseup.draggable')
+					.unbind('mousemove.draggable');
+			}
 
-			if(this.options.stop) this.options.stop.call(this.options.executor || this, e, this.element);
+			if(this.options.stop) {
+				this.options.stop.call(this.options.executor || this, e, this.element);
+			}
 			$(document).unbind('mouseup.draggable').unbind('mousemove.draggable');
 			this.initialized = false;
 			return false;
@@ -193,16 +212,23 @@
 		drag: function(e) {
 
 			var o = this.options;
-			if ($.browser.msie && !e.button) return this.stop.apply(this, [e]); // IE mouseup check
+			if ($.browser.msie && !e.button) {
+				// IE mouseup check
+				return this.stop.apply(this, [e]);
+			}
 			
 			if(!this.initialized && (Math.abs(this._MP.left-e.pageX) >= o.distance || Math.abs(this._MP.top-e.pageY) >= o.distance)) {
-				if(this.options.start) this.options.start.call(this.options.executor || this, e, this.element);
+				if(this.options.start) {
+					this.options.start.call(this.options.executor || this, e, this.element);
+				}
 				this.initialized = true;
 			} else {
-				if(!this.initialized) return false;
+				if(!this.initialized) { return false; }
 			}
 
-			if(o.drag) o.drag.call(this.options.executor || this, e, this.element);
+			if(o.drag) {
+				o.drag.call(this.options.executor || this, e, this.element);
+			}
 			return false;
 			
 		}
