3ee661b7e8b91a859ea5dba064986093929f95d8
scott.gonzalez <scott.gonzalez@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-05-04 14:47:56 +0000

    UI Base: Fixed a bunch of JavaScript warnings. UI Mouse: Fixed prevention of text selection in IE.
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5416 c715fcbe-d12f-0410-84c4-316a508785bb

--- ui/ui.base.js
+++ ui/ui.base.js
@@ -157,20 +157,21 @@
 		init: function() {
 			var self = this;
 			
-			$(this.element)
+			this.element
 				.bind('mousedown.mouse', function() { return self.click.apply(self, arguments); })
-				.bind('mouseup.mouse', function() { if(self.timer) { window.clearInterval(self.timer); } })
+				.bind('mouseup.mouse', function() { (self.timer && clearInterval(self.timer)); })
 				.bind('click.mouse', function() { if(self.initialized) { self.initialized = false; return false; } });
-			if($.browser.msie) $(element).attr('unselectable', 'on'); //Prevent text selection in IE
+			//Prevent text selection in IE
+			($.browser.msie && this.element.attr('unselectable', 'on'));
 		},
-		destroy: function() { $(this.element).unbind('mousedown.mouse'); },
+		destroy: function() { this.element.unbind('mousedown.mouse'); },
 		trigger: function() { return this.click.apply(this, arguments); },
 		click: function(e) {
 			
 			if(    e.which != 1 //only left click starts dragging
 				|| $.inArray(e.target.nodeName.toLowerCase(), this.options.dragPrevention || []) != -1 // Prevent execution on defined elements
 				|| (this.options.condition && !this.options.condition.apply(this.options.executor || this, [e, this.element])) //Prevent execution on condition
-			) return true;
+			) { return true; }
 			
 			var self = this;
 			var initialize = function() {
@@ -179,14 +180,14 @@
 				$(document).bind('mousemove.mouse', function() { return self.drag.apply(self, arguments); });
 				
 				if(!self.initalized && Math.abs(self._MP.left-e.pageX) >= self.options.distance || Math.abs(self._MP.top-e.pageY) >= self.options.distance) {
-					if(self.options.start) self.options.start.call(self.options.executor || self, e, self.element);
-					if(self.options.drag) self.options.drag.call(self.options.executor || self, e, this.element); //This is actually not correct, but expected
+					(self.options.start && self.options.start.call(self.options.executor || self, e, self.element));
+					(self.options.drag && self.options.drag.call(self.options.executor || self, e, this.element)); //This is actually not correct, but expected
 					self.initialized = true;
 				}
 			};
 
 			if(this.options.delay) {
-				if(this.timer) clearInterval(this.timer);
+				if(this.timer) { clearInterval(this.timer); }
 				this.timer = setTimeout(initialize, this.options.delay);
 			} else {
 				initialize();
@@ -197,11 +198,11 @@
 		},
 		stop: function(e) {
 			
-			if(!this.initialized)
+			if(!this.initialized) {
 				return $(document).unbind('mouseup.mouse').unbind('mousemove.mouse');
+			}
 
-			if(this.options.stop)
-				this.options.stop.call(this.options.executor || this, e, this.element);
+			(this.options.stop && this.options.stop.call(this.options.executor || this, e, this.element));
 			
 			$(document).unbind('mouseup.mouse').unbind('mousemove.mouse');
 			return false;
@@ -210,16 +211,18 @@
 		drag: function(e) {
 
 			var o = this.options;
-			if ($.browser.msie && !e.button) return this.stop.call(this, e); // IE mouseup check
+			if ($.browser.msie && !e.button) {
+				return this.stop.call(this, e); // IE mouseup check
+			}
 			
 			if(!this.initialized && (Math.abs(this._MP.left-e.pageX) >= o.distance || Math.abs(this._MP.top-e.pageY) >= o.distance)) {
-				if(o.start) o.start.call(o.executor || this, e, this.element);
+				(o.start && o.start.call(o.executor || this, e, this.element));
 				this.initialized = true;
 			} else {
-				if(!this.initialized) return false;
+				if(!this.initialized) { return false; }
 			}
 
-			if(o.drag) o.drag.call(this.options.executor || this, e, this.element);
+			(o.drag && o.drag.call(this.options.executor || this, e, this.element));
 			return false;
 			
 		}
