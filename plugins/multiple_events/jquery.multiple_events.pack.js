c2e3182b56363949ef8b751a8e0539c707a05376
joern.zaefferer <joern.zaefferer@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-05-08 19:02:00 +0000

    deleted multiple events plugin, got integrated into jQuery core
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5511 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/multiple_events/jquery.multiple_events.pack.js
+++
@@ -1,5 +0,0 @@
-/* Copyright (c) 2007 Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
- * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
- * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
- */
-eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(1($){f e=$.0.6,2=$.0.2;$.0.6=1(c,d,b,a){$.5(d.4(/\\7+/),1(){e.3($.0,c,8,b,a)})};$.0.2=1(b,c,a){$.5(c.4(/\\7+/),1(){2.3($.0,b,8,a)})}})(9);',16,16,'event|function|remove|call|split|each|add|s|this|jQuery||||||var'.split('|'),0,{}))
