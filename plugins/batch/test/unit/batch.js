56a62928c6788a91f2fe4d3ae318a3389e0a6563
brandon.aaron <brandon.aaron@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-05-09 18:14:56 +0000

    batch plugin: additional unit test
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5540 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/batch/test/unit/batch.js
+++ plugins/batch/test/unit/batch.js
@@ -4,6 +4,7 @@ test("batch", function() {
 	$.each( ['attrs', 'styles', 'offsets', 'widths', 'heights', 'htmls', 'texts', 'vals'], function(index, name) {
 		ok( $.fn[name], "Make sure " + name + " exists" );
 	});
+	ok( $('input[value=Test]').vals().constructor == Array, "Make sure the returned value is an actual Array" );
 });
 test("attrs", function() {
 	isSet( $('input[value=Test]').attrs('value'), ["Test", "Test"], "$('input[value=Test]').attrs('value')" );
