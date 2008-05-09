fb20532d0cad51160c96a6d5162df16e968cbf22
brandon.aaron <brandon.aaron@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-05-09 03:15:18 +0000

    batch plugin: 1.0 :)
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5527 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/batch/jquery.batch.js
+++ plugins/batch/jquery.batch.js
@@ -1,11 +1,11 @@
-/* Copyright (c) 2007 Brandon Aaron (http://brandonaaron.net)
+/*! Copyright (c) 2007 Brandon Aaron (http://brandonaaron.net)
  * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) 
  * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
  *
  * $LastChangedDate$
  * $Rev$
  *
- * Version: @VERSION
+ * Version: 1.0
  */
 (function($){
 
@@ -17,10 +17,20 @@ $.fn.batch = function(method) {
 	});
 };
 
-$.each( "attr css offset width height html text val".split(" "), function(index, method) {
-	$.fn[method+"s"] = function() {
-		return this.batch.apply( this, [method].concat( $.makeArray(arguments) ) );
-	};
-});
+$.batch = {
+	version: "1.0",
+	registerPlugin: function() {
+		$.each( arguments, function( index, plugin ) {
+			var method = plugin.constructor == Array && plugin[0] || plugin,
+				newMethod = plugin.constructor == Array && plugin[1] || plugin+"s";
+			if ( $.fn[ method ] && !$.fn[ newMethod ] )
+				$.fn[ newMethod ] = function() {
+					return this.batch.apply( this, [ method ].concat( $.makeArray(arguments) ) );
+				};
+		});
+	}
+};
+
+$.batch.registerPlugin( 'attr', ['css','styles'], 'offset', 'width', 'height', 'html', 'text', 'val' );
 
 })(jQuery);
--- plugins/batch/test/unit/batch.js
+++ plugins/batch/test/unit/batch.js
@@ -1,6 +1,11 @@
 module("batch");
 
 test("batch", function() {
+	$.each( ['attr', 'styles', 'offset', 'width', 'height', 'html', 'text', 'val'], function(index, name) {
+		ok( $.fn[name], "Make sure " + name + " exists" );
+	});
+});
+test("attrs", function() {
 	isSet( $('input[value=Test]').attrs('value'), ["Test", "Test"], "$('input[value=Test]').attrs('value')" );
 	isSet( $('input[value=Test]').attrs('value', function(){ return 'Updated'; }).attrs('value'), ["Updated", "Updated"], "$('input[value=Test]').attrs('value', function(){ return 'Updated'; })" );
 });
