f71ed4a568d634b02850f6f570495b2747374898
joern.zaefferer <joern.zaefferer@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-05-12 11:15:24 +0000

    validation: preparing 1.3 release
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5565 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/validate/jquery.validate.js
+++ plugins/validate/jquery.validate.js
@@ -1,5 +1,5 @@
 /*
- * jQuery validation plug-in pre-1.3
+ * jQuery validation plug-in 1.3
  *
  * http://bassistance.de/jquery-plugins/jquery-plugin-validation/
  * http://docs.jquery.com/Plugins/Validation
