d3ac86e3b2cdecede7c63a29d56980c220cb83a6
joern.zaefferer <joern.zaefferer@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-06-26 08:25:59 +0000

    sound plugin: enable stopping sounds by returning the sound element (bgsound in IE, embed in others)
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5750 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/sound/jquery.sound.js
+++ plugins/sound/jquery.sound.js
@@ -27,6 +27,10 @@
  *   timeout: 4000
  * });
  * 
+ * // stop a sound by removing the element returned by play
+ * var sound = $.sound.play(url);
+ * sound.remove();
+ * 
  * // disable playing sounds
  * $.sound.enabled = false;
  * 
@@ -37,44 +41,48 @@
 (function($) {
 	
 $.sound = {
-  tracks: {},
-  enabled: true,
-  template: function(src) {
-  	return '<embed style="height:0" loop="false" src="' + src + '" autostart="true" hidden="true"/>';
-  },
-  play: function(url, options){
-    if (!this.enabled)
-		return;
-    var settings = $.extend({
-	  url: url,
-	  timeout: 2000
-    }, options);
-	
-	if (settings.track) {
-		if (this.tracks[settings.track]) {
-			var current = this.tracks[settings.track];
-			current.Stop && current.Stop();
-			current.remove();  
+	tracks: {},
+	enabled: true,
+	template: function(src) {
+		return '<embed style="height:0" loop="false" src="' + src + '" autostart="true" hidden="true"/>';
+	},
+	play: function(url, options){
+		if (!this.enabled)
+			return;
+		var settings = $.extend({
+			url: url,
+			timeout: 2000
+		}, options);
+		
+		if (settings.track) {
+			if (this.tracks[settings.track]) {
+				var current = this.tracks[settings.track];
+				// TODO check when Stop is avaiable, certainly not on a jQuery object
+				current.Stop && current.Stop();
+				current.remove();  
+			}
 		}
+		
+		var element = $.browser.msie
+		  	? $('<bgsound/>').attr({
+		        src: settings.url,
+				loop: 1,
+				autostart: true
+		      })
+		  	: $(this.template(settings.url));
+			
+		element.appendTo("body");
+		
+		if (settings.track) {
+			this.tracks[settings.track] = element;
+		}
+		
+		setTimeout(function() {
+			element.remove();
+		}, options.timeout)
+		
+		return element;
 	}
-	
-	var element = $.browser.msie
-	  	? $('<bgsound/>').attr({
-	        src: settings.url,
-			loop: 1,
-			autostart: true
-	      })
-	  	: $(this.template(settings.url));
-    element.appendTo("body");
-	
-	if (settings.track) {
-		this.tracks[settings.track] = element;
-	}
-	
-	setTimeout(function() {
-		element.remove();
-	}, options.timeout)
-  }
 };
 
 })(jQuery);
