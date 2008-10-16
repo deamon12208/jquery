e76196a3bec295f4ddfb682daf529da3d3598fee
brandon.aaron <brandon.aaron@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-10-16 02:52:21 +0000

    gradient plugin has moved to git hub
    git-svn-id: http://jqueryjs.googlecode.com/svn/trunk@5874 c715fcbe-d12f-0410-84c4-316a508785bb

--- plugins/bgiframe/jquery.bgiframe.js
+++ plugins/bgiframe/jquery.bgiframe.js
@@ -69,6 +69,38 @@
  * @cat Plugins/bgiframe
  * @author Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
  */
+
+$.plugin('bgiframe', {
+	options: {
+		top     : 'auto', // auto == .currentStyle.borderTopWidth
+		left    : 'auto', // auto == .currentStyle.borderLeftWidth
+		width   : 'auto', // auto == offsetWidth
+		height  : 'auto', // auto == offsetHeight
+		opacity : true,
+		src     : 'javascript:false;'
+	},
+	
+	setup: function() {
+		if ( !this[0] && !this[0].setExpression ) return this;
+		var prop = function(n){return n&&n.constructor==Number?n+'px':n;},
+		    html = '<iframe class="bgiframe"frameborder="0"tabindex="-1"src="'+s.src+'"'+
+		               'style="display:block;position:absolute;z-index:-1;'+
+			               (s.opacity !== false?'filter:Alpha(Opacity=\'0\');':'')+
+					       'top:'+(s.top=='auto'?'expression(((parseInt(this.parentNode.currentStyle.borderTopWidth)||0)*-1)+\'px\')':prop(s.top))+';'+
+					       'left:'+(s.left=='auto'?'expression(((parseInt(this.parentNode.currentStyle.borderLeftWidth)||0)*-1)+\'px\')':prop(s.left))+';'+
+					       'width:'+(s.width=='auto'?'expression(this.parentNode.offsetWidth+\'px\')':prop(s.width))+';'+
+					       'height:'+(s.height=='auto'?'expression(this.parentNode.offsetHeight+\'px\')':prop(s.height))+';'+
+					'"/>';
+		this.each(function() {
+			if ( $('> iframe.bgiframe', this).length == 0 )
+				this.insertBefore( document.createElement(html), this.firstChild );
+		});
+	},
+	
+	teardown: function() {
+		
+	}
+});
 $.fn.bgIframe = $.fn.bgiframe = function(s) {
 	// This is only for IE6
 	if ( $.browser.msie && /6.0/.test(navigator.userAgent) ) {
